a25a7e438a7128b8474afea29c50bdde2271dee8
Dante le Poole <dantelepoole@gmail.com>
2022-04-11 22:30:07 -0400

    Initial import

---
+++ hasownproperty.js
@@ -0,0 +1,21 @@
+/**
+ * @module hasownproperty
+ */
+
+/**
+ * Functional and safe variant of {@link external:Object.prototype.hasOwnProperty Object.property.hasOwnProperty()}.
+ * 
+ * `hasownproperty()` is curried by default.
+ * 
+ * @func hasownproperty
+ * @param {object} object The object to check the properties of
+ * @param {(string|symbol)} key The key of the property to look for
+ * @returns {boolean}
+ */
+module.exports = require('./curry2') (
+
+    function hasownproperty(object, key) {
+        return Object.prototype.hasOwnProperty.call(object, key);
+    }
+
+)
