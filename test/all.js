2d8b83cb6b19561a54514dc1eb997294513393f2
Dante le Poole <dantelepoole@gmail.com>
2022-04-14 15:50:09 -0400

    Purge unnecessary code

--- test/all.js
+++ test/all.js
@@ -1,12 +1,10 @@
 const all = require('../all');
 const expect = require('chai').expect;
 
-const marker = Object.freeze({});
 const numbers1to10 = Object.freeze([1,2,3,4,5,6,7,8,9,10]);
 
 let callcount = 0;
 function isnumber(x) { callcount += 1; return typeof x ==='number'; };
-function notnumber(x) { return ! isnumber(x) };
 function islessthan(num, x) { callcount += 1; return (x < num); };
 
 describe('all()', function() {
@@ -37,15 +35,7 @@ describe('all()', function() {
         }
     )
 
-    it('should return false if its predicate argument returns false for one item in the list',
-        function() {
-
-            const islessthan10 = islessthan.bind(null, 10);
-            expect( all(islessthan10, numbers1to10) ).to.be.false;
-        }
-    )
-
-    it('should run its predicate argument once for each item in the list if the predicate return true for each item',
+    it('should run its predicate argument once for each item in the list if the predicate returns true for each item',
         function() {
             const result = all(isnumber, numbers1to10);
             expect( result ).to.be.true;
@@ -53,7 +43,7 @@ describe('all()', function() {
         }
     )
 
-    it('should not test remaining list items after the predicate returns false',
+    it('should not test remaining list items after the predicate first returns false',
         function() {
             const islessthan5 = islessthan.bind(null, 5);
             const result = all(islessthan5, numbers1to10);
