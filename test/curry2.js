df6a3c5fc65a0d26421df9f73eece2a859b80e72
Dante le Poole <dantelepoole@gmail.com>
2022-04-16 13:44:42 -0400

    Initial import

---
+++ test/curry2.js
@@ -0,0 +1,41 @@
+const expect = require('chai').expect;
+const curry2 = require('../curry2');
+
+function countargs(...args) {
+    return args.length;
+}
+
+function returnargs(...args) {
+    return args;
+}
+
+describe(`curry2()`, function() {
+
+    beforeEach(
+        function() {
+
+        }
+    )
+
+    it(`should return a function`,
+        function () {
+            expect( curry2(countargs) ).to.be.a('function');
+        }
+    )
+
+    it(`should collect two arguments before invoking the function`,
+        function () {
+            expect( curry2(countargs)(42)(42) ).to.be.equal(2);
+            expect( curry2(countargs)(42, 42) ).to.be.equal(2);
+
+        }
+    )
+
+    it(`should invoke the function with the collected argument`,
+        function () {
+            expect( curry2(returnargs)(42)(42) ).to.be.deep.equal([42,42]);
+            expect( curry2(returnargs)(42, 42) ).to.be.deep.equal([42,42]);
+        }
+    )
+
+})
---
+++ test/curry3.js
@@ -0,0 +1,41 @@
+const expect = require('chai').expect;
+const curry3 = require('../curry3');
+
+function countargs(...args) {
+    return args.length;
+}
+
+function returnargs(...args) {
+    return args;
+}
+
+describe(`curry3()`, function() {
+
+    beforeEach(
+        function() {
+
+        }
+    )
+
+    it(`should return a function`,
+        function () {
+            expect( curry3(countargs) ).to.be.a('function');
+        }
+    )
+
+    it(`should collect three arguments before invoking the function`,
+        function () {
+            expect( curry3(countargs)(42)(42)(42) ).to.be.equal(3);
+            expect( curry3(countargs)(42, 42, 42) ).to.be.equal(3);
+
+        }
+    )
+
+    it(`should invoke the function with the collected argument`,
+        function () {
+            expect( curry3(returnargs)(42)(42)(42) ).to.be.deep.equal([42,42,42]);
+            expect( curry3(returnargs)(42, 42, 42) ).to.be.deep.equal([42,42,42]);
+        }
+    )
+
+})
