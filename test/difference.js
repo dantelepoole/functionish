888a5f612518f54d3a11b46e97269ea433c87139
Dante le Poole <dantelepoole@gmail.com>
2022-04-16 18:33:40 -0400

    Initial import

---
+++ test/difference.js
@@ -0,0 +1,72 @@
+const expect = require('chai').expect;
+const difference = require('../difference');
+
+const list1 = [1,2,3,4,2];
+const list2 = [3,4,5,6,6];
+
+describe(`difference()`, function() {
+
+    beforeEach(
+        function() {
+
+        }
+    )
+
+    it(`should be curried by default`,
+        function () {
+            const curried = difference([]);
+            expect( curried ).to.be.a('function');
+            expect( curried([]) ).to.be.an('array');
+        }
+    )
+
+    it(`should return an array of the items in list1 that are not present in list2`,
+        function () {
+            const result = difference(list1, list2);
+            expect(result).to.be.deep.equal([1,2]);
+        }
+    )
+
+    it(`should return an array without duplicate items`,
+        function () {
+            const result = difference([1,1,1,1,1,1,2], [2,3,3,3,3,3,3,3]);
+            expect(result).to.be.deep.equal([1]);
+        }
+    )
+    
+    it(`should return an empty array if both arguments are empty arrays`,
+        function () {
+            expect( difference([],[]) ).to.be.deep.equal([]);
+        }
+    )
+
+    it(`should return a shallow copy of list1 without duplicates if list2 is empty`,
+        function () {
+            expect( difference([1,2,2,3,4],[]) ).to.be.deep.equal([1,2,3,4]);
+            
+            const alist = [1,2,3,4];
+            const result = difference(alist, []);
+            expect( result ).to.not.be.equal(alist);
+            expect( result ).to.be.deep.equal(alist);
+        }
+    )
+
+    it(`should return an empty array if list1 is empty`,
+        function () {
+            expect( difference([],[1,2,3,4]) ).to.be.deep.equal([]);
+        }
+    )
+
+    it(`should return array with its items in the same order as list1, except for duplicates`,
+        function () {
+            expect( difference([42, 23, 15, 23, 42, 1],[5,6,7,8,9]) ).to.be.deep.equal([42,23,15,1]);
+        }
+    )
+
+    it(`should accept either arrays or iterables for both arguments, and always return an array`,
+        function () {
+            expect( difference('hari', ['s','e','l','d','o','n']) ).to.be.deep.equal(['h','a','r','i']);
+            expect( difference(['h','a','r','i'], 'seldon') ).to.be.deep.equal(['h','a','r','i']);
+        }
+    )
+})
