6387d3733fee3e290f7f965b16e1c4fd499c9c66
Dante le Poole <dantelepoole@gmail.com>
2022-04-20 14:41:02 -0400

    Edit JSDoc

--- isdefined.js
+++ isdefined.js
@@ -9,8 +9,8 @@ const isvoid = require('./isvoid');
 /**
  * Return `true` if value is neither `null`, `undefined` nor `NaN`, otherwise return `false`.
  * 
- * Despite its name, this function checks for `null` and `NaN` as well as for `undefined`. So it is not the counterpart
- * to {@link module:isundefined isundefined()} (which only checks for `undefined`) but in fact the counterpart for
+ * Despite its name, this function checks for `null` and `NaN` as well as `undefined`. So it is not the counterpart
+ * to {@link module:isundefined isundefined()} (which only checks for `undefined`) but in fact the counterpart to
  * {@link module:isvoid isvoid()}.
  * 
  * @func isdefined
