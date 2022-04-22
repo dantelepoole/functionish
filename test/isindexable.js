c824530eef0ff99b93c8ea153e6c5a38b5d2d140
Dante le Poole <dantelepoole@gmail.com>
2022-04-21 21:24:54 -0400

    Initial import

---
+++ test/isindexable.js
@@ -0,0 +1,116 @@
+const expect = require('chai').expect;
+const isindexable = require('../isindexable');
+
+const markerobject = Object.freeze({});
+const markerarray = Object.freeze([]);
+const markersymbol = Symbol();
+
+describe(`isindexable()`, function() {
+
+    beforeEach(
+        function() {
+
+        }
+    )
+
+    it(`should return true if its argument is an object with a numeric own length-property of 0`,
+        function () {
+
+            const obj = {
+                length : 0
+            }
+
+            expecttrue( isindexable(obj) );
+        }
+    )
+
+    it(`should return true if its argument is an object with an own property for the key 0 AND for the key equal to its length-property minus 1`,
+        function () {
+
+            const obj = {
+                length : 42,
+                [0]    : true,
+                [41]   : true
+            }
+
+            expecttrue( isindexable(obj) );
+        }
+    )
+})
+
+function countarguments(...args) {
+    return args.length;
+}
+
+function returnarguments(...args) {
+    return args;
+}
+
+function expecttothrow(func, ...args) {
+    expect( () => func(...args) ).to.throw();
+}
+
+function expectnottothrow(func, ...args) {
+    expect( () => func(...args) ).to.not.throw();
+}
+
+function expectequal(value1, value2) {
+    expect(value1).to.be.equal(value2);
+}
+
+function expectdeepequal(value1, value2) {
+    expect(value1).to.be.deep.equal(value2);
+}
+
+function expectnotequal(value1, value2) {
+    expect(value1).to.be.not.equal(value2);
+}
+
+function expectnotdeepequal(value1, value2) {
+    expect(value1).to.be.not.deep.equal(value2);
+}
+
+function expectclone(value1, value2) {
+    expect(value1).to.be.deep.equal(value2);
+    expect(value1).to.be.not.equal(value2);
+}
+
+function expectnull(value) {
+    expect(value).to.be.null;
+}
+
+function expectundefined(value) {
+    expect(value).to.be.undefined;
+}
+
+function expectnan(value) {
+    expect(value).to.be.NaN;
+}
+
+function expectfalsy( value ) {
+    expect( !! value ).to.be.false;
+}
+
+function expectfalse(value) {
+    expect(value).to.be.false;
+}
+
+function expecttrue(value) {
+    expect(value).to.be.true;
+}
+
+function expecttruthy(value) {
+    expect( !! value ).to.be.true;
+}
+
+function expecttype(type, value) {
+    expect(value).to.be.a(type);
+} 
+
+const expectarray = expecttype.bind(null, 'array');
+const expectfunction = expecttype.bind(null, 'function');
+const expectnumber = expecttype.bind(null, 'number');
+const expectstring = expecttype.bind(null, 'string');
+const expectobject = expecttype.bind(null, 'object');
+const expectboolean = expecttype.bind(null, 'boolean');
+const expectsymbol = expecttype.bind(null, 'symbol');
