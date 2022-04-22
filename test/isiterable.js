f2c7cd844dee2033b9d4e4bd0ba1357ce00d7f15
Dante le Poole <dantelepoole@gmail.com>
2022-04-21 21:47:10 -0400

    Initial import

---
+++ test/isiterable.js
@@ -0,0 +1,124 @@
+const expect = require('chai').expect;
+const isiterable = require('../isiterable');
+
+const markerobject = Object.freeze({});
+const markerarray = Object.freeze([]);
+const markersymbol = Symbol();
+
+describe(`isiterable()`, function() {
+
+    beforeEach(
+        function() {
+
+        }
+    )
+
+    it(`should return false if its argument is a string`,
+        function () {
+            expectfalse( isiterable('') );
+            expectfalse( isiterable('foobar') );
+        }
+    )
+
+    it(`should return true if its argument has a method with the key [Symbol.iterator]`,
+        function () {
+            const obj = {
+                [Symbol.iterator] : function() {}
+            }
+
+            expecttrue( isiterable(obj) );
+        }
+    )
+
+    it(`should return true if its argument is an array, Map or Set`,
+        function () {
+            expecttrue( isiterable([]) );
+            expecttrue( isiterable( new Map() ) );
+            expecttrue( isiterable( new Set() ) );
+        }
+    )
+
+    it(`should return true if its argument is an iterator object`,
+        function () {
+            const iterator = [1,2,3][Symbol.iterator]();
+            expecttrue( isiterable(iterator) );
+        }
+    )
+})
+
+function countarguments(...args) {
+    return args.length;
+}
+
+function returnarguments(...args) {
+    return args;
+}
+
+function expecttothrow(func, ...args) {
+    expect( () => func(...args) ).to.throw();
+}
+
+function expectnottothrow(func, ...args) {
+    expect( () => func(...args) ).to.not.throw();
+}
+
+function expectequal(value1, value2) {
+    expect(value1).to.be.equal(value2);
+}
+
+function expectdeepequal(value1, value2) {
+    expect(value1).to.be.deep.equal(value2);
+}
+
+function expectnotequal(value1, value2) {
+    expect(value1).to.be.not.equal(value2);
+}
+
+function expectnotdeepequal(value1, value2) {
+    expect(value1).to.be.not.deep.equal(value2);
+}
+
+function expectclone(value1, value2) {
+    expect(value1).to.be.deep.equal(value2);
+    expect(value1).to.be.not.equal(value2);
+}
+
+function expectnull(value) {
+    expect(value).to.be.null;
+}
+
+function expectundefined(value) {
+    expect(value).to.be.undefined;
+}
+
+function expectnan(value) {
+    expect(value).to.be.NaN;
+}
+
+function expectfalsy( value ) {
+    expect( !! value ).to.be.false;
+}
+
+function expectfalse(value) {
+    expect(value).to.be.false;
+}
+
+function expecttrue(value) {
+    expect(value).to.be.true;
+}
+
+function expecttruthy(value) {
+    expect( !! value ).to.be.true;
+}
+
+function expecttype(type, value) {
+    expect(value).to.be.a(type);
+} 
+
+const expectarray = expecttype.bind(null, 'array');
+const expectfunction = expecttype.bind(null, 'function');
+const expectnumber = expecttype.bind(null, 'number');
+const expectstring = expecttype.bind(null, 'string');
+const expectobject = expecttype.bind(null, 'object');
+const expectboolean = expecttype.bind(null, 'boolean');
+const expectsymbol = expecttype.bind(null, 'symbol');
