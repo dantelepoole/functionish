7c7ca2b822c82eba96031eb31b88942948f74ee2
Dante le Poole <dantelepoole@gmail.com>
2022-04-24 21:29:43 -0400

    Add JSDoc documentation

--- lift.js
+++ lift.js
@@ -6,6 +6,13 @@
 
 const isdefined = require('./isdefined');
 
+/**
+ * Return an array containing *value* as its only item. If *value* is `undefined`, `null` or `NaN` an empty array is
+ * returned.
+ * 
+ * @param {any} value 
+ * @returns {any[]}
+ */
 module.exports = function lift(value) {
     return isdefined(value) ? [value] : [];
 }
