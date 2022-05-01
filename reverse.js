e947a27b57913f9acd372feb3847b39ba220db23
Dante le Poole <dantelepoole@gmail.com>
2022-04-30 23:00:22 -0400

    Edit JSDoc comment

--- reverse.js
+++ reverse.js
@@ -5,7 +5,7 @@
 'use strict';
 
 /**
- * Return a shallow copy of array with its items in reverse order.
+ * Return a shallow copy of list with its items in reverse order.
  * 
  * @func reverse
  * @param {any[]} list The array of items to reverse
