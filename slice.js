b1db848e31492135037cdaee9724c8e74f34a8b2
Dante le Poole <dantelepoole@gmail.com>
2022-05-02 18:26:32 -0400

    Curry, allow falsy values for endindex and edit JSDoc comment

--- slice.js
+++ slice.js
@@ -4,17 +4,22 @@
 
 'use strict';
 
+const isvoid = require('./isvoid');
+
 /**
  * Functional variant of {@link external:Array.prototype.slice Array.prototype.slice()}. Return a shallow copy of
- * the *list* array or string starting at *startindex* up to but including *endindex*.
+ * the *list* array or string starting at *startindex* up to but including *endindex*. Pass 0, `null`, `undefined` or
+ * `NaN` for *endindex* to return a copy of the entire array (i.e. set *endindex* equal to *list*'s length).
  * 
  * @func slice
  * @see {@link external:Array.prototype.slice Array.prototype.slice()}
+ * @param {number} startindex The index of the first item to include in the slice
+ * @param {number} endindex The index of the item *after* the last item to include in the slice
  * @param {(array|string)} list The string or array of items to slice
- * @param {number} [startindex=0] The index of the first item to include in the slice
- * @param {number} [endindex] The index of the item *after* the last item to include in the slice
  * @returns {any[]}
  */
-module.exports = function slice(list, startindex=0, endindex) {
-    return list.slice(startindex, endindex);
+module.exports = require('./curry3') (slice);
+
+function slice(startindex, endindex, list) {
+    return list.slice(startindex, endindex || list.length);
 }
