2d08efadc00e23990f78aad2b68a75e7f4c16d49
Dante le Poole <dantelepoole@gmail.com>
2022-05-12 21:30:51 -0400

    Initial import

---
+++ test/tojson.js
@@ -0,0 +1,12 @@
+const expect = require('chai').expect;
+const tojson = require('../tojson');
+
+describe(`tojson()`, function() {
+
+    it(`should be a reference to the stringify() method on the global JSON object`,
+        function () {
+            expect( tojson ).to.be.equal( JSON.stringify );
+        }
+    )
+
+})
