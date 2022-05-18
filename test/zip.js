bb4628ed0b7d01b40b8480df523f864b885c9f5f
Dante le Poole <dantelepoole@gmail.com>
2022-05-17 22:32:00 -0400

    Initial import

---
+++ test/zip.js
@@ -0,0 +1,25 @@
+const zip = require('../zip');
+const expect = require('chai').expect;
+
+const abc = ['a','b','c'];
+const onetwothree = [1,2,3];
+const onetwothreefourfive = [1,2,3,4,5];
+
+describe('zip()', function() {
+
+    beforeEach(
+        function() {
+        }
+    )
+
+    it('should be tested')//, 
+    //     function() {
+
+    //         const curried = zip(abc);
+    //         expect(curried).to.be.a('function');
+    //         expect( curried(onetwothree) ).to.be.an('array');
+    //     }
+    // )
+
+
+})
