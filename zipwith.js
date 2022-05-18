e75ca033e92022ba01d63c8dfeecebe6f1443cc8
Dante le Poole <dantelepoole@gmail.com>
2022-05-17 22:20:11 -0400

    Return an array or an iterable depending on the types of the arguments

--- zipwith.js
+++ zipwith.js
@@ -4,6 +4,7 @@
 'use strict';
 
 const isarray = require('./isarray');
+const minimumvalue = Math.min;
 
 /**
  * Similar to  {@link module:zip zip()} except that *func* is applied to the elements of *list1* and *list2* and the
@@ -15,12 +16,20 @@ const isarray = require('./isarray');
  *     
  * const zipwith = require('functionish/zipwith');
  * 
- * function multiply(x,y) { return (x*y) }
+ * function sum(x,y) { return (x+y) }
  * 
  * const list1 = [1,3,5];
  * const list2 = [2,4,6];
  * 
- * zipwith(multiply, list1, list2); // returns [ 2, 12, 30 ]
+ * zipwith(sum, list1, list2); // returns [ 3, 7, 11 ]
+ * 
+ * for( const item of zipwith(sum, 'foobar', 'Hari') ) console.log(item);
+ * 
+ * // prints:
+ * //   'fH'
+ * //   'oa'
+ * //   'or'
+ * //   'bi'
  * 
  * @func zipwith
  * @see {@link module:zip zip()}
@@ -32,22 +41,40 @@ const isarray = require('./isarray');
 module.exports = require('./curry3')(
 
     function zipwith(func, list1, list2) {
-        return ziparrayswith(func, toarray(list1), toarray(list2))
+        return isarray(list1) && isarray(list2) ? ziparrayswith(func, list1, list2)
+             : zipiterableswith(func, list1, list2);
     }
 
 )
 
-function toarray(value) {
-    return isarray? value : [value];
-}
-
 function ziparrayswith(func, array1, array2) {
 
-    const length = Math.min(array1.length, array2.length);
+    const itemcount = minimumvalue(array1.length, array2.length);
 
-    const resultarray = new Array(length);
+    const resultarray = new Array(itemcount);
 
-    for( let index = 0; index < length; index++ ) resultarray[index] = func(array1[index], array2[index]);
+    for( let index = 0; index < itemcount; index++ ) {
+        resultarray[index] = func(array1[index], array2[index]);
+    }
 
     return resultarray;
+}
+
+function zipiterableswith(func, list1, list2) {
+
+    const iterator1 = list1[Symbol.iterator]();
+    const iterator2 = list2[Symbol.iterator]();
+
+    function next() {
+        const item1 = iterator1.next();
+        const item2 = iterator2.next();
+
+        return (item1.done || item2.done) ? { done:true } : { done:false, value:func(item1.value, item2.value)}
+    }
+
+    return {
+        [Symbol.iterator]() {
+            return { next }
+        }
+    }
 }
