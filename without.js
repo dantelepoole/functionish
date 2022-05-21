3b27aeb150d9a880037bff3317b0348be07bb0a5
Dante le Poole <dantelepoole@gmail.com>
2022-05-21 00:06:44 -0400

    Only work with objects

--- without.js
+++ without.js
@@ -3,15 +3,8 @@
  */
 'use strict';
 
-const isarray = require('./isarray');
-const isobject = require('./isobject');
-
 /**
- * Return a copy of *list* with *values* removed. Comparison is performed using strict equality.
- * 
- * If *list* is an array, a new array is returned containing all original items except those
- * contained in *values*. If *list* is an object, a copy of *list* is returned without the properties whose names are
- * contained *values*. Otherwise, it returns *list* itself.
+ * Return a shallow copy of the *source* object without the properties whose keys are present in *keys*.
  * 
  * `without()` is curried by default.
  * 
@@ -19,44 +12,23 @@ const isobject = require('./isobject');
  *     
  * const without = require('functionish/without');
  * 
- * without( [1,2], [1,2,3] ); // returns [3]
- * without( 1, [1,2,3] ); // returns [2,3]
- * without( [], [1,2,3] ); // returns [1,2,3]
- * 
- * without( 'a', {a:'aaa', b:'bbb'} ); // returns {b:'bbb'}
+ * without( ['a'], {a:'aaa', b:'bbb'} ); // returns {b:'bbb'}
  * without( ['a','b'], {a:'aaa', b:'bbb'} ); // returns {}
- * without( [], {a:'aaa', b:'bbb'} ); // returns {a:'aaa', b:'bbb'}
  * 
  * @func without
- * @param {any[]} values The array of values to remove from *list*
- * @param {(any[]|object)} list The source to remove *values* from
- * @returns {(any[]|object)}
+ * @param {(any[]|iterable)} keys A list of keys of the properties to remove from *source*
+ * @param {object} source The object to remove the properties from
+ * @returns {object} A shallow copy of *source* with the specified properties
  */
-module.exports = require('./curry2')(
+module.exports = require('./curry2')(without)
 
-    function without(values, list) {
-
-        const skipkeys = new Set(values);
-
-        return isarray(list) ? list.filter( notpresent(skipkeys) ) 
-             : isobject(list) ? objectwithout( skipkeys, Object(list) )
-             : list;
-    }
-)
-
-function notpresent(keyset) {
-
-    return function _notincludes(key) {
-        return ! keyset.has(key);
-    }
-}
-
-function objectwithout(skipkeyset, source) {
+function without(keys, source) {
 
     const target = {};
+    const excludedkeys = new Set(keys);
 
     Object.keys(source).forEach(
-        key => skipkeyset.has(key) || (target[key] = source[key])
+        key => excludedkeys.has(key) || (target[key] = source[key])
     )
 
     return target;
