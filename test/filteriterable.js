ec02b2587b84dead4e13501ab7993d60dd694103
Dante le Poole <dantelepoole@gmail.com>
2022-05-21 21:57:25 -0400

    Initial import

---
+++ test/filteriterable.js
@@ -0,0 +1,12 @@
+const filteriterable = require('../filteriterable');
+const expect = require('chai').expect;
+
+describe('filteriterable()', function() {
+
+    beforeEach(
+        function() {
+        }
+    )
+
+    it('should be tested')
+})
---
+++ test/mapiterable.js
@@ -0,0 +1,12 @@
+const mapiterable = require('../mapiterable');
+const expect = require('chai').expect;
+
+describe('mapiterable()', function() {
+
+    beforeEach(
+        function() {
+        }
+    )
+
+    it('should be tested')
+})
