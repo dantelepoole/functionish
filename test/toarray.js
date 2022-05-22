d4f394404f94e6fd641f40d266894bfae3faf505
Dante le Poole <dantelepoole@gmail.com>
2022-05-21 23:13:24 -0400

    Remove mapping and return an argument array unaltered

--- test/toarray.js
+++ test/toarray.js
@@ -9,54 +9,35 @@ describe('toarray()', function() {
     )
 
     it('should return an array containing the items from the iterable in order', test_returnarray);
-    it('should return a shallow copy of the iterable if the iterable is an array', test_shallowcopyarray);
+    it('should return the iterable if the iterable is already an array', test_reflectarray);
     it('should return an empty array if the iterable produces no items', test_returnemptyarray);
-    it('should throw if the iterable is not iterable', test_throwbaditerable);
-    it('should apply the mapfunc to each item in the iterable if a mapfunc is provided', test_applymapfunc);
-    it('should throw if the mapfunc is not a function', test_throwbadmapfunc);
+    it('should return an empty array if the iterable is not iterable', test_returnemptyarrayifnotiterable);
+    it('should throw if iterable is null or undefined', test_throwifnullorundefined);
 
 })
 
-function double(x) {
-    return x*2;
-}
-
-function uppercase(str) {
-    return str.toUpperCase();
-}
-
-function test_applymapfunc() {
-
-    expect( toarray([3,4,5], double) ).to.be.deep.equal( [6,8,10] );
-    expect( toarray( 'foobar', uppercase) ).to.be.deep.equal( ['F','O','O','B','A','R'] );
-}
-
 function test_returnarray() {
-    expect( toarray() ).to.be.deep.equal([]);
     expect( toarray([1,2,3]) ).to.be.deep.equal([1,2,3]);
     expect( toarray('foobar') ).to.be.deep.equal(['f','o','o','b','a','r']);
 }
 
 function test_returnemptyarray() {
-    expect( toarray() ).to.be.an('array').with.length(0);
     expect( toarray('') ).to.be.an('array').with.length(0);
     expect( toarray([]) ).to.be.an('array').with.length(0);
 }
 
-function test_shallowcopyarray() {
+function test_reflectarray() {
 
     const array = [ {}, [], Symbol() ];
-    expect( toarray(array) ).to.be.deep.equal(array);
-    expect( toarray(array) ).not.to.be.equal(array);
-    expect( toarray(array)[0] ).to.be.equal( array[0] );
-    expect( toarray(array)[1] ).to.be.equal( array[1] );
-    expect( toarray(array)[2] ).to.be.equal( array[2] );
+    expect( toarray(array) ).to.be.equal(array);
 }
 
-function test_throwbaditerable() {
-    expect( ()=>toarray({}).to.throw() );
+function test_returnemptyarrayifnotiterable() {
+    expect( toarray({}) ).to.be.deep.equal([]);
 }
 
-function test_throwbadmapfunc() {
-    expect( ()=>toarray([3,4,5], {}) ).to.throw();
+function test_throwifnullorundefined() {
+    expect( ()=>toarray() ).to.throw();
+    expect( ()=>toarray(null) ).to.throw();
+    expect( ()=>toarray(undefined) ).to.throw();
 }
--- toarray.js
+++ toarray.js
@@ -4,20 +4,17 @@
 
 'use strict';
 
-const unary = require('./unary');
+const isarray = require('./isarray');
 
 /**
- * Return a new Array initialized with the items from *iterable*. If *mapfunc* is provided, each item is passed to
- * *mapfunc* and the return array is initialized with *mapfunc*'s return values instead.
+ * If *iterable* is an array return it. Otherwise, return a new Array initialized with the items from *iterable*.
+ * 
+ * If *iterable* is not iterable, an empty array is returned.
  * 
  * @func toarray
  * @param {iterable} [iterable] An iterable that produces the items to initialize the array with
- * @param {function} [mapfunc] The func to map each item produced by *iterable* with
  * @returns {any[]}
  */
-module.exports = function toarray(iterable=[], mapfunc=undefined) {
-
-    if(typeof mapfunc === 'function') mapfunc = unary(mapfunc);
-
-    return Array.from(iterable, mapfunc);
+module.exports = function toarray(iterable) {
+    return isarray(iterable) ? iterable : Array.from(iterable);
 }
