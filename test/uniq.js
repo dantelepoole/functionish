20b2dbcda54daa213008a37ac196556357c3e235
Dante le Poole <dantelepoole@gmail.com>
2022-05-21 22:31:35 -0400

    Return an iterable instead of an array

--- test/uniq.js
+++ test/uniq.js
@@ -1,5 +1,11 @@
 const uniq = require('../uniq');
 const expect = require('chai').expect;
+const isiterable = require('../isiterable');
+
+function uniqarray(iterable) {
+    expect( isiterable(iterable) || (typeof iterable === 'string') ).to.be.true;
+    return Array.from( uniq(iterable) );
+}
 
 function iterablefactory(items) {
 
@@ -12,39 +18,25 @@ function iterablefactory(items) {
 
 describe('uniq()', function() {
 
-    it('should return an array containing all items in the argument in order but without duplicates',
+    it('should return an iterable producing all items in the argument in order but without duplicates',
         function () {
-            expect( uniq( [1,2,3] ) ).to.be.deep.equal( [1,2,3] );
-            expect( uniq( [1,2,1,2,3,3] ) ).to.be.deep.equal( [1,2,3] );
-            expect( uniq( [3,3,2,2,1,1] ) ).to.be.deep.equal( [3,2,1] );
-            expect( uniq( [1,2,3,1,2,3] ) ).to.be.deep.equal( [1,2,3] );
-            expect( uniq( ['a','b','a','c','b'] ) ).to.be.deep.equal( ['a','b','c'] );
-        }
-    )
-
-    it('should throw if its argument is not iterable',
-        function () {
-            expect( () => uniq({}) ).to.throw();
-        }
-    )
-
-    it('should return an empty array if its argument is null or undefined',
-        function () {
-            expect( uniq(null) ).to.be.deep.equal([]);
-            expect( uniq(undefined) ).to.be.deep.equal([]);
-            expect( uniq() ).to.be.deep.equal([]);
+            expect( uniqarray( [1,2,3] ) ).to.be.deep.equal( [1,2,3] );
+            expect( uniqarray( [1,2,1,2,3,3] ) ).to.be.deep.equal( [1,2,3] );
+            expect( uniqarray( [3,3,2,2,1,1] ) ).to.be.deep.equal( [3,2,1] );
+            expect( uniqarray( [1,2,3,1,2,3] ) ).to.be.deep.equal( [1,2,3] );
+            expect( uniqarray( ['a','b','a','c','b'] ) ).to.be.deep.equal( ['a','b','c'] );
         }
     )
 
     it('should work with strings',
         function () {
-            expect( uniq('dante le poole') ).to.be.deep.equal(['d','a','n','t','e',' ','l','p','o']);
+            expect( uniqarray('dante le poole') ).to.be.deep.equal(['d','a','n','t','e',' ','l','p','o']);
         }
     )
 
     it('should work with iterable objects',
         function () {
-            expect( uniq( iterablefactory([1,1,2,2,3,3]) ) ).to.be.deep.equal([1,2,3]);
+            expect( uniqarray( iterablefactory([1,1,2,2,3,3]) ) ).to.be.deep.equal([1,2,3]);
         }
     )
 })
--- uniq.js
+++ uniq.js
@@ -4,13 +4,37 @@
 
 'use strict';
 
+const partial = require('./partial');
+
 /**
- * Return an array containing only the unique items in *list*, i.e. without any duplicate items.
+ * Return an iterable that produces the unique items in *iterable*, i.e. silently dropping any duplicate items it
+ * encounters.
  * 
  * @func uniq
- * @param {any[]} list The array or iterable of items to remove duplicates from
- * @returns {any[]}
+ * @param {iterable} iterable The iterable of items to remove duplicates from
+ * @returns {iterable}
  */
-module.exports = function uniq(list) {
-    return [ ...new Set(list) ];
+ module.exports = function uniq(iterable) {
+
+    return {
+        [Symbol.iterator]() {
+
+            const iterator = iterable[Symbol.iterator]();
+            const duplicateitems = new Set();
+            const next = partial(nextuniqitem, duplicateitems, iterator);
+
+            return { next }
+        }
+    }
+}
+
+function nextuniqitem(duplicateitems, iterator) {
+
+    let nextitem = iterator.next();
+
+    while( ( ! nextitem.done ) && duplicateitems.has(nextitem.value) ) nextitem = iterator.next();
+
+    nextitem.done ? duplicateitems.clear() : duplicateitems.add(nextitem.value);
+
+    return nextitem;
 }
