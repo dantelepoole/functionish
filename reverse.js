e8e36153f6ae9eff8701939897a15e8b5812f271
Dante le Poole <dantelepoole@gmail.com>
2022-05-25 21:52:08 -0400

    Always iterate to reverse the items

--- reverse.js
+++ reverse.js
@@ -7,23 +7,22 @@
 const isarray = require('./isarray');
 
 /**
- * If *list* is an array, return a new array with *list*'s items in reverse order. Otherwise, presume *list* is iterable
- * and return an iterable that produces *list*'s items in reverse order.
+ * Return an iterable object that produces the items from *iterable* in reverse order. Be aware that this function
+ * first loads all items into an array before iterating the array in reverse order.
+ * 
+ * Unlike the `Array.prototype.reverse()` method, this function does not affect the (order of) of the items in the
+ * argument if the argument is an array.
  * 
  * @func reverse
- * @param {(any[]|iterable)} list The array or iterable of items to reverse
- * @returns {(any[]|iterable)}
+ * @param {iterable} iterable The iterable of items to produce in reverse order
+ * @returns {iterable}
  */
-module.exports = function reverse(list) {
-    return isarray(list) ? list.slice().reverse() : reverseiterable(list);
-}
-
-function reverseiterable(list) {
+module.exports = function reverse(iterable) {
 
     return {
         [Symbol.iterator] : function* () {
 
-            const items = Array.from(list);
+            const items = Array.from(iterable);
             for( let index = items.length - 1; index >= 0; index -= 1) yield items[index];
         }
    }
--- test/reverse.js
+++ test/reverse.js
@@ -5,40 +5,19 @@ const isiterable = require('../isiterable');
 
 describe(`reverse()`, function() {
 
-    it(`should return an array with the items in its argument array in reverse order if the list is an array`,
+    it(`should return an iterable producing the argument's items in reverse order`,
         function () {
             const reversed = reverse([1,2,3]);
-            expect(reversed).to.be.deep.equal([3,2,1]);
+            expect( isiterable(reversed) ).to.be.true;
+            expect( Array.from(reversed) ).to.be.deep.equal([3,2,1]);
         }
     )
 
-    it(`should return an iterable that produces *list*'s items in reverse order if the list is a non-array iterable`,
-        function () {
-            const iterable = reverse('foobar');
-            expect( isiterable(iterable) ).to.be.true;
-            expect( Array.from(iterable) ).to.be.deep.equal( ['r','a','b','o','o','f'] );
-        }
-    )
-
-    it(`should not change its argument array`,
+    it(`should not change the (order of the) items in the argument`,
         function () {
             const numbers1to3 = [1,2,3];
             reverse(numbers1to3);
             expect(numbers1to3).to.be.deep.equal([1,2,3]);
-        }
-    )
-
-    it(`should not affect the items in the argument array`,
-        function () {
-            const markerobject = {};
-            const markersymbol = Symbol('foobar');
-            const markerarray = [markerobject, markersymbol];
-            const list = [markerobject, markerarray, 42, markersymbol];
-            const reversed = reverse(list);
-            expect(reversed[0]).to.be.equal(markersymbol);
-            expect(reversed[1]).to.be.equal(42);
-            expect(reversed[2]).to.be.equal(markerarray);
-            expect(reversed[3]).to.be.equal(markerobject);
         }
     )
 })
