e47449a3bb362e074f548196172a4ba9e9969a29
Dante le Poole <dantelepoole@gmail.com>
2022-05-25 21:27:31 -0400

    Refactor

--- flat.js
+++ flat.js
@@ -3,48 +3,20 @@
  */
 'use strict';
 
-function isiterable(x) {
-    // return (typeof x !== 'string' && typeof x?.[Symbol.iterator] === 'function');
-    return (typeof x?.[Symbol.iterator] === 'function');
-}
-
 /**
- * Function variant of {@link external:Array.prototype.flat Array.prototype.flat()}. Pass *depth* to *flattenable*'s
- * `flatten()`-method and return the result. If *flattenable* has no `flat()` method, assume it is iterable and return
- * an iterable that recursively flattens flattenables's items to the specified *depth*.
+ * Invoke the `flat()` method of *flattenable* with the specified depth and return the result.
  * 
- * `flatten()` is curried by default.
+ * `flat()` is curried by default.
  * 
- * @func flatten
+ * @func flat
  * @see {@link external:Array.prototype.flat Array.prototype.flat()}
- * @param {number} depth The number of dimensions to flatten *flattenable*
- * @param {(flattenable|iterable)} flattenable An object with a `flat()` method or an iterable object
+ * @param {number} [depth=1] The number specificying how many levels deep to flatten the *flattenable*
+ * @param {flattenable} flattenable An object with a `flat()` method
  * @returns {any}
  */
-module.exports = function flatten(iterable, depth=1) {
+module.exports = require('./curry2')(
 
-    return {
-        [Symbol.iterator] : function* () {
-
-            const shouldrecurse = (depth > 0);
-        
-            for(const item of iterable) {
-        
-                console.log(`flatten[${depth}]`, item);
-
-                if( shouldrecurse && isiterable(item) ) {
-        
-                    for( const subitem of flatten(item, depth-1) ) {
-                        console.log(`\t`, subitem)
-                        yield subitem;
-                    }
-
-                    continue;
-                }
-        
-                yield item;
-            }
-        }
-        
+    function flat(depth, flattenable) {
+        return flattenable.flat(depth);
     }
-}
+)
