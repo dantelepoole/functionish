3a3674d5038cbf7e513341149fdc9d8b6cd694ce
Dante le Poole <dantelepoole@gmail.com>
2022-05-28 19:10:38 -0400

    Refactor

--- linkedlist.js
+++ linkedlist.js
@@ -13,26 +13,34 @@ module.exports = linkedlist;
 
 function linkedlist(initialitems=[]) {
 
-    const nodelist = createnodelist(initialitems);
-    return linkedlistfromnodes(nodelist);
+    return linkedlistfromnodes(
+        createnodelist(initialitems)
+    )
 }
 
 function linkedlistfromnodes(nodelist) {
 
     return {
 
-        at         : partial(at, nodelist),
-        findnode   : partial(findnode, nodelist),
-        findindex  : partial(findindex, nodelist),
-        getnode    : partial(getnode, nodelist),
-        insert     : partial(insert, nodelist),
-        insertmany : partial(insertmany, nodelist),
-        peek       : partial(peek, nodelist),
-        pop        : partial(pop, nodelist),
-        push       : partial(push, nodelist),
-        pushmany   : partial(pushmany, nodelist),
-        remove     : partial(remove, nodelist),
-        reverse    : partial(reverse, nodelist),
+        at          : partial(at, nodelist),
+        clear       : partial(clear, nodelist),
+        filter      : partial(filter, nodelist),
+        findnode    : partial(findnode, nodelist),
+        findindex   : partial(findindex, nodelist),
+        getnode     : partial(getnode, nodelist),
+        insert      : partial(insert, nodelist),
+        insertmany  : partial(insertmany, nodelist),
+        map         : partial(map, nodelist),
+        peek        : partial(peek, nodelist),
+        pop         : partial(pop, nodelist),
+        push        : partial(push, nodelist),
+        pushmany    : partial(pushmany, nodelist),
+        reduce      : partial(reduce, nodelist),
+        remove      : partial(remove, nodelist),
+        reverse     : partial(reverse, nodelist),
+        shift       : partial(shift, nodelist),
+        unshift     : partial(unshift, nodelist),
+        unshiftmany : partial(unshiftmany, nodelist),
 
         get head() { return nodelist.head },
         get length() { return nodelist.length },
@@ -44,8 +52,14 @@ function linkedlistfromnodes(nodelist) {
     }
 }
 
-function at(llist, index) {
-    return getnode(llist, index)?.value;
+function at(nodelist, index) {
+    return getnode(nodelist, index)?.value;
+}
+
+function clear(nodelist) {
+
+    nodelist.head = nodelist.tail = NODE_NONE;
+    nodelist.length = 0;
 }
 
 function createnode(previousnode, value) {
@@ -87,9 +101,25 @@ function createnodelist(initialvalues=[]) {
     return nodelist;
 }
 
-function findnode(llist, predicate) {
+function filter(nodelist, predicate) {
+
+    function* filteriterate() {
+        
+        let currentnode = nodelist.head;
+
+        while(currentnode !== NODE_NONE) {
+            if( predicate(currentnode.value) ) yield currentnode.value;
+            currentnode = currentnode.next;
+        }
+    }
+
+    return linkedlist(filteriterate());
+
+}
+
+function findnode(nodelist, predicate) {
     
-    let currentnode = llist.head;
+    let currentnode = nodelist.head;
 
     while( currentnode !== NODE_NONE ) {
         if( predicate(currentnode.value) ) return currentnode;
@@ -99,13 +129,13 @@ function findnode(llist, predicate) {
     return NODE_NONE;
 }
 
-function findindex(llist, predicate) {
+function findindex(nodelist, predicate) {
 
-    let currentnode = llist.head;
+    let currentnode = nodelist.head;
     let index = 0;
 
     while( currentnode !== NODE_NONE ) {
-        if( predicate(currentnode) ) return index;
+        if( predicate(currentnode.value) ) return index;
         currentnode = currentnode.next;
         index += 1;
     }
@@ -113,11 +143,11 @@ function findindex(llist, predicate) {
     return INDEX_NOT_FOUND;
 }
 
-function getnode(llist, index) {
+function getnode(nodelist, index) {
 
-    if( index < 0 ) index = (llist.length + index);
+    if( index < 0 ) index = (nodelist.length + index);
 
-    let currentnode = llist.head;
+    let currentnode = nodelist.head;
 
     while( index > 0 && currentnode !== NODE_NONE ) {
         currentnode = currentnode.next;
@@ -133,24 +163,22 @@ function getnodelistiterator(nodelist) {
 
     return {
         next() {
-            if( currentnode.next === NODE_NONE ) return { done:true }
-
-            currentnode = currentnode.next;
-            return { done:false, value:currentnode.value }
+            currentnode = currentnode?.next;
+            return (currentnode === NODE_NONE) ? {done:true} : { done:false, value:currentnode.value }
         }
     }
 }
 
-function insert(llist, targetnode, value) {
+function insert(nodelist, targetnode, value) {
 
     const newnode = insertnode(targetnode?.previous, {value}, targetnode);
 
-    if( llist.head === targetnode ) llist.head = newnode;
-    if( llist.tail === NODE_NONE ) llist.tail = llist.head;
+    if( nodelist.head === targetnode ) nodelist.head = newnode;
+    if( nodelist.tail === NODE_NONE ) nodelist.tail = nodelist.head;
 
-    llist.length += 1;
+    nodelist.length += 1;
 
-    return llist.length;
+    return nodelist.length;
 }
 
 function insertnode(previousnode, newnode, nextnode) {
@@ -169,7 +197,7 @@ function insertnodelist(previousnode, newnodes, nextnode) {
     if( newnodes.length === 0 ) return newnodes;
 
     const headnode = newnodes.head;
-    const tailnode = newsnodes.tail;
+    const tailnode = newnodes.tail;
 
     headnode.previous = previousnode ?? nextnode?.previous;
     tailnode.next = nextnode ?? previousnode?.next;
@@ -180,7 +208,7 @@ function insertnodelist(previousnode, newnodes, nextnode) {
     return newnodes;
 }
 
-function insertmany(llist, targetnode, newvalues) {
+function insertmany(nodelist, targetnode, newvalues) {
 
     const newnodelist = createnodelist(newvalues);
 
@@ -188,66 +216,95 @@ function insertmany(llist, targetnode, newvalues) {
 
     insertnodelist(targetnode?.previous, newnodelist, targetnode);
 
-    if( llist.head === targetnode ) llist.head = newnodelist.head;
-    if( llist.tail === NODE_NONE ) llist.tail = llist.head;
+    if( nodelist.head === targetnode ) nodelist.head = newnodelist.head;
+    if( nodelist.tail === NODE_NONE ) nodelist.tail = nodelist.head;
 
-    llist.length += newnodelist.length;
+    nodelist.length += newnodelist.length;
 
-    return llist.length;
+    return nodelist.length;
 }
 
-function peek(llist) {
-    return llist.tail?.value;
+function map(nodelist, mapfunc) {
+
+    function* mapiterate() {
+        
+        let currentnode = nodelist.head;
+
+        while(currentnode !== NODE_NONE) {
+            yield mapfunc(currentnode.value);
+            currentnode = currentnode.next;
+        }
+    }
+
+    return linkedlist(mapiterate());
 }
 
-function pop(llist) {
+function peek(nodelist) {
+    return nodelist.tail?.value;
+}
 
-    const lastnode = llist.tail;
+function pop(nodelist) {
+
+    const lastnode = nodelist.tail;
 
     if( lastnode === NODE_NONE ) return undefined;
 
-    remove(llist, lastnode);
+    remove(nodelist, lastnode);
 
     return lastnode.value;
 }
 
-function push(llist, value) {
+function push(nodelist, value) {
 
-    llist.tail = createnode(llist.tail, value);
+    nodelist.tail = createnode(nodelist.tail, value);
 
-    llist.length += 1;
+    nodelist.length += 1;
 
-    return llist.length;
+    return nodelist.length;
 }
 
-function pushmany(llist, iterable) {
+function pushmany(nodelist, iterable) {
 
     const newnodes = createnodelist(iterable);
-    if( newnodes.length === 0 ) return;
+    if( newnodes.length === 0 ) return nodelist.length;
 
-    (llist.length === 0) ? (llist.head = newnodes.head) : (llist.tail.next = newnodes.head);
-    llist.tail = newnodes.tail;
-    llist.length = newnodes.length;
+    (nodelist.length === 0) ? (nodelist.head = newnodes.head) : (nodelist.tail.next = newnodes.head);
 
-    return llist.length;
+    nodelist.tail = newnodes.tail;
+    nodelist.length += newnodes.length;
+
+    return nodelist.length;
 }
 
-function remove(llist, targetnode) {
+function reduce(nodelist, reducer, initialvalue) {
+
+    let accumulator = initialvalue;
+    let currentnode = nodelist.head;
+
+    while( currentnode !== NODE_NONE ) {
+        accumulator = reducer(accumulator, currentnode.value);
+        currentnode = currentnode.next;
+    }
+
+    return accumulator;
+}
+
+function remove(nodelist, targetnode) {
 
     const nextnode = targetnode?.next;
     const previousnode = targetnode?.previous;
 
-    if( nextnode === NODE_NONE && previousnode === NODE_NONE ) return llist;
+    if( nextnode === NODE_NONE && previousnode === NODE_NONE ) return nodelist;
 
     targetnode.next = targetnode.previous = undefined;
 
     if( nextnode !== NODE_NONE ) nextnode.previous = previousnode;
     if( previousnode !== NODE_NONE ) previousnode.next = nextnode;
 
-    if( llist.head === targetnode ) llist.head = nextnode;
-    if( llist.tail === targetnode ) llist.tail = previousnode;
+    if( nodelist.head === targetnode ) nodelist.head = nextnode;
+    if( nodelist.tail === targetnode ) nodelist.tail = previousnode;
 
-    llist.length -= 1;
+    nodelist.length -= 1;
 
     return targetnode?.value;
 }
@@ -274,4 +331,29 @@ function reverse(nodelist) {
     newlist.length = nodelist.length;
 
     return linkedlistfromnodes(newlist);
+}
+
+function shift(nodelist) {
+
+    const firstnode = nodelist.head;
+
+    if( firstnode === NODE_NONE ) return undefined;
+
+    remove(nodelist, firstnode);
+
+    return firstnode.value;
+    
+}
+
+function unshift(nodelist, value) {
+
+    nodelist.head = insertnode(NODE_NONE, {value}, nodelist.head);
+
+    nodelist.length += 1;
+
+    return nodelist.length;
+}
+
+function unshiftmany(nodelist, iterable) {
+    return insertmany(nodelist, nodelist.head, iterable);
 }
