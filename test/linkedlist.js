f93628ee84164a938afa4a2379455509ddc35989
Dante le Poole <dantelepoole@gmail.com>
2022-05-28 20:52:13 -0400

    Initial import

---
+++ test/linkedlist.js
@@ -0,0 +1,13 @@
+const expect = require('chai').expect;
+const linkedlist = require('../linkedlist');
+
+describe('linkedlist()', function() {
+
+    beforeEach(
+        function() {
+        }
+    )
+
+    it('should be tested');
+
+})
