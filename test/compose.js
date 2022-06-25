03a1e2a28e77f347460027f0bcc1f5b8617088fb
Dante le Poole <dantelepoole@gmail.com>
2022-06-25 17:40:32 -0400

    Refactor

--- test/compose.js
+++ test/compose.js
@@ -1,20 +1,19 @@
 const compose = require('../compose');
 const expect = require('chai').expect;
 
-let callcount = 0;
-const invocations = [];
+const sandbox = require('sinon').createSandbox();
+const spy = sandbox.spy.bind(sandbox);
 
-function sum(a,b) { callcount += 1; invocations.push(sum); return (a+b) };
-function increment(x) { callcount += 1; invocations.push(increment); return (x+1) }
-function double(x) { callcount += 1; invocations.push(double); return (x*2) };
-function countargs(...args) { callcount += 1; invocations.push(countargs); return args.length }
+const sum = spy( function sum(a,b) { return (a+b) } )
+const increment = spy( function increment(x) { return (x+1) } )
+const double = spy( function double(x) { return (x*2) } )
+const countargs = spy( function countargs(...args) { return args.length } )
 
 describe('compose()', function() {
 
     beforeEach(
         function() {
-            callcount = 0;
-            invocations.length = 0;
+            sandbox.resetHistory();
         }
     )
 
@@ -25,36 +24,54 @@ describe('compose()', function() {
         }
     )
 
-    it('should invoke the composed functions in reverse order (right to left)',
-        function () {
-            const func = compose(double, increment, sum);
-            const result = func(1,2);
+    describe('the function returned by compose()', function() {
 
-            expect(invocations).to.be.deep.equal([sum,increment,double]);
-            expect(result).to.be.equal(8);
-        }
-    )
+        beforeEach(
+            function() {
+                sandbox.resetHistory();
+            }
+        )
 
-    it('should pass all its arguments to the first composed function',
-        function () {
-            const func = compose(countargs);
-            const result = func(1,2,3);
-            expect(result).to.be.equal(3);
-        }
-    )
+        it('should invoke the composed functions in reverse order (right to left)',
+            function () {
+                const func = compose(double, increment, sum);
+                const result = func(1,2);
 
-    it('should return its first argument if no functions are composed',
-        function () {
-            const func = compose();
-            const result = func(42, 84);
-            expect(result).to.be.equal(42);
-        }
-    )
+                expect(result).to.equal(8);
 
-    it('should return a function that throws if any of the composed functions are not functions',
-        function () {
-            const func = compose(sum, 42, double);
-            expect( ()=>func(1,2) ).to.throw();
-        }
-    )
+                expect( sum.calledImmediatelyBefore(increment) );
+                expect( sum.calledWith(1,2) );
+
+                expect( increment.calledImmediatelyBefore(double) );
+                expect( increment.calledWith(3) );
+
+                expect( double.calledImmediatelyAfter(increment) );
+                expect( double.calledWith(4) );
+            }
+        )
+
+        it('should pass all its arguments to the first composed function',
+            function () {
+                const func = compose(countargs);
+                const result = func(1,2,3);
+                expect(result).to.be.equal(3);
+            }
+        )
+
+        it('should return its first argument if no functions are composed',
+            function () {
+                const func = compose();
+                const result = func(42, 84);
+                expect(result).to.be.equal(42);
+            }
+        )
+
+        it('should throw if any of the composed functions are not functions',
+            function () {
+                const func = compose(sum, 42, double);
+                expect( ()=>func(1,2) ).to.throw();
+            }
+        )
+
+    })
 })
