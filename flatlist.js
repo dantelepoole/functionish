d834548bfd112aeffee5e01e9680bf5ad4985cb4
Dante le Poole <dantelepoole@gmail.com>
2022-06-26 00:44:55 -0400

    Add list() and flatlist()

---
+++ flatlist.js
@@ -0,0 +1,58 @@
+/**
+ * @module flatlist 
+ */
+'use strict';
+
+const ERR_BAD_LIST = `FlatListError~The list has type %s. Expected an iterable object.`;
+const ERR_BAD_DEPTH = `FlatListError~The depth is %s. Expected a positive integer number.`;
+
+const fail = require('./fail');
+const isinteger = require('./isinteger');
+const isnumber = require('./isnumber');
+const notiterable = require('./notiterable');
+const typeorclass = require('./typeorclass');
+
+const isvaliddepth = depth => (isinteger(depth) && (depth >= 0)) || (depth === Infinity);
+
+/**
+ * Recursively flatten the (iterable) items in the *list* to the specified *depth*. 
+ * 
+ * `flatlist()` is curried by default.
+ * 
+ * @func flatlist
+ * @param {number} depth The maximum recursion depth
+ * @param {iterable} list An iterable object
+ * @returns {iterable}
+ */
+module.exports = require('./curry2')(
+
+    function flatlist(depth, list) {
+
+        if( notiterable(list) ) fail(ERR_BAD_LIST, typeorclass(list));
+        if( ! isvaliddepth(depth) ) failbaddepth(depth);
+        
+        return {
+            [Symbol.iterator] : () => recursiveflat(depth, list)
+        }
+    }
+)
+
+function failbaddepth(depth) {
+
+    fail(
+        ERR_BAD_DEPTH,
+        isnumber(depth) ? String(depth) : `a ${typeorclass(depth)}`
+    )
+}
+
+function* recursiveflat(depth, iterable) {
+
+    const maxdepth = (depth <= 0);
+
+    for(const item of iterable) {
+
+        if(maxdepth || notiterable(item)) yield item;
+        else for(const subitem of recursiveflat(depth-1, item)) yield subitem;
+
+    }
+}
---
+++ list.js
@@ -0,0 +1,40 @@
+/**
+ * @module list 
+ */
+'use strict';
+
+const isiterable = require('./isiterable');
+
+/**
+ * Return an iterable object that produces the *items* in order. If an *item* itself is iterable, its items are
+ * injected individually, to a maximum recursion of 1.
+ * 
+ * @example
+ * 
+ * const list = require('functionish/list');
+ * 
+ * list(1,2,3); // returns an iterable that produces 1, 2 and 3
+ * 
+ * list([1,2,3]); // returns an iterable that produces 1, 2 and 3
+ * 
+ * list(1,2,[3,4,5]); //returns an iterable that produces 1,2,3,4 and 5
+ * 
+ * list(1,2,[[3,4,5]]); // returns an iterable that produces 1,2 and [3,4,5]
+ * 
+ * @func list
+ * @param {...any[]} items The items to list
+ * @returns {iterable}
+ */
+module.exports = function list(...items) {
+
+    return {
+        [Symbol.iterator] : function* () {
+
+            for(const item of items) {
+
+                if( isiterable(item ) ) for(const subitem of item) yield subitem;
+                else yield item;
+            }
+        }
+    }
+}
