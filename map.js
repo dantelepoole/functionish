a3d6dcc57267385392c6ab490678c381fab105d5
Dante le Poole <dantelepoole@gmail.com>
2022-06-25 22:23:48 -0400

    Refactor

--- map.js
+++ map.js
@@ -4,16 +4,24 @@
 
 'use strict';
 
+const ERR_BAD_MAPPABLE = `MapError~The mappable has type %s. Expected an object with a map() method or an iterable object.`;
+const ERR_BAD_MAPPING_FUNCTION = `MapError~The mapping function has type %s. Expected a function.`;
+
 const unary = require('./unary');
 
+const fail = require('./fail');
+const isiterable = require('./isiterable');
+const ismappable = obj => (typeof obj?.map === 'function');
+const typeorclass = require('./typeorclass');
+
 /**
- * Functional variant of {@link external:Array.prototype.map Array.prototype.map()}. Pass *func* to *list*'s
- * `map()`-method and return the result. If *list* does not have a `map()` method, it is assumed to be iterable and
- * an iterable object is returned that applies *func* to each item produced by *list*.
+ * Pass the *map* function to the `map()` method of *mappable* and return the result. If *mappable* has
+ * no such method but it is iterable, return an iterable object that applies *mapfunc* to each item produced by
+ * *mappable*.
  * 
- * *Important:* the *func* function is coerced to unary arity before it is passed to *list*'s `map()` method. This
- * means that *func* will only ever receive a single argument (the item being mapped), regardless of how many arguments
- * *list*'s `map()` method actually passes.
+ * *Important:* the *mapfunc* function is coerced to unary arity before it is passed to *mappable*'s `map()`
+ * method (if it exists). This means that *mapfunc* will only ever receive a single argument (the item being
+ * mapped), regardless of how many arguments *mappable*'s `map()` method actually passes.
  * 
  * `map()` is curried by default with binary arity.
  * 
@@ -26,26 +34,30 @@ const unary = require('./unary');
  * map(double, [1,2,3]); // returns [2,4,6]
  *     
  * @func map
- * @see {@link external:Array.prototype.map Array.prototype.map()}
  * @see {@link module:unary unary()}
  * @param {function} func The function to apply to each item in *list*
  * @param {(mappable|iterable)} list An object with a `map()` method or an iterable object
- * @returns {any} The return value of *list*'s `map()` method or an iterable object if it doesn't have one
+ * @returns {any}
  */
 
 module.exports = require('./curry2')(
 
-    function map(func, list) {
-        return (typeof list.map === 'function') ? list.map( unary(func) ) : mapiterable(func, list);
+    function map(mapfunc, mappable) {
+        
+        if(typeof mapfunc !== 'function') fail(ERR_BAD_MAPPING_FUNCTION, typeorclass(mapfunc));
+
+        return ismappable(mappable) ? mappable.map( unary(mapfunc) )
+             : isiterable(mappable) ? maplist(mapfunc, mappable)
+             : fail(ERR_BAD_MAPPABLE, typeorclass(mappable));
     }
 )
 
-function mapiterable(mapfunc, iterable) {
+function maplist(mapfunc, list) {
 
     return {
 
         [Symbol.iterator] : function* () {
-            for( const item of iterable ) yield mapfunc(item);
+            for( const item of list ) yield mapfunc(item);
         }
     }
 }
--- test/map.js
+++ test/map.js
@@ -1,159 +1,91 @@
 const expect = require('chai').expect;
-const isiterable = require('../isiterable');
 const map = require('../map');
+const isiterable = require('../isiterable');
+const range = require('../range');
 
-const markerobject = Object.freeze({});
-const markerarray = Object.freeze([]);
-const markersymbol = Symbol();
+const sandbox = require('sinon').createSandbox();
+const spy = sandbox.spy.bind(sandbox);
 
-function double(x) {
-    return x*2;
+const numbers1to5 = Object.freeze([1,2,3,4,5]);
+const double = spy( x => (x*2) );
+
+const isunaryinvocation = spy( 
+    function isunaryinvocation() {
+        expect(arguments.length).to.equal(1)
+    }
+)
+
+function spymappable(mappable) {
+
+    const object = {
+        map : mappable.map.bind(mappable)
+    }
+
+    spy(object, 'map');
+
+    return object;
 }
 
 describe(`map()`, function() {
 
     beforeEach(
         function() {
-
-        }
-    )
-
-    it(`should pass its first argument to the map() method of its second argument`,
-        function () {
-
-            let invoked;
-            function setinvoked(x) {
-                invoked = x;
-            }
-            const obj = {
-                map(func) {
-                    func(markerobject);
-                }
-            }
-
-            map(setinvoked, obj);
-            expectequal(invoked, markerobject);
-        }
-    )
-    
-    it(`should return an iterable if its second argument has no map() method but is iterable`,
-        function () {
-            const uppercase = str=>str.toUpperCase();
-            const result = map(uppercase, 'foobar');
-            expect( isiterable(result) ).to.be.true;
-            expect( Array.from(result) ).to.be.deep.equal( ['F','O','O','B','A','R'] );
-        }
-    )
-
-    it(`should ensure its first argument is only ever passed a single argument on each invocation`,
-        function () {
-            let invocationcount = 0;
-            function expectsingleargument(...args) {
-                invocationcount += 1;
-                expectequal( args.length, 1 );
-            }
-            const numbers1to10 = [1,2,3,4,5,6,7,8,9,10];
-            map(expectsingleargument, numbers1to10);
-            expectequal(invocationcount, 10);
-        }
-    )
-
-    it(`should invoke its first argument once for every item in its second argument`,
-        function () {
-            let invocationcount = 0;
-            function countinvocation() {
-                invocationcount += 1;
-            }
-            const numbers1to10 = [1,2,3,4,5,6,7,8,9,10];
-            map(countinvocation, numbers1to10);
-            expectequal(invocationcount, 10);
+            sandbox.resetHistory();
         }
     )
 
     it(`should be curried`,
         function () {
             const curried = map(double);
-            expectfunction(curried);
-
-            expectdeepequal(curried([42]), [84]);
+            expect( curried ).to.be.a('function');
+            expect( curried(numbers1to5) ).to.deep.equal([2,4,6,8,10]);
         }
     )
 
+    it(`should throw if the mapping function is not a function`,
+        function () {
+            expect( ()=>map({}, [1,2,3]) ).to.throw();
+        }
+    )
+
+    it(`should invoke the map()-method of the mappable, if it has one`,
+        function () {
+            const mappable = spymappable(numbers1to5);
+            const result = map(double, mappable);
+            expect(result).to.deep.equal([2,4,6,8,10]);
+            expect(mappable.map.callCount).to.equal(1);
+        }
+    )
+
+    it(`should return an iterable if the mappable does not have a map()-method but is iterable`,
+        function () {
+            const result = map(double, range(5));
+            expect( isiterable(result) ).to.be.true;
+            expect( Array.from(result) ).to.deep.equal([2,4,6,8,10]);
+        }
+    )
+
+    it(`should, if it iterates the mappable, call the mapping function for each item in the mappable`,
+        function () {
+            const result = map(double, range(10));
+            Array.from(result);
+            expect( double.callCount ).to.equal(10);
+        }
+    )
+
+    it(`should ensure that one and only one argument is passed to the mapping function`,
+        function () {
+            const mappable = spymappable(numbers1to5);
+            map(isunaryinvocation, mappable);
+            expect(isunaryinvocation.callCount).to.equal(5);
+        }
+    )
+
+    it(`if passed an array, it should return a new array with the mapped items`,
+        function () {
+            const result = map(double, numbers1to5);
+            expect(result).to.be.an('array').with.length(5);
+            expect(result).to.deep.equal([2,4,6,8,10]);
+        }
+    )
 })
-
-function countarguments(...args) {
-    return args.length;
-}
-
-function returnarguments(...args) {
-    return args;
-}
-
-function expecttothrow(func, ...args) {
-    expect( () => func(...args) ).to.throw();
-}
-
-function expectnottothrow(func, ...args) {
-    expect( () => func(...args) ).to.not.throw();
-}
-
-function expectequal(value1, value2) {
-    expect(value1).to.be.equal(value2);
-}
-
-function expectdeepequal(value1, value2) {
-    expect(value1).to.be.deep.equal(value2);
-}
-
-function expectnotequal(value1, value2) {
-    expect(value1).to.be.not.equal(value2);
-}
-
-function expectnotdeepequal(value1, value2) {
-    expect(value1).to.be.not.deep.equal(value2);
-}
-
-function expectclone(value1, value2) {
-    expect(value1).to.be.deep.equal(value2);
-    expect(value1).to.be.not.equal(value2);
-}
-
-function expectnull(value) {
-    expect(value).to.be.null;
-}
-
-function expectundefined(value) {
-    expect(value).to.be.undefined;
-}
-
-function expectnan(value) {
-    expect(value).to.be.NaN;
-}
-
-function expectfalsy( value ) {
-    expect( !! value ).to.be.false;
-}
-
-function expectfalse(value) {
-    expect(value).to.be.false;
-}
-
-function expecttrue(value) {
-    expect(value).to.be.true;
-}
-
-function expecttruthy(value) {
-    expect( !! value ).to.be.true;
-}
-
-function expecttype(type, value) {
-    expect(value).to.be.a(type);
-} 
-
-const expectarray = expecttype.bind(null, 'array');
-const expectfunction = expecttype.bind(null, 'function');
-const expectnumber = expecttype.bind(null, 'number');
-const expectstring = expecttype.bind(null, 'string');
-const expectobject = expecttype.bind(null, 'object');
-const expectboolean = expecttype.bind(null, 'boolean');
-const expectsymbol = expecttype.bind(null, 'symbol');
