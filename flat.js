c02a0230997c7c08925e6825314a8d169d23b63a
Dante le Poole <dantelepoole@gmail.com>
2022-06-26 01:14:13 -0400

    Refactor

--- flat.js
+++ flat.js
@@ -1,22 +1,36 @@
 /**
  * @module flatten 
  */
+
 'use strict';
 
+const ERR_BAD_FLATTENABLE = `FlatError~The flattenable has type %. Expect an object with a flat() method or an iterable object.`;
+
+const fail = require('./fail');
+const flatlist = require('./flatlist');
+const isiterable = require('./isiterable');
+const typeorclass = require('./typeorclass');
+
+const isflattenable = obj => (typeof obj?.flat === 'function');
+
 /**
- * Invoke the `flat()` method of *flattenable* with the specified depth and return the result.
+ * Pass the *depth* to *flattenable*'s `flat()` method and return the result. If *flattenable* has no such method but
+ * is iterable, return an iterable that flattens the *flattenable*'s items to the specified *depth*.
  * 
- * `flat()` is curried by default.
+ * `flat()` is curried by default with binary arity.
  * 
  * @func flat
  * @see {@link external:Array.prototype.flat Array.prototype.flat()}
- * @param {number} [depth=1] The number specificying how many levels deep to flatten the *flattenable*
- * @param {flattenable} flattenable An object with a `flat()` method
+ * @param {number} depth The maximum recursion level for flattening the *flattenable*
+ * @param {(flattenable|iterable)} flattenable An object with a `flat()` method or an iterable object
  * @returns {any}
  */
 module.exports = require('./curry2')(
 
     function flat(depth, flattenable) {
-        return flattenable.flat(depth);
+
+        return isflattenable(flattenable) ? flattenable.flat(depth)
+             : isiterable(flattenable) ? flatlist(depth, flattenable)
+             : fail(ERR_BAD_FLATTENABLE, typeorclass(flattenable));
     }
 )
--- test/flat.js
+++ test/flat.js
@@ -1,139 +1,83 @@
-const flat = require('../flat');
 const expect = require('chai').expect;
+const flat = require('../flat');
+const isiterable = require('../isiterable');
+const list = require('../list');
+const range = require('../range');
 
-const markerobject = Object.freeze({});
-const markerarray = Object.freeze([]);
-const markersymbol = Symbol();
+const sandbox = require('sinon').createSandbox();
+const spy = sandbox.spy.bind(sandbox);
 
-const array1dimension = [1,2,3,4,5];
-const array2dimensions = [[1,1], [2,2], [3,3], [4,4], [5,5]];
-const array3dimensions = [[[1,1]], [[2,2]], [[3,3]], [[4,4]], [[5,5]]];
+const array1dimension = Object.freeze([1,2,3,4,5,6,7,8,9,10]);
+const array2dimensions = Object.freeze([[1,2], [3,4], [5,6], [7,8], [9,10]]);
+const array3dimensions = Object.freeze([[[1,2]], [[3,4]], [[5,6]], [[7,8]], [[9,10]]]);
+
+function spyflattenable(flattenable) {
+
+    const object = {
+        flat : flattenable.flat.bind(flattenable)
+    }
+
+    spy(object, 'flat');
+
+    return object;
+}
 
 describe(`flat()`, function() {
 
     beforeEach(
         function() {
-
+            sandbox.resetHistory();
         }
     )
 
-    it(`should invoke the flat() method of its second argument with the first argument (depth)`,
-        function () {
-            let flatdepth = undefined;
-            const flattenable = {
-                flat(depth) {
-                    flatdepth = depth;
-                }
-            }
-            flat(42, flattenable);
-            expectequal(flatdepth, 42);
-        }
-    )
-
-    it(`should throw if its second argument has no flat() method`,
-        function () {
-            expecttothrow(flat, 42, {});
-        }
-    )
-
-    it(`if its second argument is an array, it should return a copy of the array flattened by depth number of dimensions`,
-        function () {
-            let result = flat(1, array1dimension);
-            expectdeepequal(result, array1dimension)
-
-            result = flat(1, array2dimensions);
-            expectdeepequal(result, [1,1,2,2,3,3,4,4,5,5]);
-
-            result = flat(1, array3dimensions);
-            expectdeepequal(result, array2dimensions);
-
-            result = flat(2, array3dimensions);
-            expectdeepequal(result, [1,1,2,2,3,3,4,4,5,5]);
-        }
-    )
-    
     it(`should be curried`,
         function () {
             const curried = flat(1);
-            expectfunction(curried);
-            expectdeepequal( curried(array2dimensions), [1,1,2,2,3,3,4,4,5,5] );
+            expect(curried).to.be.a('function');
+            expect(curried(array1dimension)).to.deep.equal(array1dimension);
+        }
+    )
+
+    it(`should pass the depth to *flattenable*'s flat() method if it has one`,
+        function () {
+            const flattenable = spyflattenable(array1dimension);
+            const result = flat(1, flattenable);
+            expect(result).to.deep.equal(array1dimension);
+            expect(flattenable.flat.called).to.be.true;
+        }
+    )
+
+    it(`should flatten the flattenable as an iterable if it has no flat() method`,
+        function () {
+            const result = flat(1, range(10));
+            expect( isiterable(result) ).to.be.true;
+            expect( Array.from(result) ).to.deep.equal(array1dimension);
+        }
+    )
+
+    it(`should throw if the flattenable has no flat() method and is not iterable`,
+        function () {
+            expect( ()=>flat(1, {}) ).to.throw();
+        }
+    )
+
+    it(`should, if flattenable has no flat() method but is iterable, throw if the depth is not a positive integer number or Infinity`,
+        function () {
+            expect( ()=>flat(-1, range(10)) ).to.throw();
+            expect( ()=>flat('foobar', range(10)) ).to.throw();
+            expect( ()=>flat(Infinity, range(10)) ).not.to.throw();
+        }
+    )
+
+    it(`should flatten to the specified number of dimensions`,
+        function () {
+            expect( flat(1, array2dimensions) ).to.deep.equal(array1dimension);
+            expect( flat(1, array3dimensions) ).to.deep.equal(array2dimensions);
+            expect( flat(2, array3dimensions) ).to.deep.equal(array1dimension);
+
+            expect( Array.from( flat(1, list(array2dimensions)) ) ).to.deep.equal(array1dimension);
+            expect( Array.from( flat(1, list(array3dimensions)) ) ).to.deep.equal(array2dimensions);
+            expect( Array.from( flat(2, list(array3dimensions)) ) ).to.deep.equal(array1dimension);
         }
     )
 })
-
-function countarguments(...args) {
-    return args.length;
-}
-
-function returnarguments(...args) {
-    return args;
-}
-
-function expecttothrow(func, ...args) {
-    expect( () => func(...args) ).to.throw();
-}
-
-function expectnottothrow(func, ...args) {
-    expect( () => func(...args) ).to.not.throw();
-}
-
-function expectequal(value1, value2) {
-    expect(value1).to.be.equal(value2);
-}
-
-function expectdeepequal(value1, value2) {
-    expect(value1).to.be.deep.equal(value2);
-}
-
-function expectnotequal(value1, value2) {
-    expect(value1).to.be.not.equal(value2);
-}
-
-function expectnotdeepequal(value1, value2) {
-    expect(value1).to.be.not.deep.equal(value2);
-}
-
-function expectclone(value1, value2) {
-    expect(value1).to.be.deep.equal(value2);
-    expect(value1).to.be.not.equal(value2);
-}
-
-function expectnull(value) {
-    expect(value).to.be.null;
-}
-
-function expectundefined(value) {
-    expect(value).to.be.undefined;
-}
-
-function expectnan(value) {
-    expect(value).to.be.NaN;
-}
-
-function expectfalsy( value ) {
-    expect( !! value ).to.be.false;
-}
-
-function expectfalse(value) {
-    expect(value).to.be.false;
-}
-
-function expecttrue(value) {
-    expect(value).to.be.true;
-}
-
-function expecttruthy(value) {
-    expect( !! value ).to.be.true;
-}
-
-function expecttype(type, value) {
-    expect(value).to.be.a(type);
-} 
-
-const expectarray = expecttype.bind(null, 'array');
-const expectfunction = expecttype.bind(null, 'function');
-const expectnumber = expecttype.bind(null, 'number');
-const expectstring = expecttype.bind(null, 'string');
-const expectobject = expecttype.bind(null, 'object');
-const expectboolean = expecttype.bind(null, 'boolean');
-const expectsymbol = expecttype.bind(null, 'symbol');
