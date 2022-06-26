58c55bf0674eacbe8aa2d53e5d8a1d569a5146bf
Dante le Poole <dantelepoole@gmail.com>
2022-06-26 18:33:49 -0400

    Refactor

--- head.js
+++ head.js
@@ -5,31 +5,20 @@
 'use strict';
 
 /**
- * Return the first item in *iterable* or `undefined` if *iterable* is empty.
+ * Return the first item in *indexable* or `undefined` if *indexable* is empty.
  * 
  * @example
  *     
  * const head = require('functionish/head');
  * 
  * head([1,2,3]); // returns 1
- * head(1); // returns 1
  * head([]); // returns `undefined`
  * head('foobar'); // returns 'f'
  * 
  * @func head
- * @param {iterable} iterable An iterable producing the items to retrieve the first item from.
+ * @param {indexable} indexable The indexable object to return the first item from.
  * @returns {any}
  */
-module.exports = function head(iterable) {
-
-    const firstitem = iterable?.[0];
-
-    return (firstitem !== undefined) ? firstitem : headiterable(iterable);
-}
-
-function headiterable(iterable) {
-
-    const firstitem = iterable[Symbol.iterator]().next();
-
-    return firstitem.done ? undefined : firstitem.value;
+module.exports = function head(indexable) {
+    return indexable?.[0];
 }
--- test/head.js
+++ test/head.js
@@ -1,11 +1,12 @@
 const expect = require('chai').expect;
 const head = require('../head');
 
-const markerobject = Object.freeze({});
-const markerarray = Object.freeze([]);
-const markersymbol = Symbol();
-
-const list = [42,24];
+const indexable = Object.freeze(
+    {
+        length : 1,
+        [0]    : 'foobar'
+    }
+)
 
 describe(`head()`, function() {
 
@@ -15,121 +16,26 @@ describe(`head()`, function() {
         }
     )
 
-    it(`should return the first item of iterable`,
+    it(`should return the first item of an indexable object`,
         function () {
-            expectequal( head(list), 42 );
+            expect( head([42]) ).to.equal(42);
+            expect( head('foobar') ).to.equal('f');
+            expect( head(indexable) ).to.equal('foobar');
         }
     )
 
-    it(`should return undefined if the iterable is empty`,
+    it(`should return undefined if the indexable is empty`,
         function () {
-            expectundefined( head([]) );
+            expect( head([]) ).to.be.undefined;
+            expect( head('') ).to.be.undefined;
         }
     )
 
-    it(`should return the value of iterable's property with the integer key 0 if the iterable is indexable`,
+    it(`should return undefined if the indexable is not indexable`,
         function () {
-            const obj = {}
-            obj[0] = 42;
-            expectequal( head(obj), 42 );
-        }
-    )
-
-    it(`should return the first value producdes by the iterable if the iterable is not indexable`,
-        function () {
-            expectequal( head('foobar'), 'f' );
-        }
-    )
-
-    it(`should throw if the object is not iterable`,
-        function () {
-            expecttothrow( head, null );
-            expecttothrow( head, undefined );
-            expecttothrow( head, {} );
-            expecttothrow( head, 42 );
-        }
-    )
-
-    it(`should throw if it is not passed an argument`,
-        function () {
-            expecttothrow( head );
+            expect( head() ).to.be.undefined;
+            expect( head(null) ).to.be.undefined;
+            expect( head({}) ).to.be.undefined;
         }
     )
 })
-
-function countarguments(...args) {
-    return args.length;
-}
-
-function returnarguments(...args) {
-    return args;
-}
-
-function expecttothrow(func, ...args) {
-    expect( () => func(...args) ).to.throw();
-}
-
-function expectnottothrow(func, ...args) {
-    expect( () => func(...args) ).to.not.throw();
-}
-
-function expectequal(value1, value2) {
-    expect(value1).to.be.equal(value2);
-}
-
-function expectdeepequal(value1, value2) {
-    expect(value1).to.be.deep.equal(value2);
-}
-
-function expectnotequal(value1, value2) {
-    expect(value1).to.be.not.equal(value2);
-}
-
-function expectnotdeepequal(value1, value2) {
-    expect(value1).to.be.not.deep.equal(value2);
-}
-
-function expectclone(value1, value2) {
-    expect(value1).to.be.deep.equal(value2);
-    expect(value1).to.be.not.equal(value2);
-}
-
-function expectnull(value) {
-    expect(value).to.be.null;
-}
-
-function expectundefined(value) {
-    expect(value).to.be.undefined;
-}
-
-function expectnan(value) {
-    expect(value).to.be.NaN;
-}
-
-function expectfalsy( value ) {
-    expect( !! value ).to.be.false;
-}
-
-function expectfalse(value) {
-    expect(value).to.be.false;
-}
-
-function expecttrue(value) {
-    expect(value).to.be.true;
-}
-
-function expecttruthy(value) {
-    expect( !! value ).to.be.true;
-}
-
-function expecttype(type, value) {
-    expect(value).to.be.a(type);
-} 
-
-const expectarray = expecttype.bind(null, 'array');
-const expectfunction = expecttype.bind(null, 'function');
-const expectnumber = expecttype.bind(null, 'number');
-const expectstring = expecttype.bind(null, 'string');
-const expectobject = expecttype.bind(null, 'object');
-const expectboolean = expecttype.bind(null, 'boolean');
-const expectsymbol = expecttype.bind(null, 'symbol');
