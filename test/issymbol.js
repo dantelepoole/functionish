bb42d8feef034c73a781fa801af3940450743f80
Dante le Poole <dantelepoole@gmail.com>
2022-06-28 12:48:04 -0400

    Refactor

--- test/issymbol.js
+++ test/issymbol.js
@@ -1,10 +1,6 @@
 const expect = require('chai').expect;
 const issymbol = require('../issymbol');
 
-const markerobject = Object.freeze({});
-const markerarray = Object.freeze([]);
-const markersymbol = Symbol();
-
 describe(`issymbol()`, function() {
 
     beforeEach(
@@ -15,98 +11,20 @@ describe(`issymbol()`, function() {
 
     it(`should return true if its argument has type symbol`,
         function () {
-            expecttrue( issymbol(markersymbol) );
-            expecttrue( issymbol( Symbol() ) );
-            expecttrue( issymbol( Symbol.for('foobar') ) );
+            expect( issymbol( Symbol() ) ).to.be.true;
+            expect( issymbol( Symbol.for('foobar') ) ).to.be.true;
         }
     )
 
     it(`should return false if its argument has any other type`,
         function () {
-            expectfalse( issymbol(null) );
-            expectfalse( issymbol(undefined) );
-            expectfalse( issymbol(markerobject) );
-            expectfalse( issymbol(markerarray) );
-            expectfalse( issymbol(42) );
-            expectfalse( issymbol('foobar') );
-            expectfalse( issymbol(true) );
+            expect( issymbol(null) ).to.be.false;
+            expect( issymbol(undefined) ).to.be.false;
+            expect( issymbol({}) ).to.be.false;
+            expect( issymbol([]) ).to.be.false;
+            expect( issymbol(42) ).to.be.false;
+            expect( issymbol('foobar') ).to.be.false;
+            expect( issymbol(true) ).to.be.false;
         }
     )
 })
-
-function countarguments(...args) {
-    return args.length;
-}
-
-function returnarguments(...args) {
-    return args;
-}
-
-function expecttothrow(func, ...args) {
-    expect( () => func(...args) ).to.throw();
-}
-
-function expectnottothrow(func, ...args) {
-    expect( () => func(...args) ).to.not.throw();
-}
-
-function expectequal(value1, value2) {
-    expect(value1).to.be.equal(value2);
-}
-
-function expectdeepequal(value1, value2) {
-    expect(value1).to.be.deep.equal(value2);
-}
-
-function expectnotequal(value1, value2) {
-    expect(value1).to.be.not.equal(value2);
-}
-
-function expectnotdeepequal(value1, value2) {
-    expect(value1).to.be.not.deep.equal(value2);
-}
-
-function expectclone(value1, value2) {
-    expect(value1).to.be.deep.equal(value2);
-    expect(value1).to.be.not.equal(value2);
-}
-
-function expectnull(value) {
-    expect(value).to.be.null;
-}
-
-function expectundefined(value) {
-    expect(value).to.be.undefined;
-}
-
-function expectnan(value) {
-    expect(value).to.be.NaN;
-}
-
-function expectfalsy( value ) {
-    expect( !! value ).to.be.false;
-}
-
-function expectfalse(value) {
-    expect(value).to.be.false;
-}
-
-function expecttrue(value) {
-    expect(value).to.be.true;
-}
-
-function expecttruthy(value) {
-    expect( !! value ).to.be.true;
-}
-
-function expecttype(type, value) {
-    expect(value).to.be.a(type);
-} 
-
-const expectarray = expecttype.bind(null, 'array');
-const expectfunction = expecttype.bind(null, 'function');
-const expectnumber = expecttype.bind(null, 'number');
-const expectstring = expecttype.bind(null, 'string');
-const expectobject = expecttype.bind(null, 'object');
-const expectboolean = expecttype.bind(null, 'boolean');
-const expectsymbol = expecttype.bind(null, 'symbol');
