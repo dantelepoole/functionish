fb5b85493f250ca69255cf63eaa51c0734b97d40
Dante le Poole <dantelepoole@gmail.com>
2022-06-27 22:01:48 -0400

    Refactor

--- test/islessthan.js
+++ test/islessthan.js
@@ -1,10 +1,6 @@
 const expect = require('chai').expect;
 const islessthan = require('../islessthan');
 
-const markerobject = Object.freeze({});
-const markerarray = Object.freeze([]);
-const markersymbol = Symbol();
-
 describe(`islessthan()`, function() {
 
     beforeEach(
@@ -16,149 +12,73 @@ describe(`islessthan()`, function() {
     it(`should be curried`,
         function () {
             const curried = islessthan(0);
-            expectfunction(curried);
-            expecttrue( curried(1) );
+            expect(curried).to.be.a('function');
+            expect( curried(1) ).not.to.be.a('function');
+            expect( curried(1) ).to.be.true;
         }
     )
 
     it(`should return true if its first argument is numerically less than its second argument`,
         function () {
-            expecttrue( islessthan(0, 1) );
-            expecttrue( islessthan(1, 1.1) );
-            expecttrue( islessthan(-1, 0) );
-            expecttrue( islessthan(0.99, 1) );
+            expect( islessthan(0, 1) ).to.be.true;
+            expect( islessthan(1, 1.1) ).to.be.true;
+            expect( islessthan(-1, 0) ).to.be.true;
+            expect( islessthan(0.99, 1) ).to.be.true;
         }
     )
 
     it(`should return work with bigints for either or both arguments`,
         function () {
-            expecttrue( islessthan(0n, 1n) );
-            expecttrue( islessthan(-1n, 0n) );
-            expecttrue( islessthan(0, 1n) );
-            expecttrue( islessthan(39.45, 42n) );
+            expect( islessthan(0n, 1n) ).to.be.true;
+            expect( islessthan(-1n, 0n) ).to.be.true;
+            expect( islessthan(0, 1n) ).to.be.true;
+            expect( islessthan(39.45, 42n) ).to.be.true;
         }
     )
 
     it(`should return false if its first argument is numerically less than its second argument`,
         function () {
-            expectfalse( islessthan(1, 0) );
-            expectfalse( islessthan(1.1, 1) );
-            expectfalse( islessthan(0, -1) );
-            expectfalse( islessthan(1, 0.99) );
-            expectfalse( islessthan(0,0) );
-            expectfalse( islessthan(1.0, 1) );
-            expectfalse( islessthan(1.0, 1.0) );
-            expectfalse( islessthan(1n, 1n) );
-            expectfalse( islessthan(2n, 1n) );
+            expect( islessthan(1, 0) ).to.be.false;
+            expect( islessthan(1.1, 1) ).to.be.false;
+            expect( islessthan(0, -1) ).to.be.false;
+            expect( islessthan(1, 0.99) ).to.be.false;
+            expect( islessthan(0,0) ).to.be.false;
+            expect( islessthan(1.0, 1) ).to.be.false;
+            expect( islessthan(1.0, 1.0) ).to.be.false;
+            expect( islessthan(1n, 1n) ).to.be.false;
+            expect( islessthan(2n, 1n) ).to.be.false;
         }
     )
 
     it(`should return true if both arguments are strings and the first argument is alphanumerically less than the second argument`,
         function () {
-            expecttrue( islessthan('1', 'b') );
-            expecttrue( islessthan('a', 'aa') );
-            expecttrue( islessthan('8', '9') );
-            expecttrue( islessthan('', 'a') );
+            expect( islessthan('1', 'b') ).to.be.true;
+            expect( islessthan('a', 'aa') ).to.be.true;
+            expect( islessthan('8', '9') ).to.be.true;
+            expect( islessthan('', 'a') ).to.be.true;
         }
     )
 
     it(`should return convert strings to numbers if possible`,
         function () {
-            expecttrue( islessthan(1, '8') );
-            expectfalse( islessthan('8', 1) );
+            expect( islessthan(1, '8') ).to.be.true;
+            expect( islessthan('8', 1) ).to.be.false;
         }
     )
 
 
     it(`should return the same result as the Javascript >-operator`,
         function () {
-            expectequal( islessthan('b', 1), ('b' < 1) );
-            expectequal( islessthan(1, 'b'), (1 < 'b') );
-            expectequal( islessthan([], '1'), ([] < '1') );
-            expectequal( islessthan('1', []), ('1' < []) );
-            expectequal( islessthan(null, 0), (null < 0) );
-            expectequal( islessthan(0, null), (0 < null) );
-            expectequal( islessthan(NaN, NaN), (NaN < NaN) );
-            expectequal( islessthan(undefined, null), (undefined < null) );
-            expectequal( islessthan(null, undefined), (null < undefined) );
+            expect( islessthan('b', 1) ).to.equal( ('b' < 1) );
+            expect( islessthan(1, 'b') ).to.equal( (1 < 'b') );
+            expect( islessthan([], '1') ).to.equal( ([] < '1') );
+            expect( islessthan('1', []) ).to.equal( ('1' < []) );
+            expect( islessthan(null, 0) ).to.equal( (null < 0) );
+            expect( islessthan(0, null) ).to.equal( (0 < null) );
+            expect( islessthan(NaN, NaN) ).to.equal( (NaN < NaN) );
+            expect( islessthan(undefined, null) ).to.equal( (undefined < null) );
+            expect( islessthan(null, undefined) ).to.equal( (null < undefined) );
         }
     )
 
 })
-
-function countarguments(...args) {
-    return args.length;
-}
-
-function returnarguments(...args) {
-    return args;
-}
-
-function expecttothrow(func, ...args) {
-    expect( () => func(...args) ).to.throw();
-}
-
-function expectnottothrow(func, ...args) {
-    expect( () => func(...args) ).to.not.throw();
-}
-
-function expectequal(value1, value2) {
-    expect(value1).to.be.equal(value2);
-}
-
-function expectdeepequal(value1, value2) {
-    expect(value1).to.be.deep.equal(value2);
-}
-
-function expectnotequal(value1, value2) {
-    expect(value1).to.be.not.equal(value2);
-}
-
-function expectnotdeepequal(value1, value2) {
-    expect(value1).to.be.not.deep.equal(value2);
-}
-
-function expectclone(value1, value2) {
-    expect(value1).to.be.deep.equal(value2);
-    expect(value1).to.be.not.equal(value2);
-}
-
-function expectnull(value) {
-    expect(value).to.be.null;
-}
-
-function expectundefined(value) {
-    expect(value).to.be.undefined;
-}
-
-function expectnan(value) {
-    expect(value).to.be.NaN;
-}
-
-function expectfalsy( value ) {
-    expect( !! value ).to.be.false;
-}
-
-function expectfalse(value) {
-    expect(value).to.be.false;
-}
-
-function expecttrue(value) {
-    expect(value).to.be.true;
-}
-
-function expecttruthy(value) {
-    expect( !! value ).to.be.true;
-}
-
-function expecttype(type, value) {
-    expect(value).to.be.a(type);
-} 
-
-const expectarray = expecttype.bind(null, 'array');
-const expectfunction = expecttype.bind(null, 'function');
-const expectnumber = expecttype.bind(null, 'number');
-const expectstring = expecttype.bind(null, 'string');
-const expectobject = expecttype.bind(null, 'object');
-const expectboolean = expecttype.bind(null, 'boolean');
-const expectsymbol = expecttype.bind(null, 'symbol');
--- test/islessthanorequal.js
+++ test/islessthanorequal.js
@@ -1,10 +1,6 @@
 const expect = require('chai').expect;
 const islessthanorequal = require('../islessthanorequal');
 
-const markerobject = Object.freeze({});
-const markerarray = Object.freeze([]);
-const markersymbol = Symbol();
-
 describe(`islessthatorequal()`, function() {
 
     beforeEach(
@@ -16,148 +12,72 @@ describe(`islessthatorequal()`, function() {
     it(`should be curried`,
         function () {
             const curried = islessthanorequal(0);
-            expectfunction(curried);
-            expecttrue( curried(1) );
+            expect(curried).to.be.a('function');
+            expect( curried(1) ).not.to.be.a('function');
+            expect( curried(1) ).to.be.true;
         }
     )
 
     it(`should return true if its first argument is numerically greater than its second argument`,
         function () {
-            expecttrue( islessthanorequal(0, 1) );
-            expecttrue( islessthanorequal(1, 1.1) );
-            expecttrue( islessthanorequal(-1, 0) );
-            expecttrue( islessthanorequal(0.99, 1) );
+            expect( islessthanorequal(0, 1) ).to.be.true;
+            expect( islessthanorequal(1, 1.1) ).to.be.true;
+            expect( islessthanorequal(-1, 0) ).to.be.true;
+            expect( islessthanorequal(0.99, 1) ).to.be.true;
         }
     )
 
     it(`should return work with bigints for either or both arguments`,
         function () {
-            expecttrue( islessthanorequal(0n, 1n) );
-            expecttrue( islessthanorequal(-1n, 0n) );
-            expecttrue( islessthanorequal(0, 1n) );
-            expecttrue( islessthanorequal(39.45, 42n) );
+            expect( islessthanorequal(0n, 1n) ).to.be.true;
+            expect( islessthanorequal(-1n, 0n) ).to.be.true;
+            expect( islessthanorequal(0, 1n) ).to.be.true;
+            expect( islessthanorequal(39.45, 42n) ).to.be.true;
         }
     )
 
     it(`should return true if its first argument is numerically less than or equal to its second argument`,
         function () {
-            expecttrue( islessthanorequal(0, 1) );
-            expecttrue( islessthanorequal(1, 1.1) );
-            expecttrue( islessthanorequal(-1, 0) );
-            expecttrue( islessthanorequal(0.99, 1) );
-            expecttrue( islessthanorequal(0,0) );
-            expecttrue( islessthanorequal(1.0, 1) );
-            expecttrue( islessthanorequal(1.0, 1.0) );
-            expecttrue( islessthanorequal(1n, 1n) );
-            expecttrue( islessthanorequal(1n, 2n) );
+            expect( islessthanorequal(0, 1) ).to.be.true;
+            expect( islessthanorequal(1, 1.1) ).to.be.true;
+            expect( islessthanorequal(-1, 0) ).to.be.true;
+            expect( islessthanorequal(0.99, 1) ).to.be.true;
+            expect( islessthanorequal(0,0) ).to.be.true;
+            expect( islessthanorequal(1.0, 1) ).to.be.true;
+            expect( islessthanorequal(1.0, 1.0) ).to.be.true;
+            expect( islessthanorequal(1n, 1n) ).to.be.true;
+            expect( islessthanorequal(1n, 2n) ).to.be.true;
         }
     )
 
     it(`should return true if both arguments are strings and the first argument is alphanumerically less than or equal to the second argument`,
         function () {
-            expecttrue( islessthanorequal('1', 'b') );
-            expecttrue( islessthanorequal('a', 'aa') );
-            expecttrue( islessthanorequal('8', '9') );
-            expecttrue( islessthanorequal('', 'a') );
+            expect( islessthanorequal('1', 'b') ).to.be.true;
+            expect( islessthanorequal('a', 'aa') ).to.be.true;
+            expect( islessthanorequal('8', '9') ).to.be.true;
+            expect( islessthanorequal('', 'a') ).to.be.true;
         }
     )
 
     it(`should return convert strings to numbers if possible`,
         function () {
-            expecttrue( islessthanorequal(1, '8') );
-            expectfalse( islessthanorequal('8', 1) );
+            expect( islessthanorequal(1, '8') ).to.be.true;
+            expect( islessthanorequal('8', 1) ).to.be.false;
         }
     )
 
 
     it(`should return the same result as the Javascript >-operator`,
         function () {
-            expectequal( islessthanorequal('b', 1), ('b' <= 1) );
-            expectequal( islessthanorequal(1, 'b'), (1 <= 'b') );
-            expectequal( islessthanorequal([], '1'), ([] <= '1') );
-            expectequal( islessthanorequal('1', []), ('1' <= []) );
-            expectequal( islessthanorequal(null, 0), (null <= 0) );
-            expectequal( islessthanorequal(0, null), (0 <= null) );
-            expectequal( islessthanorequal(NaN, NaN), (NaN <= NaN) );
-            expectequal( islessthanorequal(undefined, null), (undefined <= null) );
-            expectequal( islessthanorequal(null, undefined), (null <= undefined) );
+            expect( islessthanorequal('b', 1) ).to.equal( ('b' <= 1) );
+            expect( islessthanorequal(1, 'b') ).to.equal( (1 <= 'b') );
+            expect( islessthanorequal([], '1') ).to.equal( ([] <= '1') );
+            expect( islessthanorequal('1', []) ).to.equal( ('1' <= []) );
+            expect( islessthanorequal(null, 0) ).to.equal( (null <= 0) );
+            expect( islessthanorequal(0, null) ).to.equal( (0 <= null) );
+            expect( islessthanorequal(NaN, NaN) ).to.equal( (NaN <= NaN) );
+            expect( islessthanorequal(undefined, null) ).to.equal( (undefined <= null) );
+            expect( islessthanorequal(null, undefined) ).to.equal( (null <= undefined) );
         }
     )
 })
-
-function countarguments(...args) {
-    return args.length;
-}
-
-function returnarguments(...args) {
-    return args;
-}
-
-function expecttothrow(func, ...args) {
-    expect( () => func(...args) ).to.throw();
-}
-
-function expectnottothrow(func, ...args) {
-    expect( () => func(...args) ).to.not.throw();
-}
-
-function expectequal(value1, value2) {
-    expect(value1).to.be.equal(value2);
-}
-
-function expectdeepequal(value1, value2) {
-    expect(value1).to.be.deep.equal(value2);
-}
-
-function expectnotequal(value1, value2) {
-    expect(value1).to.be.not.equal(value2);
-}
-
-function expectnotdeepequal(value1, value2) {
-    expect(value1).to.be.not.deep.equal(value2);
-}
-
-function expectclone(value1, value2) {
-    expect(value1).to.be.deep.equal(value2);
-    expect(value1).to.be.not.equal(value2);
-}
-
-function expectnull(value) {
-    expect(value).to.be.null;
-}
-
-function expectundefined(value) {
-    expect(value).to.be.undefined;
-}
-
-function expectnan(value) {
-    expect(value).to.be.NaN;
-}
-
-function expectfalsy( value ) {
-    expect( !! value ).to.be.false;
-}
-
-function expectfalse(value) {
-    expect(value).to.be.false;
-}
-
-function expecttrue(value) {
-    expect(value).to.be.true;
-}
-
-function expecttruthy(value) {
-    expect( !! value ).to.be.true;
-}
-
-function expecttype(type, value) {
-    expect(value).to.be.a(type);
-} 
-
-const expectarray = expecttype.bind(null, 'array');
-const expectfunction = expecttype.bind(null, 'function');
-const expectnumber = expecttype.bind(null, 'number');
-const expectstring = expecttype.bind(null, 'string');
-const expectobject = expecttype.bind(null, 'object');
-const expectboolean = expecttype.bind(null, 'boolean');
-const expectsymbol = expecttype.bind(null, 'symbol');
