8319629bf776916a2959cb975025f2f5eff52335
Dante le Poole <dantelepoole@gmail.com>
2022-06-28 13:59:13 -0400

    Refactor

--- lift.js
+++ lift.js
@@ -4,15 +4,12 @@
 
 'use strict';
 
-const isdefined = require('./isdefined');
-
 /**
- * Return an array containing *value* as its only item. If *value* is `undefined` or `null` an empty array is
- * returned.
+ * Return an array containing *value* as its only item. If *value* is `undefined` an empty array is returned.
  * 
  * @param {any} value 
  * @returns {any[]}
  */
 module.exports = function lift(value) {
-    return isdefined(value) ? [value] : [];
+    return (value === undefined) ? [] : [value];
 }
