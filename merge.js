2dd830e115d6e28fa23e384f15e61799ad9450f1
Dante le Poole <dantelepoole@gmail.com>
2022-06-28 15:53:07 -0400

    Refactor

--- merge.js
+++ merge.js
@@ -8,7 +8,10 @@ const assign = Object.assign;
 /**
  * Merge the own, enumerable properties of all *source* objects into a new, single object.
  * 
- * This function uses {@link external:Object.assign Object.assign()} to merge the source objects.
+ * This function uses {@link external:Object.assign Object.assign()} to merge the source objects. This means that the
+ * properties of later *sources* will override properties of earlier *sources* that have the same key.
+ * 
+ * If no *sources* are passed, an empty object is returned.
  * 
  * @func merge
  * @param  {...object} sources Two or more objects to merge
--- test/merge.js
+++ test/merge.js
@@ -1,10 +1,6 @@
 const expect = require('chai').expect;
 const merge = require('../merge');
 
-const markerobject = Object.freeze({});
-const markerarray = Object.freeze([]);
-const markersymbol = Symbol();
-
 describe(`merge()`, function() {
 
     beforeEach(
@@ -13,62 +9,65 @@ describe(`merge()`, function() {
         }
     )
 
-    it(`should return a new object that contains copies of all own, enumerable properties of each argument`,
+    it(`should return a new object that contains copies of all own, enumerable properties of each source`,
         function () {
             const obj1 = { a:'a', b:'b' };
             const obj2 = { c:'c', d:'d' };
             const merged = merge(obj1, obj2);
 
-            expectdeepequal( ['a','b','c','d'], Object.keys(merged) )
-            expectequal( 'a', merged.a );
-            expectequal( 'b', merged.b );
-            expectequal( 'c', merged.c );
-            expectequal( 'd', merged.d );
+            expect( ['a','b','c','d'], Object.keys(merged) ).to.deep.equal(Object.keys(merged));
+            expect('a').to.equal(merged.a);
+            expect('b').to.equal(merged.b);
+            expect('c').to.equal(merged.c);
+            expect('d').to.equal(merged.d);
 
-            expectnotequal(merged, obj1);
-            expectnotequal(merged, obj2);
+            expect(merged, obj1).not.to.be.equal(obj1);
+            expect(merged, obj2).not.to.be.equal(obj2);
         }
     )
 
-    it(`if multiple argument have a property with the same key, it should copy the property from the last argument`,
+    it(`if multiple sources have a property with the same key, it should copy the property from the last source`,
         function () {
             const obj1 = { a:'a', b:'b' };
             const obj2 = { a: 'aa', c:'c', d:'d' };
             const merged = merge(obj1, obj2);
 
-            expectdeepequal( ['a','b','c','d'], Object.keys(merged) )
-            expectequal( 'aa', merged.a );
-            expectequal( 'b', merged.b );
-            expectequal( 'c', merged.c );
-            expectequal( 'd', merged.d );
+            expect( ['a','b','c','d'] ).to.deep.equal( Object.keys(merged) );
+            expect( 'aa' ).to.equal( merged.a );
+            expect( 'b' ).to.equal( merged.b );
+            expect( 'c' ).to.equal( merged.c );
+            expect( 'd' ).to.equal( merged.d );
 
         }
     )
 
-    it(`should make a shallow copy of the arguments' properties`,
+    it(`should make a shallow copy of the sources' properties`,
         function () {
-            const obj1 = { a:'a', b:markerobject };
+            const sentinel = {};
+
+            const obj1 = { a:'a', b:sentinel };
             const obj2 = { c:'c', d:'d' };
             const merged = merge(obj1, obj2);
 
-            expectequal( obj1.b, merged.b );
+            expect( obj1.b ).equal( merged.b );
             
         }
     )
 
-    it(`if passed a single argument, it should return a shallow copy of it`,
+    it(`if passed a single source, it should return a shallow copy of it`,
         function () {
-            const obj1 = { a:'a', b:markerobject };
+            const sentinel = {};
+            const obj1 = { a:'a', b:sentinel };
             const merged = merge(obj1);
 
-            expectdeepequal( obj1, merged );
-            expectequal(obj1.a, merged.a);
-            expectequal( obj1.b, merged.b );
+            expect( obj1 ).to.deep.equal( merged );
+            expect(obj1.a).to.equal(merged.a);
+            expect( obj1.b).to.equal(merged.b );
             
         }
     )
 
-    it(`should only copy own properties from the arguments`,
+    it(`should only copy own properties from the sources`,
         function () {
             const proto = { a:'a'};
             const obj = Object.create(proto);
@@ -76,99 +75,35 @@ describe(`merge()`, function() {
 
             const merged = merge(obj);
 
-            expectequal( 1, Object.keys(merged).length );
-            expectundefined( merged.a );
-            expectequal( 'b', merged.b );
+            expect( 1 ).to.equal( Object.keys(merged).length );
+            expect( merged.a ).to.be.undefined;
+            expect( 'b' ).to.equal( merged.b );
         }
     )
 
-    it(`should only copy enumerable properties from the arguments`,
+    it(`should only copy enumerable properties from the sources`,
         function () {
             obj = { a:'a' };
             Object.defineProperty(obj, 'b', {value:'b', enumerable:false});
             const merged = merge(obj);
 
-            expectequal( 1, Object.keys(merged).length );
-            expectundefined( merged.b );
-            expectequal( 'a', merged.a );
+            expect( 1 ).to.equal( Object.keys(merged).length );
+            expect( merged.b ).to.be.undefined;
+            expect( 'a' ).to.equal( merged.a );
             
         }
     )
+
+    it(`should return an empty object if no sources are passed`,
+        function () {
+            expect( merge() ).to.deep.equal({});
+        }
+    )
+
+    it(`should return an empty object if null or undefined is passed`,
+        function () {
+            expect( merge(null) ).to.deep.equal({});
+            expect( merge(undefined) ).to.deep.equal({});
+        }
+    )
 })
-
-function countarguments(...args) {
-    return args.length;
-}
-
-function returnarguments(...args) {
-    return args;
-}
-
-function expecttothrow(func, ...args) {
-    expect( () => func(...args) ).to.throw();
-}
-
-function expectnottothrow(func, ...args) {
-    expect( () => func(...args) ).to.not.throw();
-}
-
-function expectequal(value1, value2) {
-    expect(value1).to.be.equal(value2);
-}
-
-function expectdeepequal(value1, value2) {
-    expect(value1).to.be.deep.equal(value2);
-}
-
-function expectnotequal(value1, value2) {
-    expect(value1).to.be.not.equal(value2);
-}
-
-function expectnotdeepequal(value1, value2) {
-    expect(value1).to.be.not.deep.equal(value2);
-}
-
-function expectclone(value1, value2) {
-    expect(value1).to.be.deep.equal(value2);
-    expect(value1).to.be.not.equal(value2);
-}
-
-function expectnull(value) {
-    expect(value).to.be.null;
-}
-
-function expectundefined(value) {
-    expect(value).to.be.undefined;
-}
-
-function expectnan(value) {
-    expect(value).to.be.NaN;
-}
-
-function expectfalsy( value ) {
-    expect( !! value ).to.be.false;
-}
-
-function expectfalse(value) {
-    expect(value).to.be.false;
-}
-
-function expecttrue(value) {
-    expect(value).to.be.true;
-}
-
-function expecttruthy(value) {
-    expect( !! value ).to.be.true;
-}
-
-function expecttype(type, value) {
-    expect(value).to.be.a(type);
-} 
-
-const expectarray = expecttype.bind(null, 'array');
-const expectfunction = expecttype.bind(null, 'function');
-const expectnumber = expecttype.bind(null, 'number');
-const expectstring = expecttype.bind(null, 'string');
-const expectobject = expecttype.bind(null, 'object');
-const expectboolean = expecttype.bind(null, 'boolean');
-const expectsymbol = expecttype.bind(null, 'symbol');
