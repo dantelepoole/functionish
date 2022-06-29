cd808e116cec0b223d728ae8df22d54fe3fc64c6
Dante le Poole <dantelepoole@gmail.com>
2022-06-29 01:24:02 -0400

    Refactor

--- range.js
+++ range.js
@@ -4,6 +4,18 @@
 
 'use strict';
 
+const STEP_DECREMENT = -1;
+const STEP_INCREMENT = 1;
+
+const ERR_BAD_RANGE = `RangeError~The range %s %s. Expected an integer number.`;
+const ERR_BAD_RANGECOUNT = `RangeError~The range count %s. Expected a positive integer number.`;
+
+const fail = require('./fail');
+const notinteger = require('./notinteger');
+const typeorclass = require('./typeorclass');
+
+const notpositiveinteger = x => notinteger(x) || (x < 0);
+
 /**
  * Return an iterable that generates the integers from 1 to *count* (inclusive). If *count* is negative or not a number,
  * the returned iterable produces nothing.
@@ -12,12 +24,30 @@
  * @param {number} count The maximum number to generate
  * @returns {iterable}
  */
-module.exports = function* range(count) {
+ module.exports = function* range(start, end) {
 
-    let counter = 1;
+    if( arguments.length === 1 ) {
 
-    while( counter <= count ) {
-        yield counter;
-        counter += 1;
-    }
+        if(start === 0) return;
+        
+        if(notpositiveinteger(start)) fail(ERR_BAD_RANGECOUNT, rangeissue(start));
+        
+        [start, end] = [1, start];
+    
+    } else validaterange(start, end);
+
+    const increment = (start <= end) ? STEP_INCREMENT : STEP_DECREMENT;
+    end += increment;
+    
+    for( let counter = start; counter !== end; counter += increment ) yield counter;
+}
+
+function rangeissue(range) {
+    return (typeof range === 'number') ? `is ${range}` : `has type ${typeorclass(range)}`;
+}
+
+function validaterange(start, end) {
+
+    if( notinteger(start) ) fail(ERR_BAD_RANGE, 'start', rangeissue(start));
+    else if( notinteger(end) ) fail(ERR_BAD_RANGE, 'end', rangeissue(end));
 }
