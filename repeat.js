573889081ca7691d4e22e4687abfd32e3d647288
Dante le Poole <dantelepoole@gmail.com>
2022-07-01 12:49:24 -0400

    Refactor

--- repeat.js
+++ repeat.js
@@ -4,8 +4,15 @@
 
 'use strict';
 
+const ERR_BAD_COUNT = `RepeatError~The count has type %s. Expected a number.`;
+const ERR_BAD_FUNCTION = `RepeatError~The function has type %s. Expected a function.`;
+
+const fail = require('./fail');
+const typeorclass = require('./fail');
+
 /**
- * Invoke *func* *count* number of times, passing *args* at each invocation.
+ * Invoke *func* *count* number of times, passing *args* at each invocation. The *func* function wil be called
+ * with `repeat()`'s `this`-object on each invocation.
  * 
  * @func repeat
  * @param {number} count The number of times to repeat *func*
@@ -14,8 +21,11 @@
  */
 module.exports = function repeat(count, func, ...args) {
 
+    if(typeof count !== 'number') fail(ERR_BAD_COUNT, typeorclass(count));
+    if(typeof func !== 'function') fail(ERR_BAD_FUNCTION, typeorclass(func));
+
     while( count > 0 ) {
-        func(...args);
+        func.call(this, ...args);
         count -= 1;
     }
 }
--- test/repeat.js
+++ test/repeat.js
@@ -1,51 +1,75 @@
 const expect = require('chai').expect;
 const repeat = require('../repeat');
 
-let invocationcount = 0;
+const sandbox = require('sinon').createSandbox();
+const spy = sandbox.spy.bind(sandbox);
 
-function returnargs(...args) {
-    invocationcount += 1;
-    return args;
-}
+const returnargs = spy(
+    function returnargs(...args) {
+        return args;
+    }
+)
 
 describe(`repeat()`, function() {
 
     beforeEach(
         function() {
-            invocationcount = 0;
+            sandbox.resetHistory();
         }
     )
 
-    it(`should call its second argument the number of times specified by its first argument`,
-        function () {
-            repeat(42, returnargs);
-            expect(invocationcount).to.be.equal(42);
-        }
-    )
-
-    it(`should throw if its second argument is not a function`,
+    it(`should throw if the function is not a function`,
         function () {
             expect( ()=>repeat(42, {})).to.throw();
         }
     )
 
-    it(`should pass its third and further arguments as arguments on each invocation`,
+    it(`should throw if the count is not a number`,
         function () {
-            function checkargs(...args) {
-                expect(args).to.be.deep.equal([1,2,3]);
-            }
-            repeat(5, checkargs, 1,2,3);
+            expect( ()=>repeat('foobar', {})).to.throw();
         }
     )
 
-    it(`should not run its second argument at all if the first argument is negative or not a number`,
+    it(`should call the function the specified number of times`,
+        function () {
+            repeat(42, returnargs);
+            expect(returnargs.callCount).to.equal(42);
+        }
+    )
+
+    it(`should pass its third and further arguments to the function on each invocation`,
+        function () {
+            const checkargs = spy(
+                function checkargs(...args) {
+                    expect(args).to.deep.equal([1,2,3]);
+                }
+            )
+            repeat(5, checkargs, 1,2,3);
+            expect(checkargs.callCount).to.equal(5);
+        }
+    )
+
+    it(`should not call the function if the count is less than 1 or NaN`,
         function () {
             repeat(-1, returnargs);
-            expect(invocationcount).to.be.equal(0);
-            repeat(null, returnargs);
-            expect(invocationcount).to.be.equal(0);
-            repeat('foobar', returnargs);
-            expect(invocationcount).to.be.equal(0);
+            expect(returnargs.callCount).to.equal(0);
+            repeat(0, returnargs);
+            expect(returnargs.callCount).to.equal(0);
+            repeat(NaN, returnargs);
+            expect(returnargs.callCount).to.equal(0);
+        }
+    )
+
+    it(`should pass it's this-object to the target function on each invocation`,
+        function () {
+            const that = {};
+            const checkthis = spy(
+                function checkthis() {
+                    expect(this).to.equal(that);
+                }
+            )
+            repeat.call(that, 5, checkthis);
+            expect(checkthis.callCount).to.equal(5);
         }
     )
 })
