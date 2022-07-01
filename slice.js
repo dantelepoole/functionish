6a9a275f09e3b925239bd396c7825ad73e25230b
Dante le Poole <dantelepoole@gmail.com>
2022-07-01 16:45:34 -0400

    Refactor

--- slice.js
+++ slice.js
@@ -2,22 +2,50 @@
  * @module slice
  */
 
+const ERR_BAD_SLICABLE = `SliceError~The slicable has type %s. Expected an array, a string or a TypedArray.`;
+
 'use strict';
 
+const fail = require('./fail');
+const isarray = require('./isarray');
+const typeorclass = require('./typeorclass');
+
+const istypedarray = require('util').types.isTypedArray;
+
+const isslicable = x => (typeof x === 'string' || isarray(x) || istypedarray(x));
+
 /**
- * Functional variant of {@link external:Array.prototype.slice Array.prototype.slice()}. Return a shallow copy of
- * the *list* array or string starting at *startindex* up to but including *endindex*. Pass 0, `null`, `undefined` or
- * `NaN` for *endindex* to return a copy of the entire array (i.e. set *endindex* equal to *list*'s length).
+ * Functional variant of the `slice()` method of Javascript strings, arrays and TypedArrays.
+ * 
+ * This function is overloaded:
+ * 
+ * 1. `slice(slicable)` : calls `slicable.slice()`
+ * 2. `slice(start, slicable)` : calls `slicable.slice(start)`
+ * 3. `slice(start, end, slicable)` : calls `slicable.slice(start, end)`
+ * 
+ * In each case, `slicable` must be either an array, a string or a TypedArray instance, otherwise an error is thrown.
+ * 
+ * This function also works with Node's `Buffer` instances, but be aware that `Buffer.slice()` does not return a 
+ * shallow copy. Instead, the returned Buffer references the same memory as the (specified section) of the input
+ * Buffer.
+ * 
+ * See {@link external:Array.prototype.slice Array.prototype.slice()} for details on the *start* and *end* parameters.
  * 
  * @func slice
  * @see {@link external:Array.prototype.slice Array.prototype.slice()}
- * @param {number} startindex The index of the first item to include in the slice
- * @param {number} endindex The index of the item *after* the last item to include in the slice
- * @param {(array|string)} list The string or array of items to slice
- * @returns {any[]}
+ * @see {@link external:String.prototype.slice String.prototype.slice()}
+ * @see {@link external:TypedArray.prototype.slice TypedArray.prototype.slice()}
+ * @param {number} start The index of the first item to include in the slice
+ * @param {number} end The index of the item *after* the last item to include in the slice
+ * @param {(array|string|TypedArray)} slicable An array, a string or a TypedArray
+ * @returns {any}
  */
-module.exports = require('./curry3') (slice);
+module.exports = function slice(start, end, slicable) {
 
-function slice(startindex, endindex, list) {
-    return list.slice(startindex, endindex || list.length);
+    if(arguments.length === 1) [start,end,slicable] = [0, Infinity, start];
+    else if(arguments.length === 2) [start,end,slicable] = [start, Infinity, end];
+
+    if( isslicable(slicable) ) return slicable.slice(start, end);
+
+    fail(ERR_BAD_SLICABLE, typeorclass(slicable));
 }
--- test/slice.js
+++ test/slice.js
@@ -2,38 +2,47 @@ const expect = require('chai').expect;
 const slice = require('../slice');
 
 const numbers1to10 = [1,2,3,4,5,6,7,8,9,10];
+const numbers1to10_typed = new Uint8Array(numbers1to10);
 
 describe(`slice()`, function() {
 
-    it(`should be curried with ternary arity`,
+    it(`should return a shallow copy of the slicable if called with one argument`,
         function () {
-            const curried = slice(0);
-            expect(curried).to.be.a('function');
-
-            const curried2 = curried(numbers1to10.length);
-            expect(curried2).to.be.a('function');
-
-            expect( curried2(numbers1to10) ).to.be.an('array');
+            
+            const copy = slice(numbers1to10);
+            expect(copy).to.be.deep.equal(numbers1to10);
+            expect(copy).not.to.equal(numbers1to10);
         }
     )
 
-    it(`should return the section of the input array between the startindex (inclusive) and the endindex (exclusive)`,
+    it(`should return a shallow copy of the slicable beginning at the specified start index if called with two argument`,
         function () {
-            let result = slice(2, 7, numbers1to10);
-            expect(result).to.be.deep.equal([3,4,5,6,7]);
+            
+            const copy = slice(5, numbers1to10);
+            expect(copy).to.be.deep.equal([6,7,8,9,10]);
         }
     )
 
-    it(`should set endindex to the input array's length if endindex is a falsy value`,
+    it(`should return a shallow copy of the section of the sliceable between the specified start (inclusive) and the end (exclusive) indices if called with three arguments`,
         function () {
-            expect( slice(0, 0, numbers1to10) ).to.be.deep.equal(numbers1to10);
-            expect( slice(0, -0, numbers1to10) ).to.be.deep.equal(numbers1to10);
-            expect( slice(0, null, numbers1to10) ).to.be.deep.equal(numbers1to10);
-            expect( slice(0, undefined, numbers1to10) ).to.be.deep.equal(numbers1to10);
-            expect( slice(0, NaN, numbers1to10) ).to.be.deep.equal(numbers1to10);
-            expect( slice(0, 0n, numbers1to10) ).to.be.deep.equal(numbers1to10);
-            expect( slice(0, '', numbers1to10) ).to.be.deep.equal(numbers1to10);
-            expect( slice(0, false, numbers1to10) ).to.be.deep.equal(numbers1to10);
+            const copy = slice(5, -1, numbers1to10);
+            expect(copy).to.be.deep.equal([6,7,8,9]);
+        }
+    )
+
+    it(`should accept strings, arrays and TypedArray instances for the slicable argument`,
+        function () {
+            expect( slice(5, -1, numbers1to10) ).to.be.deep.equal([6,7,8,9]);
+            expect( slice(4, -1, 'Foobar' ) ).to.equal('a');
+            expect( slice(5, -1, new Uint8Array(numbers1to10)) ).to.deep.equal(new Uint8Array([6,7,8,9]));
+        }
+    )
+
+    it(`should throw if the slicable is not a string, an array or a TypeArray instance`,
+        function () {
+            expect( ()=>slice({}) ).to.throw();
+            expect( ()=>slice() ).to.throw();
+
         }
     )
 
@@ -44,35 +53,6 @@ describe(`slice()`, function() {
             expect( slice(0, -7, numbers1to10) ).to.be.deep.equal([1,2,3]);
             expect( slice(0, -5, numbers1to10) ).to.be.deep.equal([1,2,3,4,5]);
             expect( slice(0, 5, numbers1to10) ).to.be.deep.equal([1,2,3,4,5]);
-        }
-    )
-
-    it(`should the startindex and endindex to the slice() method of the list argument`,
-        function () {
-            let wasinvoked = false;
-            const obj = {
-                slice(arg1, arg2) {
-                    wasinvoked = true;
-                    expect(arg1).to.be.equal(42);
-                    expect(arg2).to.be.equal(-42);
-                    return 'foobar';
-                }
-            }
-
-            slice(42,-42,obj);
-            expect(wasinvoked).to.be.true;
-        }
-    )
-
-    it(`should throw if the list argument has no slice() method`,
-        function () {
-            expect( () => slice(0,0,{}) ).to.throw();
-        }
-    )
-
-    it(`should work with strings`,
-        function () {
-            expect( slice(0,3, 'Hari Seldon') ).to.be.equal('Har');
         }
     )
 })
