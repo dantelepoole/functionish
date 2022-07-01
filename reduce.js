d9e165d4dece11dcbe490809fd64c101cb38df9c
Dante le Poole <dantelepoole@gmail.com>
2022-06-30 21:58:30 -0400

    Refactor

--- reduce.js
+++ reduce.js
@@ -4,41 +4,49 @@
 
 'use strict';
 
+const ERR_BAD_REDUCABLE = `ReduceError~The reducable has type %s. Expected an object with a reduce() method or an iterable object.`;
+
 const binary = require('./binary');
 
+const fail = require('./fail');
+const isiterable = require('./isiterable');
+const typeorclass = require('./typeorclass');
+
 /**
- * Functional variant of {@link external:Array.prototype.reduce Array.prototype.reduce()}. If *list* has a `reduce()`
- * method, invoke it with *reducer* and *initialvalue*. Otherwise, *list* is assumed to be iterable and its items
- * are reduced with *reducer* and *initialvalue*.
+ * Functional variant of {@link external:Array.prototype.reduce Array.prototype.reduce()}. If *reducable* has a
+ * `reduce()` method, invoke it with *reducer* and *initialvalue*. Otherwise, if *reducable* is iterable, reduce its
+ * items with *reducer* and *initialvalue*.
  * 
- * *Important:* the *list* function is coerced to binary arity before it is passed to *list*'s `reduce()` 
+ * *Important:* the *reducable* function is coerced to binary arity before it is passed to *reducable*'s `reduce()` 
  * method. This means that *reducer* will only ever receive two arguments (the accumulator and the next value),
- * regardless of how many arguments *list*'s `reduce()` method actually passes.
+ * regardless of how many arguments *reducable*'s `reduce()` method actually passes.
  * 
- * `reduce()` is curried by default.
+ * `reduce()` is curried by default with ternary arity.
  * 
  * @example
  * 
- * const reduce = require('functionish/reduce')
+ * const reduce = require('functionish/reduce');
+ * const range = require('functionish/range');
  * 
  * function sum(a,b) { return (a+b) }
  * 
  * reduce(sum, 0, [1,2,3]); // returns 6
+ * reduce(sum, 0, range(3)); //returns 6
  * 
  * @func reduce
  * @param {function} reducer The reducer function
  * @param {any} initialvalue The initial value to pass to *reducer* as the accumulator
- * @param {(reducable|iterable)} list An object with a `reduce()`-method or an iterable object
+ * @param {reducable} reducable An object with a `reduce()`-method or an iterable object
  * @returns {any} The reduced value
  */
 
 module.exports = require('./curry3')(
 
-    function reduce(reducer, initialvalue, list) {
-        
-        return (typeof list.reduce === 'function') 
-             ? list.reduce( binary(reducer), initialvalue )
-             : reduceiterable( reducer, initialvalue, list );
+    function reduce(reducer, initialvalue, reducable) {
+
+        return (typeof reducable?.reduce === 'function') ? reducable.reduce( binary(reducer), initialvalue )
+             : isiterable(reducable) ? reduceiterable( reducer, initialvalue, reducable )
+             : fail(ERR_BAD_REDUCABLE, typeorclass(reducable));
     }
 )
 
--- test/reduce.js
+++ test/reduce.js
@@ -1,18 +1,32 @@
 const expect = require('chai').expect;
+const range = require('../range');
 const reduce = require('../reduce');
 
-let invocationcount = 0;
+const sandbox = require('sinon').createSandbox();
+const spy = sandbox.spy.bind(sandbox);
 
-function sum(a,b) {
-    invocationcount += 1;
-    return (a+b);
+const sum = spy(
+    function sum(a,b) {
+        return (a+b);
+    }
+)
+
+function spyreducable(reducable) {
+
+    const object = {
+        reduce: reducable.reduce.bind(reducable)
+    }
+
+    spy(object, 'reduce');
+
+    return object;
 }
 
 describe(`reduce()`, function() {
 
     beforeEach(
         function() {
-            invocationcount = 0;
+            sandbox.resetHistory();
         }
     )
 
@@ -25,47 +39,53 @@ describe(`reduce()`, function() {
             expect(curried).to.be.a('function');
 
             const result = curried([1,2,3]);
+            expect(result).not.to.be.a('function');
             expect(result).to.be.equal(6);
         }
     )
 
-    it(`should pass its first two arguments to the reduce() method of its third argument`,
+    it(`should pass reducable and the initialvalue to the reducer's reduce() method, if it has one`,
         function () {
-            const reducable = {
-                reduce(reducer, initialvalue) {
-                    return reducer(initialvalue, 1);
-                }
-            }
-            const result = reduce(sum, 42, reducable);
-            expect(result).to.be.equal(43);
-            expect(invocationcount).to.be.equal(1);
+            const reducable = spyreducable([1,2,3]);
+            const result = reduce(sum, 0, reducable);
+            expect(result).to.be.equal(6);
+            expect(reducable.reduce.callCount).to.equal(1);
+            expect(sum.callCount).to.equal(3);
         }
     )
 
-    it(`should reduce its third argument's items if the third argument has no reduce() method`,
+    it(`should reduce the items produced by the reducable if it has no reduce() method but it is iterable`,
         function() {
-            const concatreverse = (a,b)=>`${b}${a}`
-            const result = reduce(concatreverse, '', 'foobar');
-            expect(result).to.be.equal('raboof');
+            const result = reduce(sum, 0, range(3));
+            expect(result).to.be.equal(6);
+            expect(sum.callCount).to.equal(3);
         }
     )
 
-    it(`should throw if its third argument has no reduce() method and is not iterable`,
+    it(`should throw if the reducable has no reduce() method and is not iterable`,
         function () {
-            expect( () => reduce(sum, 0, {}) ).to.throw();
+            expect( ()=>reduce(sum, 0, {}) ).to.throw();
         }
     )
 
-    it(`should ensure its first argument is only ever passed exactly two arguments`,
+    it(`should throw if the reducer is not a function`,
         function () {
-            function countargs() {
-                invocationcount += 1;
-                expect(arguments.length).to.be.equal(2);
-                return arguments.length;
-            }
+            expect( ()=>reduce({}, 0, [1,2,3]) ).to.throw();
+        }
+    )
+
+    it(`should ensure the reducer is always called with exacty two arguments`,
+        function () {
+            const countargs = spy(
+                function(...args) {
+                    expect(args.length).to.equal(2);
+                    return args.length;
+                }
+            )
             const result = reduce(countargs, 0, [1,2,3,4]);
             expect(result).to.be.equal(2);
-            expect(invocationcount).to.be.equal(4);
+
+            for(const args of countargs.args) expect(args.length).to.equal(2);
         }
     )
 })
