176b04f85c6a4da0beecc1ef74387526f2c42c09
Dante le Poole <dantelepoole@gmail.com>
2022-07-05 00:28:15 -0400

    Refactor

--- test/typeorclass.js
+++ test/typeorclass.js
@@ -1,13 +1,50 @@
 const expect = require('chai').expect;
 const typeorclass = require('../typeorclass');
 
+class Foobar {}
+class SubFoobar extends Foobar {}
+
+class TaggedClass {
+    get [Symbol.toStringTag]() { return 'ClassTag'}
+}
+
 describe(`typeorclass()`, function() {
 
-    beforeEach(
+    it(`should return a primitive type's type`,
         function() {
-
+            expect( typeorclass('foobar') ).to.equal('string');
+            expect( typeorclass() ).to.equal('undefined');
+            expect( typeorclass(undefined) ).to.equal('undefined');
+            expect( typeorclass(1) ).to.equal('number');
+            expect( typeorclass(NaN) ).to.equal('number');
+            expect( typeorclass(1n) ).to.equal('bigint');
+            expect( typeorclass(true) ).to.equal('boolean');
+            expect( typeorclass(Symbol()) ).to.equal('symbol');
         }
     )
 
-    it(`should be tested`);
+    it(`should return 'null' if the value is null`,
+        function() {
+            expect( typeorclass(null) ).to.equal('null');
+        }
+    )
+
+    it(`should return 'Object' (capitalized) if the value is a generic object`,
+        function() {
+            expect( typeorclass({}) ).to.equal('Object');
+        }
+    )
+
+    it(`should return the value's tag if it is an instance of a class with a Symbol.toStringTag getter`,
+        function() {
+            expect( typeorclass(new TaggedClass()) ).to.equal('ClassTag');
+        }
+    )
+
+    it(`should return the name of the value's constructor function if it is object`,
+        function() {
+            expect( typeorclass(new Foobar()) ).to.equal('Foobar');
+            expect( typeorclass(new SubFoobar()) ).to.equal('SubFoobar');
+        }
+    )
 });
--- typeorclass.js
+++ typeorclass.js
@@ -9,6 +9,9 @@ const classname = require('./classname');
 /**
  * Return *value*'s Javascript type if *value* is not an object, otherwise return the name of *value*'s class.
  * 
+ * Note that if *value* is a generic object, this function returns `'Object'` (capitalized) instead of `'object'`. Also,
+ * If *value* is `null`, this function returns `'null'` instead of `'object'`.
+ * 
  * @function typeorclass
  * @see {@link module:classname classname()}
  * @param {any} value The value whose type or classname to return
@@ -18,5 +21,7 @@ module.exports = function typeorclass(value) {
 
     const type = typeof value;
 
-    return ((type !== 'object') && type) || classname(value);
+    return (type !== 'object') ? type
+         : (value ===  null) ? 'null'
+         : classname(value);
 }
