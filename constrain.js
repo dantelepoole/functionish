f27ce47c12329795510dfb98cc1ed5ab6dee40db
Dante le Poole <dantelepoole@gmail.com>
2022-07-06 23:50:23 -0400

    Refactor

--- constrain.js
+++ constrain.js
@@ -4,9 +4,15 @@
 
 'use strict';
 
+const isvoid = require('./isvoid');
+
 /**
  * If *value* lies between *lowerlimit* and *upperlimit* (both inclusively), return *value*. Otherwise, return
  * *lowerlimit* if *value* is less than *lowerlimit* or *upperlimit* if *value* is greater than *upperlimit*.
+ * 
+ * If *value* is `null`, `undefined` or `NaN`, *value* is returned.
+ * 
+ * `constrain()` is curried by default with ternary arity.
  * 
  * @example
  * 
@@ -24,14 +30,21 @@
  * const enddate = new Date('2020-01-01');
  * constrain(startdate, enddate, new Date('2010-01-01')); // returns Date('2010-01-01')
  * constrain(startdate, enddate, new Date('1999-01-01')); // returns startdate
- * * constrain(startdate, enddate, new Date('2022-01-01')); // returns enddate
+ * constrain(startdate, enddate, new Date('2022-01-01')); // returns enddate
+ * 
+ * @func constrain
+ * @param {any} lowerlimit The lower limit for *value* (inclusive)
+ * @param {any} upperlimit The uppoer limit for *value* (inclusive)
+ * @param {any} value The value to check
+ * @returns {any} Either *value* or the applicable limit
  */
 module.exports = require('./curry3') (
 
     function constrain(lowerlimit, upperlimit, value) {
 
-        return (lowerlimit > value) ? lowerlimit
-            : (upperlimit < value) ? upperlimit
-            : value;
+        return isvoid(value) ? value
+             : (lowerlimit > value) ? lowerlimit
+             : (upperlimit < value) ? upperlimit
+             : value;
     }
 )
