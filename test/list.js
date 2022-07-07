b7e05def205b178ff6352739f514c472d298dbcf
Dante le Poole <dantelepoole@gmail.com>
2022-07-06 23:32:38 -0400

    Add tests

--- test/list.js
+++ test/list.js
@@ -1,4 +1,7 @@
+const array = require('../array');
 const expect = require('chai').expect;
+const isiterable = require('../isiterable');
+const list = require('../list');
 const sandbox = require('sinon').createSandbox();
 const spy = sandbox.spy.bind(sandbox);
 
@@ -10,17 +13,32 @@ describe('list()', function() {
     
     beforeEach(function() {})
 
-    it(`should be tested`)
+    it(`should return an iterable object`, 
+        function() {
+            expect( isiterable( list(1,2,3) ) ).to.be.true
+        }
+    )
 
-    // describe(<ENTER SUBSUITE NAME>, function() {
+    describe('the iterable returned by list()', function() {
+    
+        it(`should produce the arguments to list()`, 
+            function() {
+                expect( array( list(1,2,3) ) ).to.deep.equal([1,2,3]);
+            }
+        )
 
-    //     afterEach(function() {
-    //         sandbox.resetHistory();
-    //     })
-            
-    //     beforeEach(function() {})
+        it(`should include the items produced by any nested iterable objects, one level deep`, 
+            function() {
+                expect( array( list(1,[2,3],4) ) ).to.deep.equal([1,2,3,4]);
+                expect( array( list(1,[2,[3,4]],5) ) ).to.deep.equal([1,2,[3,4],5]);
+            }
+        )
 
-    //     it(`should be tested`)
+        it(`should produce no items if no arguments were passed to list()`, 
+            function() {
+                expect( array( list() ) ).to.deep.equal([]);
+            }
+        )
 
-    // })
+    })
 })
