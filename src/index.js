56d6a776b0a8e92553e96d46cb9a3970acfaddf9
Dante le Poole <dantelepoole@gmail.com>
2022-07-09 22:41:44 -0400

    Don't auto require all functions and provide custom load() instead

--- src/index.js
+++ src/index.js
@@ -1,126 +1,73 @@
+/**
+ * @module index
+ */
 
-module.exports.AbortController = require('./AbortController');
-module.exports.AbortSignal = require('./AbortSignal');
-module.exports.always = require('./always');
-module.exports.and = require('./and');
-module.exports.any = require('./any');
-module.exports.applicable = require('./applicable');
-module.exports.apply = require('./apply');
-module.exports.at = require('./at');
-module.exports.attempt = require('./attempt');
-module.exports.batch = require('./batch');
-module.exports.binary = require('./binary');
-module.exports.bind = require('./bind');
-module.exports.call = require('./call');
-module.exports.callable = require('./callable');
-module.exports.classname = require('./classname');
-module.exports.compose = require('./compose');
-module.exports.concat = require('./concat');
-module.exports.concatlist = require('./concatlist');
-module.exports.constrain = require('./constrain');
-module.exports.copy = require('./copy');
-module.exports.curry = require('./curry');
-module.exports.curry2 = require('./curry2');
-module.exports.curry3 = require('./curry3');
-module.exports.curry4 = require('./curry4');
-module.exports.copydeep = require('./copydeep');
-module.exports.withdefault = require('./withdefault');
-module.exports.diff = require('./diff');
-module.exports.evaluate = require('./evaluate');
-module.exports.all = require('./all');
-module.exports.fail = require('./fail');
-module.exports.filter = require('./filter');
-module.exports.filterlist = require('./filterlist');
-module.exports.find = require('./find');
-module.exports.findlist = require('./findlist');
-module.exports.flat = require('./flat');
-module.exports.flatlist = require('./flatlist');
-module.exports.flip = require('./flip');
-module.exports.format = require('./format');
-module.exports.freeze = require('./freeze');
-module.exports.groupby = require('./groupby');
-module.exports.haskey = require('./haskey');
-module.exports.hasownproperty = require('./hasownproperty');
-module.exports.head = require('./head');
-module.exports.id = require('./id');
-module.exports.includes = require('./includes');
-module.exports.includeslist = require('./includeslist');
-module.exports.intersection = require('./intersection');
-module.exports.isdeepequal = require('./isdeepequal');
-module.exports.isequal = require('./isequal');
-module.exports.isgreaterthan = require('./isgreaterthan');
-module.exports.isgreaterthanorequal = require('./isgreaterthanorequal');
-module.exports.isinstanceof = require('./isinstanceof');
-module.exports.islessthan = require('./islessthan');
-module.exports.islessthanorequal = require('./islessthanorequal');
-module.exports.isoutofbounds = require('./isoutofbounds');
-module.exports.ispromise = require('./ispromise');
-module.exports.iswithinbounds = require('./iswithinbounds');
-module.exports.iterable = require('./iterable');
-module.exports.iterate = require('./iterate');
-module.exports.last = require('./last');
-module.exports.length = require('./length');
-module.exports.lift = require('./lift');
-module.exports.list = require('./list');
-module.exports.map = require('./map');
-module.exports.maplist = require('./maplist');
-module.exports.merge = require('./merge');
-module.exports.none = require('./none');
-module.exports.noop = require('./noop');
-module.exports.not = require('./not');
-module.exports.notarray = require('./notarray');
-module.exports.notdeepequal = require('./notdeepequal');
-module.exports.notempty = require('./notempty');
-module.exports.notequal = require('./notequal');
-module.exports.noterror = require('./noterror');
-module.exports.notfunction = require('./notfunction');
-module.exports.notindexable = require('./notindexable');
-module.exports.notinstanceof = require('./notinstanceof');
-module.exports.notinteger = require('./notinteger');
-module.exports.notiterable = require('./notiterable');
-module.exports.notnan = require('./notnan');
-module.exports.notnull = require('./notnull');
-module.exports.notnumber = require('./notnumber');
-module.exports.notobject = require('./notobject');
-module.exports.notprimitive = require('./notprimitive');
-module.exports.notpromise = require('./notpromise');
-module.exports.notstring = require('./notstring');
-module.exports.notsymbol = require('./notsymbol');
-module.exports.notvoid = require('./notvoid');
-module.exports.once = require('./once');
-module.exports.or = require('./or');
-module.exports.parsejson = require('./parsejson');
-module.exports.partial = require('./partial');
-module.exports.pipe = require('./pipe');
-module.exports.pluck = require('./pluck');
-module.exports.queue = require('./queue');
-module.exports.queuemicrotask = require('./queuemicrotask');
-module.exports.range = require('./range');
-module.exports.reduce = require('./reduce');
-module.exports.repeat = require('./repeat');
-module.exports.reverse = require('./reverse');
-module.exports.separate = require('./separate');
-module.exports.slice = require('./slice');
-module.exports.split = require('./split');
-module.exports.stack = require('./stack');
-module.exports.symdiff = require('./symdiff');
-module.exports.tail = require('./tail');
-module.exports.take = require('./take');
-module.exports.tap = require('./tap');
-module.exports.timeout = require('./timeout');
-module.exports.tojson = require('./tojson');
-module.exports.tostring = require('./tostring');
-module.exports.trim = require('./trim');
-module.exports.typeorclass = require('./typeorclass');
-module.exports.unary = require('./unary');
-module.exports.uniadic = require('./uniadic');
-module.exports.uniq = require('./uniq');
-module.exports.union = require('./union');
-module.exports.unless = require('./unless');
-module.exports.variadic = require('./variadic');
-module.exports.when = require('./when');
-module.exports.where = require('./where');
-module.exports.wrap = require('./wrap');
-module.exports.xor = require('./xor');
-module.exports.zip = require('./zip');
-module.exports.zipwith = require('./zipwith');
+'use strict';
+
+/**
+ * The functionish package consists of 150+ functions, each in their own module, of which you will usually only
+ * need a handful in your own modules. So it would be very ineffecient to have each functionish module loaded
+ * into memory. At the same time, it can be annoying to have to write a separate `require()` statement for each
+ * module you wish to use.
+ * 
+ * The main functionish module seeks to alleviate this annoyance by exporting an object that offers only a single
+ * `load()` method when first required. The `load()` method accepts one or more module names (function names) to
+ * load and return, and simultaneously stores them as methods of the functionish module itself. This allows you to
+ * load only the modules you need and to load them in a single statement (well, two statements, actually).
+ * 
+ * @example
+ * 
+ * const f = require('functionish');
+ * const {curry, partial, map, reduce} = f.load('curry', 'partial', 'map', 'reduce');
+ * 
+ * // the specified modules are now loaded into their respective variables and as
+ * // methods on the f-object
+ * 
+ * curry === f.curry; // true
+ * partial === f.partial; // true
+ * map === f.map; // true
+ * reduce === f.reduce; // true
+ */
+const functionish = {
+
+    /**
+     * The functionish package consists of 150+ functions, each in their own module, of which you will usually only
+     * need a handful in your own modules. So it would be very ineffecient to have each functionish module loaded
+     * into memory. At the same time, it can be annoying to have to write a separate `require()` statement for each
+     * module you wish to use.
+     * 
+     * The main functionish module seeks to alleviate this annoyance by exporting an object that offers only a single
+     * `load()` method when first required. The `load()` method accepts one or more module names (function names) to
+     * load and return, and simultaneously stores them as methods of the functionish module itself. This allows you to
+     * load only the modules you need and to load them in a single statement (well, two statements, actually).
+     * 
+     * @example
+     * 
+     * const f = require('functionish');
+     * const {curry, partial, map, reduce} = f.load('curry', 'partial', 'map', 'reduce');
+     * 
+     * // the specified modules are now loaded into their respective variables and as
+     * methods on the f-object
+     * 
+     * curry === f.curry; // true
+     * partial === f.partial; // true
+     * map === f.map; // true
+     * reduce === f.reduce; // true
+     * 
+     * @param  {...string} modules The names of one or  more modules to load
+     * @returns {object} The functionish object with the specified modules loaded
+     */
+    load(...modules) {
+        modules.forEach(modulename => loadmodule(functionish, modulename));
+        return functionish;
+    }
+}
+
+function loadmodule(functionish, modulename) {
+
+    if(modulename !== 'load' && typeof functionish[modulename] === 'undefined') {
+        functionish[modulename] = require(`./${modulename}`);
+    }
+}
+
+module.exports = functionish;
