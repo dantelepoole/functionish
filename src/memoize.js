f61e03164cc194ee97255da6c6afe69fe381af2f
Dante le Poole <dantelepoole@gmail.com>
2022-07-10 23:30:53 -0400

    Refactor and add JSDoc comment

--- src/memoize.js
+++ src/memoize.js
@@ -4,7 +4,7 @@
 
 'use strict';
 
-const CACHE_ENTRY_EMPTY = null;
+const CACHE_NONE = null;
 const ERR_BAD_CACHEFUNC = `MemoizeError~The cache function has type %s. Expected a function.`;
 const ERR_BAD_TARGETFUNC = `MemoizeError~The target function has type %s. Expected a function.`;
 
@@ -12,6 +12,52 @@ const fail = require('./fail');
 const isvoid = require('./isvoid');
 const typeorclass = require('./typeorclass');
 
+/**
+ * Return a function that cache's *targetfunc*'s return values based on its argument list.
+ * 
+ * The returned function uses *cachefunc* for the caching operations. The *cachefunc* should comply with the following
+ * conditions:
+ * 1. If called without any arguments, *cachefunc* should clear its internal cache.
+ * 2. If called with a single argument, it will be an array of the arguments pass to the *targetfunc*.
+ * The *cachefunc* should return a cached result associated with those arguments or `null` if no such
+ * result has been cached.
+ * 3. If called with two arguments, the first will be an array of the argument passed to the *targetfunc* and the
+ * second will be the result that the *cachefunc* should associate with those arguments. Note that the value will not
+ * be the actual return value of the *targetfunc*. Instead, it will be an object that holds *targetfunc*'s actual
+ * return value as a property.
+ * 
+ * If *cachefunc* is `null` or `undefined`, `memoize()` will use its default cache function. The default cache function
+ * is very simplistic and simply converts the arguments to strings to construct the key with which a return value is
+ * cached. Therefore, the default cache function is only appropriate for arguments of primitive types only. If one or
+ * more arguments have a non-primitive type (e.g. type `'object'`), *the default cache function will produce incorrect
+ * results*. Provide your own cache function to customize the caching for the types of arguments you expect your 
+ * function to receive. *Use the default cache function at your own risk.*
+ * 
+ * The returned function will have a `clearcache()` method that clears the internal cache of return values.
+ * 
+ * `memoize()` is curried by default with binary arity.
+ * 
+ * @example
+ * 
+ * const memoize = require('functionish/memoize');
+ * 
+ * const loaduserdata = memoize(
+ *     null, // use the default cache function (dangerous!)
+ *     function loaduserdata(userid) {
+ *         // retrieve data from the database
+ *     }
+ * )
+ * 
+ * loaduserdata(42); // retrieves the user data from the database
+ * loaduserdata(42); // returns the cached user data
+ * 
+ * loaduserdata.clearcache(); // clear the cached data
+ * 
+ * @func memoize
+ * @param {function} cachefunc A function that can cache the *targetfunc*'s results
+ * @param {function} targetfunc The target function to cache the results of
+ * @return {function} The memoized function
+ */
 module.exports = require('./curry2') (
 
     function memoize(cachefunc, targetfunc) {
@@ -28,11 +74,9 @@ module.exports = require('./curry2') (
             [memoizename] : function (...args) {
 
                 const cachedresult = cachefunc(args);
-
-                if(cachedresult !== CACHE_ENTRY_EMPTY) return cachedresult.value;
+                if(cachedresult !== CACHE_NONE) return cachedresult.value;
 
                 const result = targetfunc.apply(this, args);
-
                 cachefunc(args, { value:result });
 
                 return result;
@@ -40,7 +84,7 @@ module.exports = require('./curry2') (
 
         } [memoizename];
 
-        memoized.clearcache = function clearcache() { cachefunc(); }
+        memoized.clearcache = () => cachefunc();
 
         return memoized;
     }
@@ -48,18 +92,16 @@ module.exports = require('./curry2') (
 
 function cachefuncfactory() {
 
-    const cache = new Map();
+    const resultmap = new Map();
 
     return function cachefunc(args, result) {
 
-        if(arguments.length === 0) return cache.clear();
+        const argumentcount = arguments.length;
+        const cachekey = args.join();
 
-        const key = args.join(':');
+        return (argumentcount === 0) ? resultmap.clear()
+             : (argumentcount === 1) ? (resultmap.get(cachekey) ?? CACHE_NONE)
+             : void( resultmap.set(cachekey, result) );
 
-        if(arguments.length === 1) return (cache.get(key) ?? CACHE_ENTRY_EMPTY);
-
-        cache.set(key, result);
-
-        return result;
     }
 }
