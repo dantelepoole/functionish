dc9cff80658b87e0d094d10f9552d8575750ed34
Dante le Poole <dantelepoole@gmail.com>
2022-07-12 16:17:30 -0400

    Initial commit

---
+++ src/pop.js
@@ -0,0 +1,15 @@
+/**
+ * @module pop
+ */
+
+'use strict';
+
+/**
+ * Function variant of {@link external:Array.prototype.pop Array.prototype.pop()}.
+ * 
+ * @param {any[]} array The array to pop the last item from
+ * @returns {any}
+ */
+module.exports = function pop(array) {
+    return array.pop();
+}
---
+++ test/pop.js
@@ -0,0 +1,37 @@
+const expect = require('chai').expect;
+const pop = require('../src/pop');
+
+const sentinel = {};
+
+describe(`pop()`, function() {
+
+    it(`should return the last item in the array`,
+        function() {
+            expect( pop([1,2,sentinel]) ).to.equal(sentinel);
+        }
+    )
+
+    it(`should remove the last item from the array`,
+        function() {
+            const array = [sentinel];
+
+            expect( array.length ).to.equal(1);
+            expect( pop(array) ).to.equal(sentinel);
+            expect( array.length ).to.equal(0);
+        }
+    )
+
+    it(`should return undefined if the array is empty`,
+        function() {
+            expect( pop([]) ).to.be.undefined;
+        }
+    )
+
+    it(`should throw if the argument has no pop() method`,
+        function() {
+            expect( ()=>pop({}) ).to.throw();
+            expect( ()=>pop() ).to.throw();
+            expect( ()=>pop(null) ).to.throw();
+        }
+    )
+})
