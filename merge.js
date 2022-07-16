c8db61f18d1f8966809fd3e84db7cef7f4e250bb
Dante le Poole <dantelepoole@gmail.com>
2022-07-15 21:00:01 -0400

    Add JSDoc comment

--- merge.js
+++ merge.js
@@ -6,6 +6,16 @@
 
 const curry2 = require('./curry2');
 
+/**
+ * Copy the own, enumerable properties of the *sources* to a new object. Properties of earlier *source* object will
+ * be overwritten by properties of later *source* objects that have the same key.
+ * 
+ * `merge()` is curried by default with binary arity.
+ * 
+ * @func merge
+ * @param {...obj[]} sources Two or more objects to merge
+ * @returns {object} A new object 
+ */
 module.exports = curry2(
 
     function merge(...sources) {
