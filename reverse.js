33185b4bd307e4a58435b7d1df1aa8cc5488b949
Dante le Poole <dantelepoole@gmail.com>
2022-09-13 11:38:14 -0400

    Edit JSDoc comment

--- reverse.js
+++ reverse.js
@@ -5,7 +5,7 @@
 'use strict';
 
 const EMPTY_STRING = '';
-const ERR_BAD_LIST = `ReverseError~The list has type %s. Expected an iterable object.`;
+const ERR_BAD_LIST = `ReverseError~The list has type %s. Expected an iterable object or a string.`;
 
 const fail = require('./fail');
 const isarray = require('./isarray');
@@ -15,10 +15,11 @@ const typeorclass = require('./typeorclass');
 
 /**
  * Return an iterable object that produces the items from *list* in reverse order, without affecting or altering
- * *list* itself.
+ * *list* itself. Although *functionish* does not recognize strings as being iterable, `reverse()` does work with
+ * strings.
  * 
  * @func reverse
- * @param {iterable} list An iterable object producing the items to reverse
+ * @param {iterable} list An iterable object producing the items to reverse, or a string
  * @returns {iterable}
  */
 module.exports = function reverse(list) {
