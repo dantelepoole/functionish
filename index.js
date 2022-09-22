359b9957336d06f44650c8db56830f2141caf94e
Dante le Poole <dantelepoole@gmail.com>
2022-09-22 17:24:39 -0400

    Refactor

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 
 'use strict';
 
+const FUNCTION_LOAD = 'load';
+
 /**
  * The functionish package consists of 150+ functions, each in their own module, of which you will usually only
  * need a handful in your own modules. So it would be very ineffecient to have each functionish module loaded
@@ -41,33 +43,29 @@ const functionish = {
      * load and return, and simultaneously stores them as methods of the functionish module itself. This allows you to
      * load only the modules you need and to load them in a single statement (well, two statements, actually).
      * 
-     * @example
-     * 
-     * const f = require('functionish');
-     * const {curry, partial, map, reduce} = f.load('curry', 'partial', 'map', 'reduce');
-     * 
-     * // the specified modules are now loaded into their respective variables and as
-     * methods on the f-object
-     * 
-     * curry === f.curry; // true
-     * partial === f.partial; // true
-     * map === f.map; // true
-     * reduce === f.reduce; // true
-     * 
-     * @param  {...string} modules The names of one or  more modules to load
+     * @param  {...string} modules The names of one or more modules to load
      * @returns {object} The functionish object with the specified modules loaded
      */
     load(...modules) {
-        modules.forEach(modulename => loadmodule(functionish, modulename));
+        modules.forEach(modulename => loadfunction(modulename));
         return functionish;
     }
 }
 
-function loadmodule(functionish, modulename) {
+function loadfunction(functionname) {
+    isloadable(functionname) && loadmodule(functionname);
+}
 
-    if(modulename !== 'load' && typeof functionish[modulename] === 'undefined') {
-        functionish[modulename] = require(`./${modulename}`);
-    }
+function loadmodule(modulename) {
+    functionish[modulename] = require(`${__dirname}/${modulename}`);
+}
+
+function isloadable(functionname) {
+    return (functionname !== FUNCTION_LOAD && notloaded(functionname));
+}
+
+function notloaded(functionname) {
+    return (functionish[functionname] === undefined);
 }
 
 module.exports = functionish;
