7445d76360922aca3a96ffb21b1e465dc99903cd
Dante le Poole <dantelepoole@gmail.com>
2022-11-05 22:56:36 -0400

    Allow array and non-string path arguments

--- pluck.js
+++ pluck.js
@@ -4,20 +4,27 @@
 
 'use strict';
 
-const INDEX_NOT_FOUND = -1;
 const KEY_SEPARATOR_CHAR = '.';
-const TARGET_UNDEFINED = undefined;
+const PROPERTY_NOT_FOUND = undefined;
 
-const isempty = require('./isempty');
-const isequal = require('./isequal');
-const notobject = require('./notobject');
-const notstring = require('./notstring');
+const isarray = require('./isarray');
+const isobject = require('./isobject');
+const isstring = require('./isstring');
+const isundefined = require('./isundefined');
 
-const notfound = isequal(INDEX_NOT_FOUND);
+const propertyreducer = (source, key) => isobject(source) ? source[key] : PROPERTY_NOT_FOUND;
 
 /**
- * Return the value of the property of *source* identified by *path*. The *path* argument may be a compound path, i.e.
- * a path that goes multiple levels deep. In that case, the individual property keys should be separated by a `.`.
+ * Return the value of the property of *source* specified by *path*.
+ * 
+ * The *path* argument may be a compound path, i.e. a path that goes multiple levels deep. In that case, the
+ * individual property keys should be separated by a `.`.
+ * 
+ * The *path* may be any type that can be used as a property key, e.g. a string, symbol or number. Therefore, you can
+ * access individual array items when the pluck property is an array.
+ *  
+ * The *path* may also be an array of individual property keys. In this case, the array items may not be compound
+ * paths.
  * 
  * If the path is invalid or if any intermediate property has a non-object value, `undefined` is returned. That means
  * that `pluck()` does not recurse down into the properties of, for example, a string or a function.
@@ -29,22 +36,27 @@ const notfound = isequal(INDEX_NOT_FOUND);
  * const pluck = require('functionish/pluck');
  * 
  * const source = {
- *   name  : 'Hari Seldon',
- *   world : 'Trantor',
- *   book  : {
- *     title  : 'Foundation',
- *     author : {
- *       name : 'Isaac Asimov',
- *     },
- *     year   : 1942
+ *   firstname : 'Donald',
+ *   lastname  : 'Duck',
+ *   nephews  : [
+ *      { firstname: 'Huey', lastname: 'Duck' },
+ *      { firstname: 'Dewey', lastname: 'Duck' },
+ *      { firstname: 'Louie', lastname: 'Duck' }
+ *   ],
+ *   creator : {
+ *       name : 'Walt Disney',
+ *       born : {
+ *           year : 1901,
+ *           city : 'Chicago'
+ *       }
  *   }
  * }
  * 
- * pluck('name', source); // returns 'Hari Seldon'
- * pluck('book.author.name', source); // returns 'Isaac Asimov'
- * pluck('book.year', source); // returns 1942
- * pluck('book.pagecount', source); // returns `undefined`
- * pluck('name.length', source); // returns `undefined`
+ * pluck('firstname', source); // returns 'Donald'
+ * pluck('creator.born.city', source); // returns 'Chicago'
+ * pluck('nephews.length', source); // returns 3
+ * pluck( ['nephews', 1, 'firstname' ], source); // returns 'Dewey'
+ * pluck( 'wife', source ); // returns undefined
  * 
  * @func pluck
  * @param {string} path The path of the property to retrieve
@@ -55,18 +67,12 @@ module.exports = require('./curry2')(
 
     function pluck(path, source) {
 
-        if( notstring(path) ) return source?.[path];
+        path = isstring(path) ? path.split(KEY_SEPARATOR_CHAR)
+             : isarray(path) ? path
+             : isundefined(path) ? []
+             : [path];
 
-        const index = path.indexOf(KEY_SEPARATOR_CHAR);
-
-        if( notfound(index) ) return source?.[path];
-
-        const [key, nextpath] = [path.slice(0, index), path.slice(index+1)]
-        const target = source?.[key];
-        
-        return isempty(nextpath) ? target
-             : notobject(target) ? TARGET_UNDEFINED
-             : pluck(nextpath, target);
+        return path.reduce(propertyreducer, source);
     }
 
 )
