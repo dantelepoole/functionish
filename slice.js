10564f12267ca5da0f8f58013c0dccd7dae8180c
Dante le Poole <dantelepoole@gmail.com>
2022-11-18 14:08:39 -0400

    Refactor to work with all iterable objects

--- slice.js
+++ slice.js
@@ -2,23 +2,21 @@
  * @module slice
  */
 
-const ERR_BAD_SLICABLE = `SliceError~The slicable has type %s. Expected an array, a string or a TypedArray.`;
-
 'use strict';
 
+const ERR_BAD_SLICABLE = `SliceError~The slicable has type %s. Expected an iterable object.`;
+
 const fail = require('./fail');
-const isarray = require('./isarray');
+const isiterable = require('./isiterable');
 const typeorclass = require('./typeorclass');
 
-const istypedarray = require('util').types.isTypedArray;
-
-const isslicable = x => (typeof x === 'string' || isarray(x) || istypedarray(x));
-
-const hassoleargument = args => (args.length === 1);
-const hastwoarguments = args => (args.length === 2);
+const isslicable = slicable => (typeof slicabe?.slice === 'function');
 
 /**
- * Functional variant of the `slice()` method of Javascript strings, arrays and TypedArrays.
+ * Functional variant of the Javascript `slice()` method for strings, array and TypedArrays. This function works
+ * with all iterable objects. If the *slicable* argument has a `slice()`-method, that method is called. Otherwise,
+ * if the *slicable* is iterable, it is loaded into array before calling the `slice()` method. If *slicable* is
+ * neither slicable nor iterable, an error is thrown.
  * 
  * This function is overloaded:
  * 
@@ -26,27 +24,26 @@ const hastwoarguments = args => (args.length === 2);
  * 2. `slice(start, slicable)` : calls `slicable.slice(start)`
  * 3. `slice(start, end, slicable)` : calls `slicable.slice(start, end)`
  * 
- * In each case, `slicable` must be either an array, a string or a TypedArray instance, otherwise an error is thrown.
- * 
  * This function also works with Node's `Buffer` instances, but be aware that `Buffer.slice()` does not return a 
  * shallow copy. Instead, the returned Buffer references the same memory as the (specified section) of the input
  * Buffer.
  * 
- * See {@link external:Array.prototype.slice Array.prototype.slice()} for details on the *start* and *end* parameters.
- * 
  * @func slice
- * @see {@link external:Array.prototype.slice Array.prototype.slice()}
- * @see {@link external:String.prototype.slice String.prototype.slice()}
- * @see {@link external:TypedArray.prototype.slice TypedArray.prototype.slice()}
  * @param {number} start The index of the first item to include in the slice
  * @param {number} end The index of the item *after* the last item to include in the slice
- * @param {(array|string|TypedArray)} slicable An array, a string or a TypedArray
- * @returns {any}
+ * @param {iterable} slicable An iterable object
+ * @returns {array}
  */
 module.exports = function slice(start, end, slicable) {
 
-    hassoleargument(arguments) ? ([start, end, slicable] = [0, Infinity, start])
-    : hastwoarguments(arguments) && ([start, end, slicable] = [start, Infinity, end]);
+    const argcount = arguments.length;
 
-    return isslicable(slicable) ? slicable.slice(start, end) : fail(ERR_BAD_SLICABLE, typeorclass(slicable));
+    [slicable, start, end] = (argcount === 1) ? [start, 0, Number.MAX_SAFE_INTEGER]
+                           : (argcount === 2) ? [end, (start ?? 0), Number.MAX_SAFE_INTEGER]
+                           : [slicable, (start ?? 0), (end ?? Number.MAX_SAFE_INTEGER)];
+
+    return isslicable(slicable) ? slicable.slice(start, end)
+         : isiterable(slicable) ? Array.from(slicable).slice(start, end)
+         : fail(ERR_BAD_SLICABLE, typeorclass(slicable));
 }
+
