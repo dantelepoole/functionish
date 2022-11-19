4c3044cc72997d7c68231dc1670a30bc52d59a1b
Dante le Poole <dantelepoole@gmail.com>
2022-11-19 17:07:22 -0400

    Rename recursive() to recurse() and refactor

--- recurse.js
+++ recurse.js
@@ -6,44 +6,55 @@
 
 const RECURSE_SYMBOL = Symbol.for(`functionish/recurse#RECURSE_SYMBOL`);
 
+const resolvefunction = require('./resolvefunction');
+
+const _recurse = (...args) => ({ args, recurse:RECURSE_SYMBOL });
+
 /**
- * When used in conjunction with {@link module:recursive recursive()}, this function
- * allows a function to emulate tail recursion.
+ * Return a function that allows *func* to emulate tail recursion.
  * 
  * To emulate tail recursion, instead of calling itself recursively, the target function
- * should call `recurse()` instead with the same arguments and return the result. Assuming
- * the target function was first passed to {@link module:recursive recursive()}, the result
+ * should call `this()` instead with the same arguments and return the result. The result
  * will be a recursive call to the target function without risk of exhausting the stack.
  * 
- * To work correctly, the call to `recurse()` *must* be a tail call, i.e. `recurse()`'s return
+ * To work correctly, the call to `this()` *must* be a tail call, i.e. `this()`'s return
  * value must be the only thing returned, without any additional intermediate operations.
  * 
- * The recursion ends when the target function returns anything other than the `recurse()`'s
- * return value.
+ * The recursion ends when the target function returns anything other than the result of
+ * calling `this()`.
+ * 
+ * Because `recurse()` relies on `this`, *func* may not be an arrow function, nor may it
+ * be bound to specific `this`-value.
  * 
  * @example
- * const recursive = require('functionish/recursive');
  * const recurse = require('functionish/recurse');
  * 
- * const factorial_recursive = recursive(
- *     function factorial(f, x) {
- *         if(x < 0) throw new Error('negative x');
- *         return (x === 0) ? f : recurse(f*x, x-1);
+ * const factorial = recurse(
+ * 
+ *     function (f, x) {
+ *      
+ *         return (arguments.length === 1) ? this(1, f)
+ *              : (x < 2) ? f
+ *              : this(f*x, x-1);
  *     }
  * )
- * 
- * const factorial = x => factorial_recursive(1, x);
  * 
  * factorial(8); // returns 40320
  * 
  * @func recurse
- * @see {@link module:recursive recursive()}
- * @param  {...any} args The arguments to pass recursively
+ * @param {function} func The function (or path to a function) to make recursive
+ * @returns {function}
  */
-module.exports = function recurse(...args) {
+module.exports = function recurse(func) {
 
-    return {
-        args,
-        recurse: RECURSE_SYMBOL
+    func = resolvefunction(func);
+    
+    return function recursivefunction(...args) {
+
+        let result = func.call(_recurse, ...args);
+
+        while(result?.recurse === RECURSE_SYMBOL) result = func.call(_recurse, ...result.args);
+
+        return result;
     }
 }
