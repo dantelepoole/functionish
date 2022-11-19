3a4cf33ebe312174beca06623732215dddea3380
Dante le Poole <dantelepoole@gmail.com>
2022-11-19 16:11:16 -0400

    Edit JSDoc comment

--- recurse.js
+++ recurse.js
@@ -12,22 +12,27 @@ const RECURSE_SYMBOL = Symbol.for(`functionish/recurse#RECURSE_SYMBOL`);
  * 
  * To emulate tail recursion, instead of calling itself recursively, the target function
  * should call `recurse()` instead with the same arguments and return the result. Assuming
- * the target function was passed to {@link module:recursive recursive()} first, the result
- * will a recursive call to the target function without exhausting the stack.
+ * the target function was first passed to {@link module:recursive recursive()}, the result
+ * will be a recursive call to the target function without risk of exhausting the stack.
  * 
- * The recursion ends when the target function returns any other value.
+ * To work correctly, the call to `recurse()` *must* be a tail call, i.e. `recurse()`'s return
+ * value must be the only thing returned, without any additional intermediate operations.
+ * 
+ * The recursion ends when the target function returns anything other than the `recurse()`'s
+ * return value.
  * 
  * @example
  * const recursive = require('functionish/recursive');
  * const recurse = require('functionish/recurse');
  * 
- * const factorial = recursive(
- *     function factorial(total, number) {
- *         return (arguments.length === 1) ? recurse(1, total)
- *              : (number === 0) ? total
- *              : recurse(total*number, number-1);
+ * const factorial_recursive = recursive(
+ *     function factorial(f, x) {
+ *         if(x < 0) throw new Error('negative x');
+ *         return (x === 0) ? f : recurse(f*x, x-1);
  *     }
  * )
+ * 
+ * const factorial = x => factorial_recursive(1, x);
  * 
  * factorial(8); // returns 40320
  * 
