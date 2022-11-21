77e36e6cb36c9c3aa575b6f92bf82b65db7de8c0
Dante le Poole <dantelepoole@gmail.com>
2022-11-20 21:27:13 -0400

    Refactor

--- hassingleitem.js
+++ hassingleitem.js
@@ -4,18 +4,13 @@
 
 'use strict';
 
-const isequal = require('./isequal');
-const isundefined = require('./isundefined');
-
-const isone = isequal(1);
-
 /**
  * Return `true` if *value* has a `length` or `size` property that is equal to `1`.
  * 
  * @func hassingleitem
- * @param {any} value An object with a `length`-property
+ * @param {any} value An object with a `length`- or `size`-field
  * @returns {boolean}
  */
 module.exports = function hassingleitem(value) {
-    return isone(value?.length) || ( isundefined(value?.length) && isone(value?.size) );
+    return (value?.length === 1) || ( (value?.length === undefined) && (value?.size === 1) );
 }
