d76b0d80ccaf7e83eadcd21a247d43596591904f
Dante le Poole <dantelepoole@gmail.com>
2022-11-21 22:11:42 -0400

    Refactor

--- uniqfilter.js
+++ uniqfilter.js
@@ -7,19 +7,17 @@
 const bind = require('./bind');
 
 /**
- * Return a filter function that accepts only unique values and rejects duplicates. The returned filter function 
- * can be passed to {@link module:predicate predicate()} to be used in a transducer, or can be used anywhere a filter
- * function is expected (e.g. {@link external:Array.prototype.filter Array.prototype.filter()}).
+ * Return a filter function that accepts only unique values and rejects duplicates.
  * 
  * The returned filter function maintains a cache of all values passed through the filter. So you should never keep
  * a uniqfilter-instance around indefinitely, that would cause a memory leak. Instead, call `uniqfilter()` to create a
  * new filter each time you need one and let the garbage collector collect it as soon as you are finished with it.
  * 
- * Nevertheless, you can call the `clearcache()` method of the returned filter function to explicitly clear its cache
+ * Nevertheless, you can call the `clear()` method of the returned filter function to explicitly clear its cache
  * of all entries.
  * 
  * For the same reason, a uniqfilter-instance is not reusable, since on subsequent runs it will recognize the values
- * from earlier runs as being duplicates (unless you call `clearcache()` inbetween runs).
+ * from earlier runs as being duplicates (unless you call `clear()` inbetween runs).
  * 
  * @example
  * 
@@ -28,21 +26,17 @@ const bind = require('./bind');
  * const filter = uniqfilter();
  * [1,2,2,3,3,3,4,4,4,4,5,5,5,5,5].filter(filter); // returns [1,2,3,4,5]
  * 
- * filter.clearcache();
+ * filter.clear();
  * 
  * @func uniqfilter
- * @see {@link module:predicate predicate()}
  * @returns {function}
  */
 module.exports = function uniqfilter() {
 
-    const uniqitems = new Set();
-    
-    uniqtransformation.clearcache = bind('clear', uniqitems);
+    const duplicateitems = new Set();
+    const uniqfilter = item => (duplicateitems.size !== duplicateitems.add(item).size);
 
-    return uniqtransformation;
-    
-    function uniqtransformation(value) {
-        return (uniqitems.size !== uniqitems.add(value).size);
-    }
+    uniqfilter.clear = bind('clear', duplicateitems);
+
+    return uniqfilter;
 }
