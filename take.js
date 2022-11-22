c2b9ba746e96a5e144adf517d8c286254aaab08a
Dante le Poole <dantelepoole@gmail.com>
2022-11-21 21:55:46 -0400

    Refactor to only work with iterables

--- take.js
+++ take.js
@@ -4,68 +4,61 @@
 
 'use strict';
 
-const ERR_BAD_COUNT = `TakeError~The count %s. Expected a positive integer number.`;
-const ERR_BAD_LIST = `TakeError~The list has type %s. Expected an object with a slice() method or an iterable object.`;
+const ERR_BAD_COUNT = `TakeError~The itemcount %s. Expected a positive integer number.`;
+const ERR_BAD_LIST = `TakeError~The list has type %s. Expected an iterable object.`;
 
 const and = require('./and');
 const fail = require('./fail');
-const isfunction = require('./isfunction');
 const isgreaterthanorequal = require('./isgreaterthanorequal');
 const isinteger = require('./isinteger');
-const isiterable = require('./isiterable');
 const isnumber = require('./isnumber');
+const notiterable = require('./notiterable');
 const typeorclass = require('./typeorclass');
 
 const ispositiveinteger = and(isinteger, isgreaterthanorequal(0));
-const issliceable = x => isfunction(x?.slice);
 
 /**
- * If *list* has a `slice()`, invoke it as `slice(0, *count*)` and return the result. If *list* is an iterable object,
- * return an iterable object containing the first *count* items produced by *list*. Otherwise, throw an error.
+ * Return an iterable object containing the first *itemcount* items produced by *list*.
  * 
  * `take()` is curried by default with binary arity.
  * 
  * @func take
- * @param {number} count The number of items to take from *list*
- * @param {iterable} list An object with a `slice()` method or an iterable object.
- * @returns {any}
+ * @param {number} itemcount The number of items to take from *list*
+ * @param {iterable} list An iterable object.
+ * @returns {iterable}
  */
 module.exports = require('./curry2')(
 
-    function take(count, list) {
+    function take(itemcount, list) {
 
-        checkcount(count);
+        checkcount(itemcount);
 
-        count = Math.max(0, count);
+        itemcount = Math.max(0, itemcount);
 
-        return issliceable(list) ? list.slice(0, count)
-             : isiterable(list) ? takeiterable(count, list)
-             : fail(ERR_BAD_LIST, typeorclass(list));
+        notiterable(list) && fail(ERR_BAD_LIST, typeorclass(list));
+
+        return {
+            [Symbol.iterator] : function* () {
+    
+                let counter = 0;
+    
+                for(const item of list) {
+                    
+                    if( isgreaterthanorequal(itemcount, counter) ) break;
+    
+                    counter += 1;
+    
+                    yield item;
+                }
+            }
+        }
 
     }
 )
 
 function checkcount(count) {
 
-    return ispositiveinteger(count) ? count
-         : fail(ERR_BAD_COUNT, isnumber(count) ? `is ${count}` : `has type ${typeorclass(count)}`);
-}
+    if( ispositiveinteger(count) ) return count;
 
-function takeiterable(itemcount, list) {
-
-    return {
-        [Symbol.iterator] : function* () {
-
-            let counter = 0;
-
-            for(const item of list) {
-                
-                if( isgreaterthanorequal(itemcount, counter) ) break;
-
-                counter += 1;
-
-                yield item;
-            }
-        }
-    }
+    fail(ERR_BAD_COUNT, isnumber(count) ? `is ${count}` : `has type ${typeorclass(count)}`);
 }
--- test/take.js
+++ test/take.js
@@ -24,53 +24,18 @@ describe(`take()`, function() {
         function () {
             const curried = take(3);
             expect(curried).to.be.a('function');
-            expect(curried('foobar')).to.be.a('string');
+            expect( Array.from(curried(range(5)))).to.deep.equal([1,2,3]);
         }
     )
 
-    it(`should invoke the list's slice() method if it has one`,
-        function () {
-
-            const sliceable = spyslice(
-                {
-                    slice(...args) {
-                        expect( args.length ).to.equal(2);
-                        return args;
-                    }
-                }
-            )
-
-            expect( take(3, sliceable) ).to.deep.equal([0,3]);
-            expect( sliceable.slice.calledOnceWith(0,3) ).to.be.true;
-        }
-    )
-
-    it(`should return a string with the first specified number of characters if the list is a string`,
-        function () {
-
-            const string = spyslice('foobar');
-            expect( take(3, string) ).to.equal('foo');
-            expect( string.slice.calledOnceWith(0,3) ).to.be.true;
-        }
-    )
-
-    it(`should return an array with the first specified number of items if the list is an array`,
-        function () {
-
-            const array = spyslice([1,2,3,4,5]);
-            expect( take(3, array) ).to.deep.equal([1,2,3]);
-            expect( array.slice.calledOnceWith(0,3) ).to.be.true;
-        }
-    )
-
-    it(`should return an iterable object producing the first specified number of items if the list is an iterable object`,
+    it(`should return an iterable object producing the first specified number of items in the list`,
         function () {
             expect( isiterable( take(3, range(5)) ) ).to.be.true;
             expect( Array.from(take(3, range(5))) ).to.deep.equal([1,2,3]);
         }
     )
 
-    it(`should throw if the list is neither sliceable nor iterable`,
+    it(`should throw if the list is not iterable`,
         function () {
             expect( ()=>take(3,{}) ).to.throw();
         }
@@ -81,7 +46,6 @@ describe(`take()`, function() {
             expect( ()=>take(0, []) ).not.to.throw();
             expect( ()=>take(42, []) ).not.to.throw();
             expect( ()=>take(-1, []) ).to.throw();
-            expect( ()=>take('foobar', []) ).to.throw();
         }
     )
 })
