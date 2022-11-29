f5237f9f1cae903c4e8a8641dfeca2355f92fb1b
Dante le Poole <dantelepoole@gmail.com>
2022-11-28 22:39:20 -0400

    Initial import

---
+++ transform/transform.js
@@ -0,0 +1,25 @@
+/**
+ * @module transform
+ */
+
+'use strict';
+
+const ERR_BAD_LIST = `TransformError~The list has type %s. Expected an iterable object.`;
+const ERR_BAD_TRANSFORMATIONS = `TransformError~The transformations argument has type %s. Expected an array of functions.`;
+
+const curry2 = require('../curry2');
+const fail = require("../fail");
+const notarray = require('../notarray');
+const notiterable = require('../notiterable');
+const transformer = require('./transformer');
+const typeorclass = require("../typeorclass");
+
+module.exports = curry2(transform);
+
+function transform(transformations, list) {
+
+    notarray(transformations) && fail(ERR_BAD_TRANSFORMATIONS, typeorclass(transformations));
+    notiterable(list) && fail(ERR_BAD_LIST, typeorclass(list));
+
+    return transformer(...transformations)(list);
+}
---
+++ transform/transformer.js
@@ -0,0 +1,60 @@
+/**
+ * @module transformer
+ */
+
+'use strict';
+
+const ERR_BAD_LIST = `TransformerError~The list has type %s. Expected an iterable object.`;
+const ERR_BAD_TRANSFORMATION = `TransformerError~The transformation has type %s. Expected a function.`;
+const FILTER_INCLUDE = true;
+const FILTER_REJECT = false;
+
+const fail = require("../fail");
+const map = require('../map');
+const notfunction = require("../notfunction");
+const notiterable = require('../notiterable');
+const typeorclass = require("../typeorclass");
+
+const validatetransformations = map(
+    transformation => notfunction(transformation) 
+                       &&
+                      fail(ERR_BAD_TRANSFORMATION, typeorclass(transformation))
+)
+
+module.exports = function transformer(...transformations) {
+
+    validatetransformations(transformations);
+
+    return function functionish_transform(list) {
+
+        notiterable(list) && fail(ERR_BAD_LIST, typeorclass(list));
+
+        return transform(transformations, list);
+    }
+}
+
+function transform(transformations, list) {
+
+    return {
+        [Symbol.iterator]: function* () {
+
+            for(let value of list) {
+
+                let index = 0;
+
+                while(index < transformations.length) {
+
+                    const result = transformations[index](value);
+
+                    if(result !== FILTER_INCLUDE) value = result;
+
+                    if(value === FILTER_REJECT) break;
+
+                    index += 1;
+                }
+
+                if(value !== FILTER_REJECT) yield value;
+            }
+        }
+    }
+}
