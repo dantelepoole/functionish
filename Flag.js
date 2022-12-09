0e2f55a4b8f5894dc77d60e602c906fddb8e9afa
Dante le Poole <dantelepoole@gmail.com>
2022-12-08 21:02:57 -0400

    Initial import

---
+++ Flag.js
@@ -0,0 +1,63 @@
+/**
+ * @class Flag
+ */
+
+'use strict';
+
+const bool = x => !!x;
+
+class Flag {
+
+    static get FALSE() {
+        return new Flag(false);
+    }
+
+    static get TRUE() {
+        return new Flag(true);
+    }
+
+    #value = false;
+
+    constructor(initialvalue=false) {
+        this.#value = bool(initialvalue);
+    }
+
+    flip() {
+        return (this.#value = ! this.#value);
+    }
+
+    is(value) {
+        
+        const valuetype = typeof value;
+
+        return (valuetype === 'boolean') ? (value === this.#value)
+             : (valuetype === 'object' && (value instanceof Flag)) ? (value.#value === this.#value)
+             : (bool(value) === this.#value);
+    }
+
+    isFalse() {
+        return (! this.#value);
+    }
+
+    isTrue() {
+        return this.#value;
+    }
+
+    set(newvalue) {
+        return (this.#value = bool(newvalue));
+    }
+
+    toBool() {
+        return this.#value;
+    }
+
+    toString() {
+        return `Flag[$${this.#value}]`;
+    }
+
+    value() {
+        return this.#value;
+    }
+}
+
+module.exports = Flag;
---
+++ test/Flag.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect;
+const Flag = require('../Flag');
+
+describe(`Flag`, function() {
+
+    it(`should be tested`);
+    
+})
