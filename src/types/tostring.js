80025396ad13b0edb651e1ce55ae202ecaae2164
Dante le Poole <dantelepoole@gmail.com>
2023-01-01 00:34:50 -0400

    Move to src/types and edit JSDoc comment

--- src/types/tostring.js
+++ src/types/tostring.js
@@ -1,5 +1,5 @@
 /**
- * @module misc/tostring
+ * @module types/tostring
  */
 
 'use strict';
@@ -8,10 +8,10 @@ const EMPTY_STRING = '';
 
 /**
  * Return a string representation of *value*, converting `null` and `undefined` to empty strings instead of
- * 'null', 'undefined' like the Javascript {@link external:String String()} function does. Otherwise, this
- * function behaves identically to {@link external:String String()}.
+ * 'null', 'undefined' like the Javascript {@link external:String String()} function does.
  * 
- * @func tostring
+ * @function tostring
+ * @see {@link external:String String()}
  * @param {any} value The value to convert to a string
  * @returns {string}
  */
