0f30056a5576f19ab107cd36c7caa06df9cbed12
Dante le Poole <dantelepoole@gmail.com>
2023-01-03 21:49:35 -0400

    Refactor

--- src/select.js
+++ src/select.js
@@ -4,33 +4,42 @@
 
 'use strict';
 
+const DEFAULT_PREDICATE = () => true;
+const DATAAWARE_ID = (expression, args) => args[0];
+
 const always = require('./always');
 const compose = require('./compose');
+const id = require('./id');
+const isdefined = require('./isdefined');
 const isfunction = require('./types/isfunction');
 const last = require('./collections/last');
+const map = require('./map');
+const partial = require('./partial');
 const pop = require('./collections/pop');
 const when = require('./when');
 
-const expressbranch = branch => (expression, args) => branch(...args);
-const expresspredicate = predicate => expression => predicate(expression);
+const expressionizeclause = clause => [predicatedata => clause[0](predicatedata), (_, args) => clause[1](...args)];
+const expressionize = partial(map, expressionizeclause);
 
 const hasdefaultbranch = compose(isfunction, last);
-const getdefaultbranch = when(hasdefaultbranch, pop, always(id));
-const getdefaultexpressbranch = compose(expressbranch, getdefaultbranch);
+const initdefaultbranch = clauses = hasdefaultbranch(clauses) && clauses.push( [DEFAULT_PREDICATE, pop(clauses)] );
 
-const expressreducer = (nextclause, clause) => when(expresspredicate(clause[0]), expressbranch(clause[1]), nextclause);
-const simplereducer = (nextclause, clause) => when(clause[0], clause[1], nextclause);
-
-const simpleselect = clauses => clauses.reduceRight(simplereducer, getdefaultbranch(clauses));
+const selectreducer = (nextclause, clause) => when(clause[0], clause[1], nextclause);
+const simpleselect = clauses => clauses.reduceRight(selectreducer, id);
 
 module.exports = function select(expression, ...clauses) {
-    return isvoid(expression) ? simpleselect(clauses) : expresselect(expression, clauses);
+
+    initdefaultbranch(clauses);
+
+    return isdefined(expression) ? expressionselect(expression, clauses)
+                                 : simpleselect(clauses);
 }
 
-function expresselect(expression, clauses) {
+function expressionselect(expression, clauses) {
 
-    const select = clauses.reduceRight(expressreducer, getdefaultexpressbranch(clauses));
+    clauses = expressionize(clauses);
 
-    return (...args) => select( expression(...args), args );
+    const executeselect = clauses.reduceRight(selectreducer, DATAAWARE_ID);
 
+    return (...args) => executeselect( expression(...args), args );
 }
