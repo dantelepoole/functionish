1109ef3082219689a3f9feb9e1e2589c638bc494
Dante le Poole <dantelepoole@gmail.com>
2023-01-05 20:57:08 -0400

    Major refactor

--- src/select.js
+++ src/select.js
@@ -5,41 +5,50 @@
 'use strict';
 
 const DEFAULT_PREDICATE = () => true;
-const DATAAWARE_ID = (expression, args) => args[0];
+const CONTEXTUAL_ID = context => context.args[0];
+const SIMPLE_ID = x => x;
 
-const always = require('./always');
 const compose = require('./compose');
-const id = require('./id');
-const isdefined = require('./isdefined');
+const curry2 = require('./curry2');
+const curry3 = require('./curry3');
 const isfunction = require('./types/isfunction');
+const isvoid = require('./types/isvoid');
 const last = require('./collections/last');
-const map = require('./map');
-const partial = require('./partial');
 const pop = require('./collections/pop');
 const when = require('./when');
 
-const expressionizeclause = clause => [predicatedata => clause[0](predicatedata), (_, args) => clause[1](...args)];
-const expressionize = partial(map, expressionizeclause);
+const map = curry2(__dirname + '/map');
+const push = curry2(__dirname + './collections/push');
+const reduceright = curry3(__dirname + '/lists/reduceright');
 
+const defaultclausefactory = defaultbranch => [DEFAULT_PREDICATE, defaultbranch];
 const hasdefaultbranch = compose(isfunction, last);
-const initdefaultbranch = clauses = hasdefaultbranch(clauses) && clauses.push( [DEFAULT_PREDICATE, pop(clauses)] );
+const initdefaultclause = compose(defaultclausefactory, pop);
+const initdefaultbranch = clauses => push( clauses, initdefaultclause(clauses) );
+const initclauses = when(hasdefaultbranch, initdefaultbranch);
 
 const selectreducer = (nextclause, clause) => when(clause[0], clause[1], nextclause);
-const simpleselect = clauses => clauses.reduceRight(selectreducer, id);
+const selector = reduceright(selectreducer);
+const selectcontextual = selector(CONTEXTUAL_ID);
+const selectsimple = selector(SIMPLE_ID);
+
+const contextfactory = expression => (...args) => ( { test:expression(...args), args } );
+
+const contextualbranch = clause => context => clause[1](...context.args);
+const contextualpredicate = clause => context => clause[0](context.test);
+
+const contextualize = clause => [ contextualpredicate(clause), contextualbranch(clause) ];
+const contextualizeall = map(contextualize);
+const contextualselectorfactory = compose(selectcontextual, contextualizeall);
+
+const contextualselector = (expression, clauses) => compose(
+    contextualselectorfactory(clauses),
+    contextfactory(expression)
+)
 
 module.exports = function select(expression, ...clauses) {
 
-    initdefaultbranch(clauses);
+    initclauses(clauses);
 
-    return isdefined(expression) ? expressionselect(expression, clauses)
-                                 : simpleselect(clauses);
-}
-
-function expressionselect(expression, clauses) {
-
-    clauses = expressionize(clauses);
-
-    const executeselect = clauses.reduceRight(selectreducer, DATAAWARE_ID);
-
-    return (...args) => executeselect( expression(...args), args );
+    return isvoid(expression) ? selectsimple(clauses) : contextualselector(expression, clauses);
 }
