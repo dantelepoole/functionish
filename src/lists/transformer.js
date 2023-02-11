06702c501d140c1c5c6a7e236a0df692b87cd0a8
Dante le Poole <dantelepoole@gmail.com>
2023-02-10 23:58:22 -0400

    Refactor

--- src/lists/transformer.js
+++ src/lists/transformer.js
@@ -4,25 +4,71 @@
 
 'use strict';
 
-const CONTEXT_NONE = undefined;
 const TRANSFORM_REJECT = Symbol.for('functionish/transform/TRANSFORM_REJECT');
 
 const id = require('../id');
+const isarray = require('../types/isarray');
+const isfunction = require('../types/isfunction');
 const notboolean = require('../types/notboolean');
 
-const transformreducer = (transform, transformation) => _transform.bind(CONTEXT_NONE, transform, transformation);
+const _transform = transformation => source => isfunction(source)
+                                             ? transducer(transformation, source)
+                                             : listtransformer(transformation, source);
 
 function transformer(...transformations) {
-    return transformations.reduceRight(transformreducer, id)
+
+    const transformation = transformations.reduceRight(transformationreducer, id);
+
+    return _transform(transformation);
 }
 
-function _transform(transform, transformation, value) {
+function listtransformer(transformation, list) {
 
-    const result = transformation(value);
+    const transformedlist = transformingiterable(transformation, list);
 
-    return notboolean(result) ? transform(result)
-         : result ? transform(value)
-         : TRANSFORM_REJECT;
+    return isarray(list)
+         ? Array.from(transformedlist)
+         : transformedlist;
+}
+
+function transformingiterable(transformation, list) {
+
+    return {
+
+        [Symbol.iterator] : function* () {
+
+            for(const value of list) {
+
+                const result = transformation(value);
+
+                if(result !== TRANSFORM_REJECT) yield result;
+            }
+        }
+    }
+}
+
+function transducer(transformation, reducer) {
+
+    return function _transformreducer(current, nextvalue) {
+
+        nextvalue = transformation(nextvalue);
+
+        return (nextvalue === TRANSFORM_REJECT)
+             ? current
+             : reducer(current, nextvalue);
+    }
+}
+
+function transformationreducer(nexttransformation, transformation) {
+
+    return function transformvalue(value) {
+
+        const result = transformation(value);
+
+        return notboolean(result) ? nexttransformation(result)
+             : result ? nexttransformation(value)
+             : TRANSFORM_REJECT;
+    }
 }
 
 module.exports = transformer;
