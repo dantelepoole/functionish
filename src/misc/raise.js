0488aaa5b71e31384c5e4e1695a04991a2617ba1
Dante le Poole <dantelepoole@gmail.com>
2023-02-15 21:26:24 -0400

    Initial import

---
+++ src/misc/raise.js
@@ -0,0 +1,22 @@
+/**
+ * @module misc/raise
+ */
+
+'use strict';
+
+/**
+ * Functional variant of Javascript's `throw` operator.
+ * 
+ * @example <caption>Example usage of `raise()`</caption>
+ * 
+ * raise( new Error() ); // throws the new Error
+ * 
+ * @function raise
+ * @param {any} error The value to throw
+ * @throws {any} The argument
+ */
+function raise(error) {
+    throw error;
+}
+
+module.exports = raise;
