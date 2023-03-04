e86ef12e7dddc0ae277e28c45ba50f47673bd83e
Dante le Poole <dantelepoole@gmail.com>
2023-03-04 19:58:49 -0400

    Initial import

---
+++ src/math/iseven.js
@@ -0,0 +1,25 @@
+/**
+ * @module math/iseven
+ */
+
+'use strict';
+
+/**
+ * Return `true` if *value* is divisible by `2`.
+ * 
+ * @example
+ * const iseven = require('functionish/math/iseven');
+ * 
+ * iseven(42); // returns true
+ * iseven(1); // returns false
+ * iseven(0); // returns true
+ * 
+ * @function iseven
+ * @param {number} value The value to check
+ * @returns {boolean}
+ */
+function iseven(value) {
+    return (value % 2) === 0;
+}
+
+module.exports = iseven;
---
+++ src/math/isodd.js
@@ -0,0 +1,25 @@
+/**
+ * @module math/isodd
+ */
+
+'use strict';
+
+/**
+ * Return `true` if *value* is not divisible by `2`.
+ * 
+ * @example
+ * const isodd = require('functionish/math/isodd');
+ * 
+ * isodd(42); // returns false
+ * isodd(1); // returns true
+ * isodd(0); // returns false
+ * 
+ * @function isodd
+ * @param {number} value The value to check
+ * @returns {boolean}
+ */
+function isodd(value) {
+    return (value % 2) !== 0;
+}
+
+module.exports = isodd;
