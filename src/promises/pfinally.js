432025c7afe96be2f1ef68839834d048433ded4f
Dante le Poole <dantelepoole@gmail.com>
2023-03-04 21:34:28 -0400

    Refactor

--- src/promises/pfinally.js
+++ src/promises/pfinally.js
@@ -4,10 +4,19 @@
 
 'use strict';
 
+const compose = require('../compose');
 const curry = require('../curry');
+const flip = require('../flip');
+const raise = require('../misc/raise');
+const tap = require('../tap');
+
+const initreject = compose(raise, tap);
+const initresolve = compose(tap, flip);
 
 /**
  * Functional variant of {@link external:Promise.prototype.finally Promise.prototype.finally()}.
+ * 
+ * [to do: fulfilled value is passed]
  * 
  * `pfinally()` is curried by default with unary arity.
  * 
@@ -17,7 +26,11 @@ const curry = require('../curry');
  * @returns {Promise}
  */
 function pfinally(onfinally, promise) {
-    return promise.finally(onfinally);
+
+    const onresolve = initresolve(onfinally);
+    const onreject = initreject(onfinally);
+
+    return promise.then(onresolve, onreject);
 }
 
 module.exports = curry(1, pfinally);
