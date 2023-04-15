21e96b91fc1e4b375ac2b3c6c0e1a3e12af68022
Dante le Poole <dantelepoole@gmail.com>
2023-04-15 10:19:10 -0400

    Refactor

--- src/promises/pcompose.js
+++ src/promises/pcompose.js
@@ -4,13 +4,15 @@
 
 'use strict';
 
-const PROMISE_NONE = undefined;
+const PROMISE_NONE = Symbol('functionish/promises/pcompose#PROMISE_NONE');
 
 const promisereducer = args => (promise, func) => (promise === PROMISE_NONE) ? func(...args) : promise.then(func);
 
 function pcompose(...funcs) {
 
-    const _pcompose = (...args) => funcs.reduceRight(promisereducer(args), PROMISE_NONE);
+    const _pcompose = (funcs.length > 0)
+                    ? (...args) => funcs.reduceRight( promisereducer(args), PROMISE_NONE )
+                    : (...args) => Promise.resolve(args[0]);
 
     return _pcompose;
 }
