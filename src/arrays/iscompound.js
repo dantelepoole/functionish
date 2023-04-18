55f2cd83a08e0931bcda19ae229f95cdf997b071
Dante le Poole <dantelepoole@gmail.com>
2023-04-17 21:26:54 -0400

    Initial import

---
+++ src/arrays/iscompound.js
@@ -0,0 +1,20 @@
+/**
+* @module arrays/iscompound
+*/
+
+'use strict';
+
+/**
+ * Return `true` if the value of *collection*'s `length` property equals `1`. If *collection* has no `length` property,
+ * its `size` property is checked instead. If *collection* has neither a `length` property nor a `size` property, this
+ * function returns `false`.
+ * 
+ * @func iscompound
+ * @param {any} collection An object with a numeric `length` or `size` property.
+ * @returns {boolean}
+ */
+function iscompound(collection) {
+    return (collection.length ?? collection.size) > 1;
+}
+
+module.exports = iscompound;
