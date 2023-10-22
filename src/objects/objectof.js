e85981255ff6fe56dfc7198da988f04966af4d77
Dante le Poole <dantelepoole@gmail.com>
2023-10-22 13:59:09 -0400

    Refactor

--- src/objects/objectof.js
+++ src/objects/objectof.js
@@ -4,11 +4,7 @@
 
 'use strict';
 
-const TYPE_STRING = 'string';
-
 const curry = require('../curry');
-
-const notarray = require('../types/notarray)');
 
 /**
  * to do
@@ -20,21 +16,9 @@ const notarray = require('../types/notarray)');
  * to do
  * 
  * @function objectof
- * @param {(string|string[])} key The property key or array of property keys to populate the object with
- * @param {...any[]} values The value or values to assign to the object's property or properties
- * @return {object}
  */
-function objectof(key, ...values) {
-
-    return (typeof key === TYPE_STRING || notarray(key))
-         ? { [key]:values[0] }
-         : populate({}, key, values)
+function objectof(key, value) {
+    return { [key]:value }
 }
 
-function populate(target, keys, values) {
-
-    for(let index = 0; index < keys.length; index += 1) target[ keys[index] ] = values[index];
-
-    return target;
-}
 module.exports = curry(1, objectof);
