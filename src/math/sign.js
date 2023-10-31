e5f23197d18ce24f4179127568d4879f107beee0
Dante le Poole <dantelepoole@gmail.com>
2023-10-30 22:04:05 -0400

    Initial import

---
+++ src/math/sign.js
@@ -0,0 +1,17 @@
+/**
+ * @module math/sign
+ */
+
+'use strict';
+
+/**
+ * to do
+ * 
+ * @example
+ * to do
+ * 
+ * @function sign
+ * @param {number} value The value to retrieve the sign of
+ * @returns {boolean}
+ */
+module.exports = Math.sign;
