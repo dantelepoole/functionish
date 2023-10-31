b9a49245dca4fc00aa18a017f3f447716b3a24f2
Dante le Poole <dantelepoole@gmail.com>
2023-10-30 22:03:49 -0400

    Initial import

---
+++ src/math/cuberoot.js
@@ -0,0 +1,18 @@
+/**
+ * @module math/cuberoot
+ */
+
+'use strict';
+
+/**
+ * [to do]
+ * 
+ * @example
+ * 
+ * [to do]
+ * 
+ * @function cuberoot
+ * @param {number} value The value to calculate the cube root of
+ * @returns {number}
+ */
+module.exports = Math.cbrt;
