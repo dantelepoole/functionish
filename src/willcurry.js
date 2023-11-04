7ed1eca6b0834dc892337c3c5401631b7c848713
Dante le Poole <dantelepoole@gmail.com>
2023-11-04 09:39:14 -0400

    Initial import

---
+++ src/willcurry.js
@@ -0,0 +1,21 @@
+/**
+ * @module willcurry
+ */
+
+'use strict';
+
+const { CurryArity } = require('../lib/FSymbol');
+
+/**
+ * to do
+ * 
+ * @function willcurry
+ * @param {function} targetfunc
+ * @param {any[]} argcount
+ * @returns {boolean}
+ */
+function willcurry(targetfunc, argcount) {
+    return (targetfunc[CurryArity] >= argcount);
+}
+
+module.exports = willcurry;
