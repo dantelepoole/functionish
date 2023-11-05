878450ac984068b138eba83f0e9b065c1bf7237b
Dante le Poole <dantelepoole@gmail.com>
2023-11-05 11:31:40 -0400

    Refactor

--- src/lists/zip.js
+++ src/lists/zip.js
@@ -5,31 +5,35 @@
 'use strict';
 
 const ERR_BAD_MODE = `functionish/lists/zip(): The mode argument is '%s'. Expected 'left', 'right', 'long' or 'short'.`;
+const VALID_ZIP_MODES = ['left', 'long', 'right', 'short'];
+const MISSING_VALUE = undefined;
 
-const ZIP_MODES = {
-    LEFT  : 'left',
-    LONG  : 'long',
-    RIGHT : 'right',
-    SHORT : 'short'
+const ZIPFUNCTIONS = {
+    left  : zipleft,
+    long  : ziplong,
+    right : zipright,
+    short : zipshort
 }
 
-const VALID_ZIP_MODES = [ZIP_MODES.LEFT, ZIP_MODES.LONG, ZIP_MODES.RIGHT, ZIP_MODES.SHORT];
 
 const compose = require('../compose');
 const curry = require('../curry');
 const error = require('../errors/error');
 const flip = require('../flip');
 const includes = require('./includes');
-const isstring = require('../types/isstring');
+const iterator = require('./iterator');
 const list = require('./list');
 const lowercase = require('../misc/lowercase');
+const or = require('../logic/or');
+const partial = require('../partial');
 const raise = require('../errors/raise');
 const stringlimit = require('../misc/stringlimit');
 
-const raisebadmode = compose(raise, error.Type(ERR_BAD_MODE), stringlimit('...', 32));
+const normalizemode = compose(lowercase, String);
 
-const isvalidmode = compose( flip(includes, VALID_ZIP_MODES), lowercase );
-const validatemode = mode => isstring(mode) && isvalidmode(mode) || raisebadmode(mode);
+const isvalidmode = compose(flip(includes, VALID_ZIP_MODES));
+const raisebadmode = compose(raise, error.Type(ERR_BAD_MODE), stringlimit('...', 10));
+const validatemode = or(isvalidmode, raisebadmode);
 
 /**
  * Return an iterable that produces 2-element arrays containing the next item from *list1* and
@@ -51,25 +55,100 @@ const validatemode = mode => isstring(mode) && isvalidmode(mode) || raisebadmode
  * @param {iterable} list2 The iterable to zip the items from *list1* with
  * @returns {iterable}
  */
-function zip(mode=ZIP_MODES.SHORT, list1, list2) {
+function zip(mode='short', list1, list2) {
+
+    mode = normalizemode(mode);
 
     validatemode(mode);
+
+    const ziplists = partial(ZIPFUNCTIONS[mode], list1, list2);
+    
+    return list(ziplists);
 }
 
-function zipleft(list1, list2) {
+function* zipleft(left, right) {
 
+    const leftiterator = iterator(left);
+    const rightiterator = iterator(right);
+
+    let leftitem = leftiterator.next();
+
+    while( !leftitem.done ) {
+
+        const rightitem = rightiterator.next();
+
+        yield rightitem.done
+            ? [leftitem.value, MISSING_VALUE]
+            : [leftitem.value, rightitem.value];
+
+        leftitem = leftiterator.next();
+    }
 }
 
-function zipright(list1, list2) {
+function* zipright(left, right) {
 
+    const leftiterator = iterator(left);
+    const rightiterator = iterator(right);
+
+    let rightitem = rightiterator.next();
+
+    while( !rightitem.done ) {
+
+        const leftitem = leftiterator.next();
+
+        yield leftitem.done
+            ? [MISSING_VALUE, rightitem.value]
+            : [leftitem.value, rightitem.value];
+
+        rightitem = rightiterator.next();
+    }
 }
 
 function ziplong(list1, list2) {
+
+    const iterator1 = iterator(list1);
+    const iterator2 = iterator(list2);
+
+    let item1 = iterator1.next();
+    let item2 = iterator2.next();
+
+    while( !item1.done ) {
+        
+        yield item2.done
+            ? [item1.value, MISSING_VALUE]
+            : [item1.value, item2.value];
+
+        item1 = iterator1.next();
+        item2 = iterator2.next();
+    }
+
+    while( !item2.done ) {
+        yield [MISSING_VALUE, item2.value];
+        item2 = iterator2.next();
+    }
 
 }
 
 function zipshort(list1, list2) {
 
+    const iterator1 = iterator(list1);
+    const iterator2 = iterator(list2);
+
+    let item1 = iterator1.next();
+    let item2 = iterator2.next();
+
+    while( ! (item1.done || item2.done) ) {
+        
+        yield [item1.value, item2.value];
+
+        item1 = iterator1.next();
+        item2 = iterator2.next();
+    }
+
+    while( !item2.done ) {
+        yield [MISSING_VALUE, item2.value];
+        item2 = iterator2.next();
+    }
 }
 
 module.exports = curry(1, zip);
