0c73ee9eb276cf47782d32ac4c05a827baca54fb
Dante le Poole <dantelepoole@gmail.com>
2023-11-21 12:27:54 -0400

    Edit

--- src/apply.js
+++ src/apply.js
@@ -4,8 +4,6 @@
 
 'use strict';
 
-const curry1 = require('./curry1');
-
 /**
  * Functional variant of {@link external:Function.prototype.apply Function.prototype.apply()}.
  * Pass *args* to *targetfunc* and return the result. `apply()` passes its own `this` value to
@@ -29,11 +27,11 @@ const curry1 = require('./curry1');
  * @param {any[]} args An array with the arguments to pass to *targetfunc*
  * @returns {any}
  */
-const apply = curry1(
+function apply(targetfunc, args) {
 
-    function apply(targetfunc, args) {
-        return targetfunc.apply(this, args);
-    }
-)
+    return (arguments.length === 1)
+         ? function _apply(argarray) { return targetfunc.apply(this, argarray) }
+         : targetfunc.apply(this, args);
+}
 
 module.exports = apply;
--- test/apply.js
+++ test/apply.js
@@ -2,8 +2,14 @@ const apply = require('../src/apply');
 const expect = require('chai').expect;
 const sinon = require("sinon");
 
+const THIS = {foo:'bar'}
+
 const collectargs = (...args) => args;
-const fakeapply = sinon.replace(collectargs, 'apply', sinon.fake( (THIS, args) => collectargs(THIS, ...args) ));
+const fakeapplycollectargs = sinon.replace(collectargs, 'apply', sinon.fake( (that, argarray) => collectargs.call(that, ...argarray) ) );
+
+function isTHIS() {
+    return (THIS === this);
+}
 
 describe( 'apply()', function() {
 
@@ -14,12 +20,55 @@ describe( 'apply()', function() {
         it(`should call the 'apply()' method of its first argument`,
 
             function() {
-                expect(fakeapply.callCount).to.equal(0);
+                expect(fakeapplycollectargs.callCount).to.equal(0);
                 apply(collectargs, []);
-                expect(fakeapply.callCount).to.equal(1);
+                expect(fakeapplycollectargs.callCount).to.equal(1);
             }
         )
 
-        it(`TO DO: implement additional tests`);
+        it(`should pass its own 'this' value' as the first argument to the 'apply()' method of its first argument`,
+
+            function() {
+                const isthis = apply.call(THIS, isTHIS, []);
+                expect(isthis).to.be.true;
+            }
+        )
+
+        it(`should pass its second argument as the second argument to the 'apply()' method of its first argument`,
+
+            function() {
+                const argarray = ['fubar', 42, {}];
+                apply(collectargs, argarray);
+                expect(fakeapplycollectargs.args[0][1]).to.deep.equal(argarray);
+            }
+        )
+
+        it(`should return the return value of the call to the 'apply()' method of its first argument`,
+
+            function() {
+                const argarray = ['fubar', 42, {}];
+                const retval = apply(collectargs, argarray);
+                expect(fakeapplycollectargs.callCount).to.equal(1);
+                expect(retval).to.be.deep.equal(argarray);
+            }
+        )
+
+        it(`should be curried with unary arity`,
+
+            function() {
+
+                const argarray = ['fubar', 42, {}];
+
+                expect(fakeapplycollectargs.callCount).to.equal(0);
+                
+                const applycollectargs = apply(collectargs);
+                expect(fakeapplycollectargs.callCount).to.equal(0);
+                expect(applycollectargs).to.be.a('function');
+
+                const retval = applycollectargs(argarray);
+                expect(fakeapplycollectargs.callCount).to.equal(1);
+                expect(retval).to.be.deep.equal(argarray);
+            }
+        )
     }
 );
