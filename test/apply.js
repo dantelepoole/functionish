2377526ecabd902b0f13abd20d15a7133acbf890
Dante le Poole <dantelepoole@gmail.com>
2023-11-21 13:02:33 -0400

    Restructure code

--- test/apply.js
+++ test/apply.js
@@ -17,58 +17,43 @@ describe( 'apply()', function() {
             sinon.reset();
         })
 
-        it(`should call the 'apply()' method of the target function`,
+        it(`should call the 'apply()' method of the target function`, function() {
+            expect(fakeapplycollectargs.callCount).to.equal(0);
+            apply(collectargs, []);
+            expect(fakeapplycollectargs.callCount).to.equal(1);
+        })
 
-            function() {
-                expect(fakeapplycollectargs.callCount).to.equal(0);
-                apply(collectargs, []);
-                expect(fakeapplycollectargs.callCount).to.equal(1);
-            }
-        )
+        it(`should pass its own 'this' value' as the first argument to the 'apply()' method of the target function`, function() {
+            const isthis = apply.call(THIS, isTHIS, []);
+            expect(isthis).to.be.true;
+        })
 
-        it(`should pass its own 'this' value' as the first argument to the 'apply()' method of the target function`,
+        it(`should pass its second argument as the second argument to the 'apply()' method of the target function`, function() {
+            const argarray = ['fubar', 42, {}];
+            apply(collectargs, argarray);
+            expect(fakeapplycollectargs.args[0][1]).to.deep.equal(argarray);
+        })
 
-            function() {
-                const isthis = apply.call(THIS, isTHIS, []);
-                expect(isthis).to.be.true;
-            }
-        )
+        it(`should return the return value of the call to the 'apply()' method of the target function`, function() {
+            const argarray = ['fubar', 42, {}];
+            const retval = apply(collectargs, argarray);
+            expect(fakeapplycollectargs.callCount).to.equal(1);
+            expect(retval).to.be.deep.equal(argarray);
+        })
 
-        it(`should pass its second argument as the second argument to the 'apply()' method of the target function`,
+        it(`should be curried with unary arity`, function() {
 
-            function() {
-                const argarray = ['fubar', 42, {}];
-                apply(collectargs, argarray);
-                expect(fakeapplycollectargs.args[0][1]).to.deep.equal(argarray);
-            }
-        )
+            const argarray = ['fubar', 42, {}];
 
-        it(`should return the return value of the call to the 'apply()' method of the target function`,
+            expect(fakeapplycollectargs.callCount).to.equal(0);
+            
+            const applycollectargs = apply(collectargs);
+            expect(fakeapplycollectargs.callCount).to.equal(0);
+            expect(applycollectargs).to.be.a('function');
 
-            function() {
-                const argarray = ['fubar', 42, {}];
-                const retval = apply(collectargs, argarray);
-                expect(fakeapplycollectargs.callCount).to.equal(1);
-                expect(retval).to.be.deep.equal(argarray);
-            }
-        )
-
-        it(`should be curried with unary arity`,
-
-            function() {
-
-                const argarray = ['fubar', 42, {}];
-
-                expect(fakeapplycollectargs.callCount).to.equal(0);
-                
-                const applycollectargs = apply(collectargs);
-                expect(fakeapplycollectargs.callCount).to.equal(0);
-                expect(applycollectargs).to.be.a('function');
-
-                const retval = applycollectargs(argarray);
-                expect(fakeapplycollectargs.callCount).to.equal(1);
-                expect(retval).to.be.deep.equal(argarray);
-            }
-        )
+            const retval = applycollectargs(argarray);
+            expect(fakeapplycollectargs.callCount).to.equal(1);
+            expect(retval).to.be.deep.equal(argarray);
+        })
     }
 );
