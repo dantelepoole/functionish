15cbb5cde03eeeb755314bc004e72921080989c4
Dante le Poole <dantelepoole@gmail.com>
2023-11-21 13:34:38 -0400

    Edit JSdoc comment

--- src/flip.js
+++ src/flip.js
@@ -5,14 +5,29 @@
 'use strict';
 
 /**
- * to do
+ * Return a function that calls *targetfunc* with its first two arguments swapped. Any further
+ * arguments passed to the flipped function are forwarded unchanged.
+ * 
+ * The flipped function is curried with unary arity, meaning that it requires at least two arguments
+ * to run. If you need the flipped function to run regardless of the number of arguments, pass the flipped
+ * function to @see {@link module:binary binary()}.
  * 
  * @example <caption>Example usage of `flip()`</caption>
  * 
- * [to do]
+ * const { flip } = require('functionish');
+ * 
+ * const sort = (a,b) => (a <= b) ? [a,b] : [b,a];
+ * const reversesort = flip(sort);
+ * 
+ * sort(41, 42); // returns [41, 42]
+ * sortreverse(41, 42); // returns [42, 41]
+ * 
+ * const sortreverse41 = sortreverse(41); // returns a curried function
+ * sortreverse41(42); // returns [42, 41]
+ * sortreverse41(40); // returns [41, 40]
  * 
  * @function flip
- * @param {function} targetfunc The function to flip the arguments of
+ * @param {function} targetfunc The function to flip the arguments for
  * @returns {function}
  */
 function flip(targetfunc) {
