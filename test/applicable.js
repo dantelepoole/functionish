4320e510bd869d0e8c7eb8678b0fca64e47057b5
Dante le Poole <dantelepoole@gmail.com>
2023-11-20 21:31:43 -0400

    initial import

---
+++ test/applicable.js
@@ -0,0 +1,43 @@
+const applicable = require('../src/applicable');
+const expect = require('chai').expect;
+
+const UNIQTHING = {}
+
+const collectargs = (...args) => args;
+const countargs = (...args) => args.length;
+const id = x => x;
+
+describe( 'applicable()', function() {
+
+        it('should return a function',
+            function() {
+                expect( applicable() ).to.be.a('function');
+            }
+        )
+
+        describe( 'The result function', function() {
+
+            it('should call its first argument as a function and pass all arguments passed to applicable() in order',
+                function() {
+                    const apply1 = applicable(UNIQTHING);
+                    expect( apply1(id) ).to.equal(UNIQTHING);
+
+                    const apply2 = applicable(UNIQTHING, 42, 'fubar', applicable);
+                    expect( apply2(collectargs) )
+                        .to.deep.equal( [UNIQTHING,42,'fubar', applicable] );
+
+                    const apply3 = applicable();
+                    expect( apply3(countargs) ).to.equal(0);
+                }
+            )
+
+            it('should throw if its argument is not a function',
+                function() {
+                    const apply1 = applicable(UNIQTHING);
+                    expect( () => apply1('not-a-function') ).to.throw;
+                }
+            )
+
+        })
+    }
+);
