a615e4f6957d1932a8f41464bc5af8667bdf1f6a
Dante le Poole <dantelepoole@gmail.com>
2023-11-22 18:52:04 -0400

    Restructure curry() code and edit JSDoc comment

--- src/defaultto.js
+++ src/defaultto.js
@@ -5,13 +5,14 @@
 'use strict';
 
 const compose = require('./compose');
-const curry = require('./curry');
+const curry1 = require('./curry1');
 const isvoid = require('./types/isvoid');
 
 const coalesce = defaultvalue => value => isvoid(value) ? defaultvalue : value;
 
 /**
- * [to do]
+ * Return a function that passes its arguments to *targetfunc* and returns its return value. If the return value
+ * is <abbrev caption="null or undefined">void</abbrev>, *defaultvalue* is returned instead.
  * 
  * `defaultto()` is curried by default with unary arity.
  * 
@@ -26,13 +27,13 @@ const coalesce = defaultvalue => value => isvoid(value) ? defaultvalue : value;
  * getuser(42);
  * 
  * @function defaultto
- * @param {any} defaultvalue The value to return if *func* returns a void value
+ * @param {any} defaultvalue The value to return if *targetfunc* returns a <abbrev caption="null or undefined">void</abbrev> value
  * @param {function} targetfunc The target function
  * @returns {function}
  */
-function defaultto(defaultvalue, targetfunc) {
+const defaultto = curry1(function defaultto(defaultvalue, targetfunc) {
     return compose( coalesce(defaultvalue), targetfunc );
-}
+})
 
 
-module.exports = curry(1, defaultto);
+module.exports = defaultto;
