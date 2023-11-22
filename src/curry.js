e23823ad97e6b2518983b053f8c01eb6db5396d6
Dante le Poole <dantelepoole@gmail.com>
2023-11-21 22:59:21 -0400

    Fix bug and edit JSDoc comment

--- src/curry.js
+++ src/curry.js
@@ -11,31 +11,36 @@ const and = require('./logic/and');
 const compose = require('./compose');
 const error = require('./errors/error');
 const isfunction = require('./types/isfunction');
+const isinteger = require('./types/isinteger');
 const ispositiveorzero = require('./math/ispositiveorzero');
+const or = require('./logic/or');
 const raise = require('./errors/raise');
 const type = require('./types/type');
 
-const isvalidarity = and(Number.isSafeInteger, ispositiveorzero);
-
 const badarityerror = compose(raise, error.Type(ERRORMSG_BAD_ARITY));
-const validatearity = arity => isvalidarity(arity) || badarityerror(arity);
+const isvalidarity = and(isinteger, ispositiveorzero);
+const validatearity = or(isvalidarity, badarityerror);
 
 const badfunctionerror = compose(raise, error.Type(ERRORMSG_BAD_FUNCTION), type);
-const validatefunction = func => isfunction(func) || badfunctionerror(func);
+const validatefunction = or(isfunction, badfunctionerror);
 
 const _curry = (targetfunc, arity, ...args) => (arity < args.length) ? targetfunc(...args)
                                              : (arity === args.length) ? targetfunc.bind(null, ...args)
                                              : applycurry(targetfunc, arity, args);
 
 /**
- * to do
+ * Return a function that curries the first *arity* number of arguments and invokes *targetfunc* only after
+ * *arity* + 1 arguments have been received.
+ * 
+ * The *arity* should be set to the minimum number of arguments to curry, i.e. one less than the number of
+ * arguments required to run *targetfunc*. If arity is `0`, *targetfunc* itself is returned. If 
  * 
  * @example <caption>Example usage of `curry()`</caption>
  * 
  * to do
  * 
  * @function curry
- * @param {number} arity The number of arguments to curry (one less than the minimum number of parameters required by *targetfunc*)
+ * @param {number} arity The number of arguments to curry
  * @param {function} targetfunc The function to curry
  * @returns {function}
  */
@@ -49,7 +54,7 @@ function curry(arity, targetfunc) {
 }
 
 function applycurry(targetfunc, arity, curriedargs=[]) {
-    return _curry.bind(null, targetfunc, arity - curriedargs.length, ...curriedargs);
+    return _curry.bind(null, targetfunc, arity, ...curriedargs);
 }
 
 module.exports = curry;
