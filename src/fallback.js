9011f2b52134d0227b5d3cb4496b065584ef3e2d
Dante le Poole <dantelepoole@gmail.com>
2023-11-22 21:27:57 -0400

    Edit JSDoc comment and rename parameter

--- src/fallback.js
+++ src/fallback.js
@@ -4,36 +4,44 @@
 
 'use strict';
 
-const THIS_NULL = null;
-
 const id = require('./id');
 const isdefined = require('./types/isdefined');
 const isvoid = require('./types/isvoid');
 
 /**
  * Return a function that passes its arguments to each *func* in order and returns the return value
- * of the first function that does not throw an error or return a <abbr title="null, undefined or NaN">void</abbr>
- * value. If the last *func* throws, the returned function also throws. If the last *func* returns a
- * <abbr title="null, undefined or NaN">void</abbr> value, the returned function returns `undefined`.
+ * of the first function that does not throw an error or a <abbr title="null or undefined">void</abbr>
+ * value.
+ * 
+ * If the last *func* throws, the returned function also throws. If the last *func* returns a
+ * <abbr title="null or undefined">void</abbr> value, the returned function returns `undefined`.
  * 
  * If the *funcs* array is empty, the returned function returns its first argument.
  * 
- * [to do: this]
- * 
  * @example <caption>Example usage of `fallback()`</caption>
  * 
+ * const { fallback } = require('functionish');
  * 
- * [to do]
+ * const id = x => x;
+ * const throwerror = x => { throw new Error(x) }
+ * const fortytwo = () => 42;
+ * 
+ * const execute = fallback(id, throwerror, fortytwo);
+ * 
+ * execute(null); // returns 42
+ * execute(); // returns 42
+ * execute(41); // returns 41
+ * 
  * 
  * @function fallback
- * @param {...function[]} funcs The array of functions to run in order
+ * @param {...function[]} targetfuncs The array of functions to run in order
  * @returns {any}
  */
-function fallback(...funcs) {
+function fallback(...targetfuncs) {
 
-    const finalfunc = funcs.pop() ?? id;
+    const finalfunc = targetfuncs.pop() ?? id;
 
-    return runfallback.bind(THIS_NULL, finalfunc, funcs);
+    return runfallback.bind(null, finalfunc, targetfuncs);
 }
 
 function runfallback(finalfunc, funcs, ...args) {
