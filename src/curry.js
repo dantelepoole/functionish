77f390146a06fb1227300b3a0ea5d53fe09ad34f
Dante le Poole <dantelepoole@gmail.com>
2023-11-23 18:24:04 -0400

    Edit JSDoc comment

--- src/curry.js
+++ src/curry.js
@@ -29,20 +29,37 @@ const _curry = (targetfunc, arity, ...args) => (arity < args.length) ? targetfun
                                              : applycurry(targetfunc, arity, args);
 
 /**
- * Return a function that curries the first *arity* number of arguments and invokes *targetfunc* only after
- * *arity* + 1 arguments have been received.
+ * Return a function that curries the first *arity* number of arguments on successive calls and invokes *targetfunc*
+ * only after at least *arity* arguments have been received.
  * 
  * The *arity* should be set to the minimum number of arguments to curry, i.e. one less than the number of
- * arguments required to run *targetfunc*. If arity is `0`, *targetfunc* itself is returned. If 
+ * arguments required to run *targetfunc*. If *arity* is `0`, *targetfunc* itself is returned. If *arity* is negative
+ * or not an integer, or if *targetfunc* is not a function, an error is thrown.
+ * 
+ * Once *arity* number of arguments have been curried, the currying function will always invoke *targetfunc*, even if
+ * no additional arguments are passed.
  * 
  * @example <caption>Example usage of `curry()`</caption>
  * 
- * to do
+ * const { curry } = require('functionish');
+ * 
+ * const add = curry( 1, (x,y) => (x+y) );
+ * 
+ * const increment = add(1);
+ * const decrement = add(-1);
+ * 
+ * increment(42); // returns 43
+ * decrement(42); // returns 41
+ * 
+ * increment(); // returns NaN even though no second argument was passed, because the arity was already reached
+ * 
+ * add(1,2); // returns 3 (no currying because the arity was exceeded on the first call)
  * 
  * @function curry
  * @param {number} arity The number of arguments to curry
  * @param {function} targetfunc The function to curry
  * @returns {function}
+ * @throws {TypeError} if *arity* is negative or not an integer, or if *targetfunc* is not a function
  */
 function curry(arity, targetfunc) {
 
