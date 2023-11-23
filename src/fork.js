5c33bd45c593ff95609f9f32293f02b0c78a4d7f
Dante le Poole <dantelepoole@gmail.com>
2023-11-22 22:41:05 -0400

    Fix join() bug and edit JSDoc comment

--- src/fork.js
+++ src/fork.js
@@ -4,26 +4,31 @@
 
 'use strict';
 
-const THIS_NULL = null;
-
 const applicable = require('./applicable');
 const compose = require('./compose');
+const uniadic = require('./uniadic');
 
-const runfork = (funcs, ...args) => funcs.map( applicable(...args) )
+const _fork = (forkfuncs, ...args) => forkfuncs.map( applicable(...args) )
 
 /**
- * [to do]
+ * Return a function that passes its arguments to each *targetfunc* and returns an array holding the return values.
+ * 
+ * The returned function has a method `join()` that accepts a join-function and returns a new function that passes
+ * the array of return values to the join-function as a rest parameter and returns its return value. 
  * 
  * @example <caption>Example usage of `fork()`</caption>
  * 
  * [to do]
  * 
+ * @function fork
+ * @param {...function[]} targetfuncs The functions to call
+ * @returns {function}
  */
 function fork(...targetfuncs) {
     
-    const forkrunner = runfork.bind(THIS_NULL, targetfuncs);
+    const forkrunner = _fork.bind(null, targetfuncs);
 
-    forkrunner.join = joinfunc => compose(joinfunc, forkrunner);
+    forkrunner.join = joinfunc => compose(uniadic(joinfunc), forkrunner);
 
     return forkrunner;
 }
