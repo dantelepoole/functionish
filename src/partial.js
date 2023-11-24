8010801b8a80e8f85c1556e56f4de5d88db450b0
Dante le Poole <dantelepoole@gmail.com>
2023-11-23 23:37:45 -0400

    Add argument type check and edit JSDoc comment

--- src/partial.js
+++ src/partial.js
@@ -4,31 +4,44 @@
 
 'use strict';
 
-const THIS_NULL = null;
+const ERR_BAD_TARGETFUNCTION = `functionish/partial(): The target function has type %'s'. Expected a function.`;
+
+const format = require('./misc/format');
+const isfunction = require('./types/isfunction');
+const typeorclassname = require('./types/typeorclassname');
 
 /**
- * [to do: this value]
+ * Return a function that pre-binds the *partialargs* to *targetfunc*, so that when the returned function is called
+ * it is passed the *partialargs* followed by its own arguments.
  * 
  * @example <caption>Example usage of `partial()`</caption>
  * 
  * const partial = require('functionish/partial')
- * function sum(a,b) {
- *     return (a+b)
- * }
+ * 
+ * const sum = (a,b) => (a+b);
  * 
  * const increment = partial(sum, 1);
+ * 
  * increment(42); // returns 43
  * 
  * @function partial
  * @param {function} targetfunc The function to partially apply
- * @param  {...any} partialargs Zero or more arguments to partially apply *targetfunc* with
+ * @param  {...any} partialargs The arguments to partially apply *targetfunc* to
  * @returns {function}
  */
 function partial(targetfunc, ...partialargs) {
 
-    return (partialargs.length > 0) && targetfunc.bind(THIS_NULL, ...partialargs) 
-            ||
-           targetfunc; 
+    validatetargetfunction(targetfunc);
+    
+    return (partialargs.length > 0) && targetfunc.bind(null, ...partialargs) || targetfunc; 
+}
+
+function validatetargetfunction(targetfunc) {
+
+    if( isfunction(targetfunc) ) return targetfunc;
+
+    const errormessage = format(ERR_BAD_TARGETFUNCTION, typeorclassname(targetfunc));
+    throw new TypeError(errormessage);
 }
 
 module.exports = partial;
