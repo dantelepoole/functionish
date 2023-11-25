e92a60a95c64437ceb2a4b121b6593ef69836596
Dante le Poole <dantelepoole@gmail.com>
2023-11-25 14:18:15 -0400

    Edit JSDoc comment

--- src/Void.js
+++ src/Void.js
@@ -7,14 +7,20 @@
 const isfunction = require('./types/isfunction');
 
 /** 
- * to do
+ * Always return `undefined`. If *expression* is a function, it is called with *args* and its return value is
+ * ignored.
  * 
  * @example <caption>Example usage of `Void()`</caption>
  * 
- * to do
+ * const { Void } = require('functionish');
+ * 
+ * Void(); // returns undefined
+ * Void(console.log, 'fubar'); // prints `fubar` to stdout and returns undefined
  * 
  * @function Void
- * @returns {function}
+ * @param {any} expression A function to call or any other value
+ * @param {...any[]} args The arguments to pass to *expression* if it is a function
+ * @returns {undefined}
  */
 function Void(expression, ...args) {
     isfunction(expression) && expression(...args);
