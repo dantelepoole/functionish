61025edea5765d8e6e6a57b0e1a9f8f64d2a0761
Dante le Poole <dantelepoole@gmail.com>
2023-11-25 14:14:40 -0400

    Add input validation and edit JSDoc comment

--- src/variadic.js
+++ src/variadic.js
@@ -4,12 +4,18 @@
 
 'use strict';
 
+const ERR_BAD_TARGETFUNCTION = `functionish/variadic(): The target function has type '%s'. Expected a function.`;
+
+const format = require('./misc/format');
+const isfunction = require('./types/isfunction');
+const typeorclassname = require('./types/typeorclassname');
+
 /**
- * Return a function that accepts a spread paramter list and invokes the *func* function, passing the arguments
- * as a single array.
+ * Return a function that accepts a variadic argument list and invokes *targetfunc* with the arguments as a single
+ * array.
  * 
- * Use this function to convert a uniadic function that accepts a single array argument to a variadic function with
- * a spread parameter list.
+ * Use this function to convert a function that accepts a single iterable argument to a function with a variadic
+ * argument list.
  * 
  * If *targetfunc* is not a function, the returned function will throw an error. If the returned function is called
  * without any arguments, *targetfunc* will be invoked with an empty array.
@@ -30,9 +36,21 @@
  * @see {@link module:uniadic uniadic()}
  * @param {function} targetfunc The function to make variadic
  * @returns {function}
+ * @throws {TypeError} if *targetfunc* is not a function
  */
-function variadic(targetfunc) {   
+function variadic(targetfunc) {
+
+    validatetargetfunction(targetfunc);
+
     return (...args) => targetfunc(args);
+}
+
+function validatetargetfunction(targetfunc) {
+
+    if( isfunction(targetfunc) ) return targetfunc;
+
+    const errormessage = format(ERR_BAD_TARGETFUNCTION, typeorclassname(targetfunc));
+    throw new TypeError(errormessage);
 }
 
 module.exports = variadic;
