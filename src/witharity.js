6630a7669e74aafc8fe70539ad2ab776c57d0e44
Dante le Poole <dantelepoole@gmail.com>
2023-11-25 14:47:21 -0400

    Add input validation

--- src/witharity.js
+++ src/witharity.js
@@ -4,10 +4,16 @@
 
 'use strict';
 
-const curry = require('./curry');
+const ERR_BAD_ARITY = `functionish/witharity(): The arity '%s'. Expected a positive integer.`;
+const ERR_BAD_TARGETFUNCTION = `functionish/witharity(): The target function has type '%s'. Expected a function.`;
 
-const hasnumbertype = x => (typeof x === 'number');
-const ispositiveinteger = x => (x >= 0) && Number.isSafeInteger(x);
+const curry1 = require('./curry1');
+const format = require('./misc/format');
+const isfunction = require('./types/isfunction');
+const isinteger = require('./types/isinteger');
+const isnumberornan = require('./types/isnumberornan');
+const ispositiveorzero = require('./math/ispositiveorzero');
+const typeorclassname = require('./types/typeorclassname');
 
 /**
  * Set the arity (number of parameters) for *func*. This function returns a new function that always passes *arity*
@@ -38,26 +44,35 @@ const ispositiveinteger = x => (x >= 0) && Number.isSafeInteger(x);
  * @param {function} func The target function
  * @returns {function}
  */
-function witharity(arity, func) {
+const witharity = curry1(function witharity(arity, targetfunc) {
 
     validatearity(arity);
+    validatetargetfunction(targetfunc);
 
     return function _witharity(...args) {
     
-        (args.length === arity) || (args.length = arity);
+        args.length = arity;
 
-        return func(...args);
+        return targetfunc(...args);
     }
+})
+
+function validatetargetfunction(targetfunc) {
+
+    if( isfunction(targetfunc) ) return targetfunc;
+
+    const errormessage = format(ERR_BAD_TARGETFUNCTION, typeorclassname(targetfunc));
+    throw new TypeError(errormessage);
 }
 
 function validatearity(arity) {
 
-    if( ispositiveinteger(arity) ) return arity;
+    if( isinteger(arity) && ispositiveorzero(arity) ) return arity;
 
-    const explanation = hasnumbertype(arity) ? `is ${arity}` : `has type ${typeof arity}`;
+    const explanation = isnumberornan(arity) ? `is ${arity}` : `has type ${typeof arity}`;
+    const errormessage = format(ERR_BAD_ARITY, explanation);
 
-    const errormessage = `functionish/witharity(): The arity ${explanation}. Expected a positive integer.`;
     throw new TypeError(errormessage);
 }
 
-module.exports = curry(1, witharity);
+module.exports = witharity;
