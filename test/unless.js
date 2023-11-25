58692fc9ab942a6c534533430cdbd7b94de1dcf1
Dante le Poole <dantelepoole@gmail.com>
2023-11-24 22:58:50 -0400

    Initial import

---
+++ test/unless.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect;
+
+describe( 'unless()', function() {
+
+    it.skip('should be tested', function() {
+        // noop
+    })
+})
---
+++ test/unlessx.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect;
+
+describe( 'unlessx()', function() {
+
+    it.skip('should be tested', function() {
+        // noop
+    })
+})
