94e7c078d10d2ea595bb19d3d0d052ca5768545a
Dante le Poole <dantelepoole@gmail.com>
2023-11-25 10:17:07 -0400

    Add input validation and edit JSDoc comment

--- src/trycatch.js
+++ src/trycatch.js
@@ -4,38 +4,64 @@
 
 'use strict';
 
-const THIS_NULL = null;
+const ERR_BAD_TARGETFUNCTION = `functionish/flip(): The target function has type '%s'. Expected a function.`;
 
 const curry = require('./curry');
+const format = require('./misc/format');
 const isfunction = require('./types/isfunction');
+const typeorclassname = require('./types/typeorclassname');
 
 /**
- * to do
+ * Return a function that passes its arguments *targetfunc* and returns the result. If *targetfunc* throws, *onerror*
+ * is called.
+ * 
+ * If *onerror* is a function, it is three arguments: the thrown value, *targetfunc* and the array of arguments passed
+ * to *targetfunc* when it threw. If *onerror* is not a function, its value is returned directly.
+ * 
+ * `trycatch()` is curried by default with unary arity.
  * 
  * @example <caption>Example usage of `trycatch()`</caption>
  * 
- * to do
+ * const { trycatch } = require('functionish');
+ * 
+ * const onerror = (err,func,args) = { console.error(err.message, func.name, args); return 'fubar' }
+ * const dosomething = () => { throw new Error('something went wrong') }
+ * 
+ * const result = trycatch(onerror, dosomething, 42);
+ * // prints 'something went wrong', 'dosomething', [42]
+ * // result = 'fubar'
  * 
  * @function trycatch
- * @param {(function|any)} onerror The function to call or the value to return if *func* throws
+ * @param {any} onerror The function to call or the value to return if *targetfunc* throws
  * @param {function} targetfunc The function to run
- * @returns {any}
+ * @returns {any} function
  */
 function trycatch(onerror, targetfunc) {
-    return _trycatch.bind(THIS_NULL, onerror, targetfunc);
+
+    validatetargetfunction(targetfunc);
+
+    return _trycatch.bind(null, onerror, targetfunc);
 }
 
-function _trycatch(errorhandler, targetfunc, ...args) {
+function _trycatch(onerror, targetfunc, ...args) {
 
     try {
         return targetfunc(...args);
     } catch(error) {
         
-        return isfunction(errorhandler)
-             ? errorhandler(error, targetfunc, args)
-             : errorhandler;
+        return isfunction(onerror)
+             ? onerror(error, targetfunc, args)
+             : onerror;
     }
 
+}
+
+function validatetargetfunction(targetfunc) {
+
+    if( isfunction(targetfunc) ) return targetfunc;
+
+    const errormessage = format(ERR_BAD_TARGETFUNCTION, typeorclassname(targetfunc));
+    throw new TypeError(errormessage);
 }
 
 module.exports = curry(1, trycatch);
