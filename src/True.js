8ab45d231fa335fcc5e5a6d00d0bd68172f384e1
Dante le Poole <dantelepoole@gmail.com>
2023-11-24 22:48:42 -0400

    Edit JSDoc comment

--- src/True.js
+++ src/True.js
@@ -7,14 +7,20 @@
 const isfunction = require('./types/isfunction');
 
 /** 
- * to do
+ * Always return the boolean `true`. If *expression* is a function, it is called with *args* and its return value is
+ * ignored.
  * 
  * @example <caption>Example usage of `True()`</caption>
  * 
- * to do
+ * const { True } = require('functionish');
+ * 
+ * True(); // returns true
+ * True(console.log, 'fubar'); // prints `fubar` to stdout and returns true
  * 
  * @function True
- * @returns {function}
+ * @param {any} expression A function to call or any other value
+ * @param {...any[]} args The arguments to pass to *expression* if it is a function
+ * @returns {boolean} Always boolean `true`
  */
 function True(expression, ...args) {
 
