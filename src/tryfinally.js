47c94c823c8ab113ac3e349d24c717b7f0916c39
Dante le Poole <dantelepoole@gmail.com>
2023-11-25 11:47:55 -0400

    Refactor, add input validation and edit JSDoc

--- src/tryfinally.js
+++ src/tryfinally.js
@@ -4,45 +4,78 @@
 
 'use strict';
 
-const THIS_NULL = null;
+const ERR_BAD_FINALLYHANDLER = `functionish/tryfinally(): The finally handler has type '%s'. Expected a function.`;
+const ERR_BAD_TARGETFUNCTION = `functionish/tryfinally(): The target function has type '%s'. Expected a function.`;
 
-const curry = require('./curry');
+const curry1 = require('./curry1');
+const format = require('./misc/format');
+const isfunction = require('./types/isfunction');
+const nothrow = require('./nothrow');
 const raise = require('./errors/raise');
+const typeorclassname = require('./types/typeorclassname');
 
 /**
- * to do
+ * Return a function that passes its arguments *targetfunc* and always calls *onfinally* with the result.
+ * 
+ * *onfinally()* must be a function. It will always be passed an error argument followed by a data argument. If
+ * *targetfunc* throws, the error argument will hold the thrown value and the data argument will be `undefined`. If
+ * *targetfunc* returns, the error argument will be `undefined` and the data argument will hold *targetfunc*'s return
+ * value.
+ * 
+ * If *onfinally()* returns the same error value that *targetfunc* threw, the error value will be rethrown. Otherwise,
+ * *onfinally()*'s return value is passed directly back by to the caller.
+ * 
+ * `tryfinally()` is curried by default with unary arity.
  * 
  * @example <caption>Example usage of `tryfinally()`</caption>
  * 
- * to do
+ * const { tryfinally } = require('functionish');
+ * 
+ * const onfinally = (error,data) = error ? (console.error(error), NaN) : data;
+ * const baddouble = tryfinally(onfinally, () => { throw new Error('something went wrong') });
+ * const gooddouble = tryfinally(onfinally, x => 2*x);
+ * 
+ * baddouble(42); // prints an error to stderr and returns NaN
+ * gooddouble(42)); // returns 84
  * 
  * @function tryfinally
- * @param {function} onfinally to do
- * @param {function} func The function to run
- * @returns {any}
+ * @param {function} onfinally The finally handler function
+ * @param {function} targetfunc The function to run
+ * @returns {function}
  */
-function tryfinally(onfinally, func) {
-    return _tryfinally.bind(THIS_NULL, onfinally, func);
-}
+const tryfinally = curry1(function tryfinally(onfinally, targetfunc) {
+
+    validatefinallyhandler(onfinally);
+    validatetargetfunction(targetfunc);
+
+    return _tryfinally.bind(null, onfinally, nothrow(targetfunc));
+})
 
 function _tryfinally(finallyhandler, targetfunc, ...args) {
 
-    const {data, iserror} = nothrow(targetfunc, ...args);
+    const [data, error] = targetfunc(targetfunc, ...args);
 
-    const result = finallyhandler(data, iserror);
+    const result = finallyhandler(error, data);
 
-    return (result !== data) ? result
-         : iserror ? raise(data)
-         : data;
+    (result === error) && raise(error);
+
+    return result;
 }
 
-function nothrow(targetfunc, ...args) {
+function validatefinallyhandler(onfinally) {
 
-    try {
-        return { data:targetfunc(...args), iserror:false }
-    } catch(error) {
-        return { data:error, iserror:true }
-    }
+    if( isfunction(onfinally) ) return onfinally;
+
+    const errormessage = format(ERR_BAD_FINALLYHANDLER, typeorclassname(onfinally));
+    throw new TypeError(errormessage);
 }
 
-module.exports = curry(1, tryfinally);
+function validatetargetfunction(targetfunc) {
+
+    if( isfunction(targetfunc) ) return targetfunc;
+
+    const errormessage = format(ERR_BAD_TARGETFUNCTION, typeorclassname(targetfunc));
+    throw new TypeError(errormessage);
+}
+
+module.exports = curry1(1, tryfinally);
