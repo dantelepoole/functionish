e33591d0c334c766856d3acf41a8a13b36f14c41
Dante le Poole <dantelepoole@gmail.com>
2023-11-24 22:44:38 -0400

    Add input validation and edit JSDoc comment

--- src/tap.js
+++ src/tap.js
@@ -4,11 +4,14 @@
 
 'use strict';
 
+const ERR_BAD_TARGETFUNCTION = `functionish/tap(): The target function has type %'s'. Expected a function.`;
+
+const format = require('./misc/format');
+const isfunction = require('./types/isfunction');
+const typeorclassname = require('./types/typeorclassname');
+
 /**
- * Return a function that passes its arguments to *func* and returns its own first
- * argument.
- * 
- * [to do: partialargs]
+ * Return a function that passes its arguments to *func* and returns its own first argument.
  * 
  * @example <caption>Example usage of `tap()`</caption>
  * 
@@ -18,11 +21,23 @@
  * 
  * log(42); // prints 42 to the screen and returns 42
  * 
- * @param {function} func The function to tap
+ * @function tap
+ * @param {function} targetfunc The function to tap
  * @returns {function}
  */
-function tap(func) {
-    return (...args) => (func(...args), args[0]);
+function tap(targetfunc) {
+
+    validatetargetfunction(targetfunc);
+    
+    return (...args) => (targetfunc(...args), args[0]);
+}
+
+function validatetargetfunction(targetfunc) {
+
+    if( isfunction(targetfunc) ) return targetfunc;
+
+    const errormessage = format(ERR_BAD_TARGETFUNCTION, typeorclassname(targetfunc));
+    throw new TypeError(errormessage);
 }
 
 module.exports = tap;
