060812bd3e2fadb29a65995355cb7c6be687edf1
Dante le Poole <dantelepoole@gmail.com>
2023-12-01 21:50:40 -0400

    Remove the whenx().for() method

--- src/whenx.js
+++ src/whenx.js
@@ -16,13 +16,6 @@ const isfunction = require('./types/isfunction');
  * argument to pass to the *condition* and only passes any further arguments to either *truebranch* or *falsebranch*
  * depending on whether the *condition* returned a truthy or falsy value.
  * 
- * It is also possible to pass multiple arguments to the *condition* by passing them to the `for()` method of the
- * returned function. This method will then return either the *truebranch* or the *falsebranch* depending on the
- * *condition*'s boolish return value. The returned branch can then be called separately with its own set of arguments.
- * Note that the returned branch will always be a function, even if the branch itself passed to `whenx()` as a
- * static, non-function value. In that case, the returned branch will ignore its arguments and always returned that
- * static value.
- * 
  * Other than this, `whenx()` behaves identically to {@link module:when when()}.
  * 
  * `whenx()` is curried by default with unary arity.
@@ -39,22 +32,6 @@ const isfunction = require('./types/isfunction');
  * 
  * productorsum(42, 1, 2, 3, 4, 5); // follows the true branch and returns the product of 120
  * productorsum(41, 1, 2, 3, 4, 5); // follows the false branch and returns the sum of 15
- * 
- * @example <caption>Example usage of the `whenx().for()` method</caption> 
- * 
- * const { whenx } = require('functionish');
- * 
- * const iseven = x => (x%2) === 0;
- * const product = (...numbers) => numbers.reduce( (a,b) => (a*b), 1 );
- * const sum = (...numbers) => numbers.reduce( (a,b)=>(a+b), 0 );
- * 
- * const productorsum = whenx(iseven, product, sum);
- * 
- * const productorsum42 = productorsum.for(42);
- * const productorsum41 = productorsum.for(41);
- * 
- * productorsum42(1,2,3,4,5); // follows the true branch and returns the product of 120
- * productorsum41(1,2,3,4,5); // follows the false branch and returns the sum of 15
  * 
  * @function whenx
  * @see {@link module:when when()}
@@ -82,15 +59,9 @@ function buildwhenx(condition, truebranch, falsebranch) {
     isfunction(truebranch) || (truebranch = always(truebranch));
     isfunction(falsebranch) || (falsebranch = always(falsebranch));
 
-    const _whenx = (conditionarg, ...branchargs) => condition(conditionarg)
-                                                  ? truebranch(...branchargs)
-                                                  : falsebranch(...branchargs);
-
-    _whenx.for = (...conditionargs) => condition(...conditionargs)
-                                     ? truebranch
-                                     : falsebranch;
-
-    return _whenx;
+    return (conditionarg, ...branchargs) => condition(conditionarg)
+                                          ? truebranch(...branchargs)
+                                          : falsebranch(...branchargs);
 }
 
 module.exports = whenx;
