53429bb6c9c56364eb402f0dbeba82ae4c426d99
Dante le Poole <dantelepoole@gmail.com>
2023-12-05 17:28:17 -0400

    Refactor and add JSDoc comment

--- src/math/runningaverage.js
+++ src/math/runningaverage.js
@@ -4,25 +4,55 @@
 
 'use strict';
 
-const add = (a,b) => (a+b);
+const sumreducer = (a,b) => (a+b);
 
+/**
+ * Return a function that tracks a running average of its argument numbers over multiple calls.
+ * 
+ * The return function accepts a variable number of numeric arguments and returns the average value of all arguments
+ * received in all calls, up to and including the current.
+ * 
+ * The returned function has a `reset()` method that resets its internal state to `0`.
+ * 
+ * The returned function does not verify the argument types, so its behaviour is unpredictable if passed anything other
+ * than number types.
+ * 
+ * @example <caption>Example usage of `runningaverage()`</caption>
+ * 
+ * const { runningaverage } = require('functionish/math');
+ * 
+ * const trackaverage = runningaverage();
+ * 
+ * trackaverage(1); // returns 1
+ * trackaverage(2,3); // returns 2
+ * trackaverage(4,5,6,7,8,9); // returns 5
+ * 
+ * trackaverage.reset();
+ * 
+ * trackaverage(); // returns 0
+ * 
+ * @function runningaverage
+ * @returns {function}
+ */
 function runningaverage() {
 
     let count = 0;
     let total = 0;
 
-    return function _runningaverage(...values) {
+    function _runningaverage(...numbers) {
 
-        const valuecount = values.length;
-
-        count += valuecount;
+        count += numbers.length;
         
-        total += (valuecount === 1)
-               ? values[0]
-               : values.reduce(add, 0);
+        total += (numbers.length === 1)
+               ? numbers[0]
+               : numbers.reduce(sumreducer, 0);
 
-        return (count > 0) ? (total/count) : 0;
+        return (total/count) || 0;
     }
+
+    _runningaverage.reset = () => (count = total = 0);
+
+    return _runningaverage;
 }
 
 module.exports = runningaverage;
