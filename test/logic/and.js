e7252df30113b333dadf40cd2f70bfea5c3b1ca2
Dante le Poole <dantelepoole@gmail.com>
2023-12-06 19:01:38 -0400

    Refactor

--- test/logic/and.js
+++ test/logic/and.js
@@ -1,16 +1,15 @@
 const and = require('../../src/logic/and');
 const expect = require('chai').expect;
-
-const sandbox = require('sinon').createSandbox();
-const spy = sandbox.spy.bind(sandbox);
+const sinon = require('sinon');
 
 const TRUTHY_VALUE = {};
 const FALSY_VALUE = '';
 
-const always = spy(x => () => x);
-const isnumber = spy( x => (typeof x === 'number') );
-const iseven = spy( x => (x%2) === 0 );
-const isstring = spy( x => (typeof x === 'string') );
+const always = x => () => x;
+
+const isnumber = sinon.fake( x => (typeof x === 'number') );
+const iseven = sinon.fake( x => (x%2) === 0 );
+const isstring = sinon.fake( x => (typeof x === 'string') );
 
 describe('and()', function() {
 
@@ -23,63 +22,81 @@ describe('and()', function() {
     )
 
     it('should return a function', function() {
+
+        expect( and() ).to.be.a('function');
+        expect( and(isnumber) ).to.be.a('function');
         expect( and(isnumber, iseven) ).to.be.a('function');
     }) 
 
     describe('The function returned by and()', function() {
 
-        beforeEach(
-            function() {
-                isnumber.resetHistory();
-                iseven.resetHistory();
-                isstring.resetHistory();
-            }
-        )
-
         it('should return true if no predicates were passed to and()', function() {
-            expect( and()() ).to.be.true;
+
+            const conjunct = and();
+
+            expect( conjunct() ).to.be.true;
         }) 
 
         it('should return the return value of the last predicate if all predicates return a truthy value', function() {
 
-            const alwaystrue = spy( always(TRUTHY_VALUE) );
-            const _and = and(isnumber, iseven, alwaystrue);
-            expect( _and(42) ).to.be.equal(TRUTHY_VALUE);
+            const alwaystruthy = sinon.fake( always(TRUTHY_VALUE) );
+            const isevennumber = and( isnumber, iseven, alwaystruthy );
+            
+            const retval = isevennumber(42);
+
+            expect( !!isnumber.returnValues[0] ).to.be.true;
+            expect( !!iseven.returnValues[0] ).to.be.true;
+            expect(retval).to.be.equal(TRUTHY_VALUE);
 
         }) 
 
         it('should call each predicate if all predicates return a truthy value', function() {
 
-            const alwaystrue = spy( always(TRUTHY_VALUE) );
-            const _and = and(isnumber, iseven, alwaystrue);
-            expect( _and(42) ).to.be.equal(TRUTHY_VALUE);
+            const alwaystruthy = sinon.fake( always(TRUTHY_VALUE) );
+            
+            expect( isnumber.callCount ).to.equal(0);
+            expect( iseven.callCount ).to.equal(0);
+            expect( alwaystruthy.callCount ).to.equal(0);
 
-            expect(isnumber.callCount).to.be.equal(1);
-            expect(iseven.callCount).to.be.equal(1);
-            expect(alwaystrue.callCount).to.be.equal(1);
+            const isevennumber = and( isnumber, iseven, alwaystruthy );
+            
+            isevennumber(42);
+
+            expect( isnumber.callCount ).to.equal(1);
+            expect( iseven.callCount ).to.equal(1);
+            expect( alwaystruthy.callCount ).to.equal(1);
 
         }) 
 
         it('should return the return value of the first predicate that returns a falsy value', function() {
 
-            const _and = and(isnumber, always(FALSY_VALUE), iseven);
-            expect( _and(42) ).to.be.equal(FALSY_VALUE);
+            const conjunct = and(isnumber, always(FALSY_VALUE), iseven);
+
+            expect( conjunct(42) ).to.be.equal(FALSY_VALUE);
         }) 
 
         it('should short-circuit if any predicate returns a falsy value', function() {
 
-            const _and = and(isnumber, isstring, iseven);
-            expect( _and(42) ).to.be.false;
+            const alwaysfalsy = sinon.fake( always(FALSY_VALUE) );
+            
+            expect( isnumber.callCount ).to.equal(0);
+            expect( alwaysfalsy.callCount ).to.equal(0);
+            expect( iseven.callCount ).to.equal(0);
 
-            expect(isnumber.callCount).to.be.equal(1);
-            expect(isstring.callCount).to.be.equal(1);
-            expect(iseven.callCount).to.be.equal(0);
+            const isevennumber = and( isnumber, alwaysfalsy, iseven );
+            
+            isevennumber(41);
+
+            expect( isnumber.callCount ).to.equal(1);
+            expect( alwaysfalsy.callCount ).to.equal(1);
+            expect( iseven.callCount ).to.equal(0);
         }) 
 
         it('should throw if any predicate is not a function', function() {
 
-            const _and = and(isnumber, 'foobar');
-            expect( ()=>_and(42) ).to.throw();
+            const conjunct = and(isnumber, 'foobar');
+            
+            expect( ()=>conjunct(42) ).to.throw();
         })
     })
 })
