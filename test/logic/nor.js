fc7d020c405c1eeb5cd3626cd38b04848ed0df79
Dante le Poole <dantelepoole@gmail.com>
2023-12-08 22:19:49 -0400

    Refactor and test for non-function predicates

--- test/logic/nor.js
+++ test/logic/nor.js
@@ -1,16 +1,14 @@
 const nor = require('../../src/logic/nor');
 const expect = require('chai').expect;
-
-const sandbox = require('sinon').createSandbox();
-const spy = sandbox.spy.bind(sandbox);
+const sinon = require('sinon');
 
 const TRUTHY_VALUE = {};
 const FALSY_VALUE = '';
 
-const always = spy(x => () => x);
-const isnumber = spy( x => (typeof x === 'number') );
-const iseven = spy( x => (x%2) === 0 );
-const isstring = spy( x => (typeof x === 'string') );
+const always = x => () => x;
+const isnumber = sinon.fake( x => (typeof x === 'number') );
+const iseven = sinon.fake( x => (x%2) === 0 );
+const isstring = sinon.fake( x => (typeof x === 'string') );
 
 describe('nor()', function() {
 
@@ -28,30 +26,32 @@ describe('nor()', function() {
 
     describe('The function returned by nor()', function() {
 
-        beforeEach(
-            function() {
-                isnumber.resetHistory();
-                iseven.resetHistory();
-                isstring.resetHistory();
-            }
-        )
-
         it('should return true if no predicates were passed to or()', function() {
             expect( nor()() ).to.be.true;
         }) 
 
         it('should return true if all predicates return a falsy value', function() {
 
-            const alwaysfalse = spy( always(FALSY_VALUE) );
-            const _nor = nor(isnumber, iseven, alwaysfalse);
+            const _nor = nor(isnumber, iseven, always(FALSY_VALUE));
             expect( _nor('foobar') ).to.be.true;
+        }) 
+
+        it('should return false if any predicate returns a truthy value', function() {
+
+            const _nor = nor(isnumber, iseven, always(TRUTHY_VALUE));
+            expect( _nor('foobar') ).to.be.false;
         }) 
 
         it('should call each predicate if all predicates return a falsy value', function() {
 
-            const alwaysfalse = spy( always(FALSY_VALUE) );
+            const alwaysfalse = sinon.fake( always(FALSY_VALUE) );
             const _nor = nor(isnumber, iseven, alwaysfalse);
-            expect( _nor('foobar') ).to.be.true;
+
+            expect(isnumber.callCount).to.be.equal(0);
+            expect(iseven.callCount).to.be.equal(0);
+            expect(alwaysfalse.callCount).to.be.equal(0);
+
+            _nor('foobar');
 
             expect(isnumber.callCount).to.be.equal(1);
             expect(iseven.callCount).to.be.equal(1);
@@ -59,20 +59,20 @@ describe('nor()', function() {
 
         }) 
 
-        it('should return false if any predicate returns a truthy value', function() {
-
-            const _nor = nor(isnumber, always(TRUTHY_VALUE), iseven);
-            expect( _nor('foobar') ).to.be.false;
-        }) 
-
         it('should short-circuit if any predicate returns a truthy value', function() {
 
             const _nor = nor(isnumber, isstring, iseven);
-            expect( _nor('foobar') ).to.be.false;
+            _nor('foobar');
 
             expect(isnumber.callCount).to.be.equal(1);
             expect(isstring.callCount).to.be.equal(1);
             expect(iseven.callCount).to.be.equal(0);
         }) 
+
+        it(`should evaluate a predicate's value if the predicate is not a function`, function() {
+
+            expect( nor(isnumber, TRUTHY_VALUE, isstring)({}) ).to.be.false;
+            expect( nor(isnumber, isstring, FALSY_VALUE)({}) ).to.be.true;
+        })
     })
 })
