c82b788dd7c891628380c45b786324c7555b088c
Dante le Poole <dantelepoole@gmail.com>
2023-12-08 22:05:35 -0400

    Accept non-function predicates

--- src/logic/xor.js
+++ src/logic/xor.js
@@ -4,18 +4,22 @@
 
 'use strict';
 
-const curry = require('../curry');
+const curry1 = require('../curry1');
+const callable = require('../callable');
 
 /**
  * Return a function that passes its arguments to both *predicate1* and *predicate2* returns `true` and only if
  * the return values are boolean complements, i.e. if either one returns a truthy value and the other returns a falsy
- * value. If both return the same boolish value, `false` is returned.
+ * value. If both return the same boolish value, `false` is returned. If no predicates are passed, `false` is returned.
  * 
- * [to do: curried]
+ * A *predicate* may be a function or non-function value. In the latter case, the *predicate*'s boolish value is
+ * evaluated directly.
+ * 
+ * `xor()` is curried by default with unary arity.
  * 
  * @example <caption>Example usage of `xor()`</caption>
  * 
- * const xor = require('functionish/logic/xor');
+ * const { xor } = require('functionish/logic');
  * 
  * function haveyourcake() {...}
  * function eatittoo() {...}
@@ -27,11 +31,13 @@ const curry = require('../curry');
  * @param {any} predicate2 The second predicate to evaluate
  * @returns {boolean}
  */
-function xor(predicate1, predicate2) {
+const xor = curry1(function xor(predicate1, predicate2) {
+
+    [predicate1, predicate2] = [ callable(predicate1), callable(predicate2)];
 
     return (...args) => predicate1(...args) 
                       ? !predicate2(...args)
                       : !!predicate2(...args);
-}
+});
 
-module.exports = curry(1, xor);
+module.exports = xor;
