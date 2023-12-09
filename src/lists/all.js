4e2a3e4bbe37b8238799b46816333213224c8c47
Dante le Poole <dantelepoole@gmail.com>
2023-12-09 16:25:17 -0400

    Edit JSDoc comment

--- src/lists/all.js
+++ src/lists/all.js
@@ -12,7 +12,9 @@ const isvoid = require('../types/isvoid');
  * function to each item in *list* and return `true` if and only if *predicate* returns `true` each time. Otherwise,
  * return `false`.
  * 
- * [to do: predicate is function or void]
+ * If the *predicate* is <abbr title="null or undefined">void</abbr>, `all()` evaluates the boolish values of the
+ * individual *list* items instead. If *predicate* is neither <abbr title="null or undefined">void</abbr> nor a
+ * function, an error is thrown.
  * 
  * The function is short-circuited, so it returns `false` as soon as the *predicate* returns a falsy value, without
  * evaluating any remaining items in *list*.
@@ -25,13 +27,16 @@ const isvoid = require('../types/isvoid');
  * 
  * const iseven = x => (x%2) === 0;
  *     
- * all(iseven, [2,4,6]); // returns `true`
- * all(iseven, [1,42,3]);  // returns `false`
+ * all(iseven, [2,4,6]); // returns true
+ * all(iseven, [1,42,3]);  // returns true
+ * 
+ * all(null, [true, 'true', 1]); // returns true because all list items are truthy
+ * all(null, [0, 1]); // returns false because the first list item is falsy
  * 
  * @function all
  * @see {@link module:any any()}
  * @see {@link module:none none()}
- * @param {function} predicate The predicate function
+ * @param {function} [predicate=undefined] The predicate function
  * @param {iterable} list An iterable object producing the items to test
  * @returns {boolean}
  */
