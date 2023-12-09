7456c2841b58b29f25654f86f59db5af8b5b1089
Dante le Poole <dantelepoole@gmail.com>
2023-12-09 16:36:09 -0400

    Edit JSDoc comment

--- src/lists/any.js
+++ src/lists/any.js
@@ -8,10 +8,14 @@ const curry1 = require('../curry1');
 const isvoid = require('../types/isvoid');
 
 /**
- * Apply the *predicate* function to each item in *list* and return `true` when *predicate* returns a truthy value. If
- * *predicate* returns a falsy value for each item, return `false`.
+ * Apply the *predicate* function to each item in *list* and return `true` when *predicate* returns a truthy value for
+ * a *list* item. If *predicate* returns a falsy value for each item, return `false`.
  * 
- * [to do: predicate is function or void]
+ * If the *predicate* is <abbr title="null or undefined">void</abbr>, `any()` evaluates the boolish values of the
+ * individual *list* items instead. If *predicate* is neither <abbr title="null or undefined">void</abbr> nor a
+ * function, an error is thrown.
+ * 
+ * If the *list* is empty, `any()` returns `false`.
  * 
  * The function is short-circuited, so it returns `true` as soon as the *predicate* returns a truthy value, without
  * evaluating any remaining items in *list*.
@@ -24,12 +28,15 @@ const isvoid = require('../types/isvoid');
  * 
  * const iseven = x => (x%2) === 0;
  *     
- * any(iseven, [1,3,7,42,9]); // returns `true`
+ * any(iseven, [1,3,7,42,9]); // returns true
+ * 
+ * any(null, [0,'',false]); // returns false because no list items are truthy
+ * any(null, [1, '', false]); // returns true because the first list item is truthy;
  * 
  * @function any
  * @see {@link module:all all()}
  * @see {@link module:none none()}
- * @param {function} predicate The predicate function
+ * @param {function} [predicate=undefined] The predicate function
  * @param {iterable} list An iterable object producing items to test
  * @returns {boolean}
  */
