fe9d53a0429ee5c41c5a7088d56bc1c1b50b9df5
Dante le Poole <dantelepoole@gmail.com>
2023-12-08 21:43:00 -0400

    Accept a non-function predicate

--- src/lists/any.js
+++ src/lists/any.js
@@ -4,11 +4,14 @@
 
 'use strict';
 
-const curry = require('../curry');
+const curry1 = require('../curry1');
+const isvoid = require('../types/isvoid');
 
 /**
  * Apply the *predicate* function to each item in *list* and return `true` when *predicate* returns a truthy value. If
  * *predicate* returns a falsy value for each item, return `false`.
+ * 
+ * [to do: predicate is function or void]
  * 
  * The function is short-circuited, so it returns `true` as soon as the *predicate* returns a truthy value, without
  * evaluating any remaining items in *list*.
@@ -30,11 +33,15 @@ const curry = require('../curry');
  * @param {iterable} list An iterable object producing items to test
  * @returns {boolean}
  */
-function any(predicate, list) {
+const any = curry1(function any(predicate, list) {
 
-    for(const value of list) if( predicate(value) ) return true;
+    if( isvoid(predicate) ) {
+        for(const value of list) if(value) return true;
+    } else {
+        for(const value of list) if( predicate(value) ) return true;
+    }
 
     return false;
-}
+});
 
-module.exports = curry(1, any);
+module.exports = any;
