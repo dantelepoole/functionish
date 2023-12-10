367f2ec5d8f39ac2564c11fd96ed5ce8ae67d2bb
Dante le Poole <dantelepoole@gmail.com>
2023-12-09 23:33:38 -0400

    Edit JSDoc comment

--- src/lists/append.js
+++ src/lists/append.js
@@ -10,15 +10,30 @@ const isstring = require('../types/isstring');
 const list = require('./list');
 
 /**
- * to do
+ * If *list* is a string, return a new string with the *newitems* appended to *list* in order. Otherwise, *list* should
+ * ben an iterable object, in which case a new list is returned with consists of the items *list* followed by *newitems*
+ * in order.
+ * 
+ * `append()` always returns a list (i.e. iterable object), not an array, even if the input *list* is an array.
+ * 
+ * @example <caption>Example usage of `append()`</caption>
+ * 
+ * const { array } = require('functionish/lists');
+ * 
+ * array( 'foo', 'ba', 'r' ); // returns `foobar'
+ * array( [1,2,3], 4, 5 ); // returns a iterable object that generates the numbers 1 through 5.
  * 
  * @function append
+ * @param {iterable} list A string or iterable object to which to append the *newitems*
+ * @param {...any[]} newitems One or more items to append to the *list*
+ * @returns {iterable}
+ * @throws {TypeError} if *list* is not a string or iterable object
  */
-function append(base, ...items) {
+function append(list, ...newitems) {
 
-    return isstring(base)
-         ? base + items.join(EMPTY_STRING)
-         : appendlists(base, items);
+    return isstring(list)
+         ? list + newitems.join(EMPTY_STRING)
+         : appendlists(list, newitems);
 }
 
 function appendlists(list1, list2) {
