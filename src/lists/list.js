ebc5906cedd424ef92e2a2d271f11198e20aaae4
Dante le Poole <dantelepoole@gmail.com>
2023-12-12 20:41:08 -0400

    Add @@isConcatSpreadable symbol and edit JSDoc

--- src/lists/list.js
+++ src/lists/list.js
@@ -15,22 +15,41 @@ const typeorclassname = require('../types/typeorclassname');
 
 class List {
 
-    constructor(aaiterator) {
-        this[Symbol.iterator] = aaiterator;
+    constructor(iteratormethod) {
+        this[Symbol.iterator] = iteratormethod;
+        this[Symbol.isConcatSpreadable] = true;
     }
 
     [Symbol.iterator]
 
-    [Symbol.isConcatSpreadable] = true
+    [Symbol.isConcatSpreadable]
 }
 
 const islist = list => (list instanceof List);
 const raisebadsource = compose(raise, error.Type(ERR_BAD_SOURCE), typeorclassname);
 
 /**
- * to do
+ * Return an object whose sole purpose is to be lazily iterable.
+ * 
+ * If the *source* is a function, it is assumed to be the \@\@iterator method to invoke when the returned object is
+ * iterated over. This may a generator function or a function that returns an iterator object.
+ * 
+ * If the *source* is already a list object, it is returned unaltered.
+ * 
+ * Otherwise, the *source* is itself assumed to be an iterable object, and the returned object bind's to *source*'s
+ * \@\@iterator method, effectively masking the *source* object itself. The return value will perform lazy iteration,
+ * so it will reflects any changes to *source*'s contents in between iterations.
+ * 
+ * The returned object is also {@link external:Symbol.isConcatSpreadable concat-spreadable}.
+ * 
+ * @example <caption>Example usage of `list()`</caption>
+ * 
+ * const { list } = require('functionish/lists');
+ * 
+ * [to do]
  * 
  * @function list
+ * @see {@link external:Symbol.isConcatSpreadable Symbol.isConcatSpreadable}
  * @param {*} source 
  * @returns 
  */
