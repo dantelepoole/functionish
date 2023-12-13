9dcf2d4d362089004516aa3f2989f591d8b0719d
Dante le Poole <dantelepoole@gmail.com>
2023-12-12 22:47:51 -0400

    Refactor and edit JSDoc comment

--- src/lists/map.js
+++ src/lists/map.js
@@ -5,28 +5,27 @@
 'use strict';
 
 const ERR_BAD_LIST = `functionish/lists/map(): The source argument has type %s. Expected an iterable object.`;
+const ERR_BAD_MAPFUNC = `functionish/lists/map(): The map function has type %s. Expected a function.`;
 
 const compose = require('../compose');
-const curry = require('../curry');
+const curry1 = require('../curry1');
 const error = require('../errors/error');
 const isfunction = require('../types/isfunction');
-const isiterable = require('../types/isiterable');
+const isiterablenotstring = require('../types/isiterablenotstring');
 const list = require('./list');
 const raise = require('../errors/raise');
 const typeorclassname = require('../types/typeorclassname');
 
 const raisebadlisterror = compose(raise, error.Type(ERR_BAD_LIST), typeorclassname);
+const raisebadmapfuncerror = compose(raise, error.Type(ERR_BAD_MAPFUNC), typeorclassname);
 
 /**
- * Return an iterable object that passes each value to the *mapfunc* function and produces the results.
+ * If the *sourcelist* has a method called `map()`, pass the *mapfunc* function to this method and return the
+ * result. Otherwise, return a lazy iterable object that passes each item in the *sourcelist* to the *mapfunc*
+ * and returns the results.
  * 
- * If *list* is an array, this function calls its {@link external:Array.prototype.map Array.prototype.map()}
- * method and returns the result. However, the *mapfunc* function will only ever be called with a single
- * argument (the current list item), not the additional arguments that {@link external:Array.prototype.map Array.prototype.map()}
- * passes to its function.
- * 
- * If *list* is not an array, it is presumed to be iterable and an iterable object is returned
- * that operates lazily.
+ * If the *sourcelist* has a `map()`-method (e.g. an Array) the *mapfunc* is passed to this method and result
+ * is returned.
  * 
  * `map()` is curried by default with unary arity.
  * 
@@ -40,26 +39,26 @@ const raisebadlisterror = compose(raise, error.Type(ERR_BAD_LIST), typeorclassna
  * Array.from(results); // returns [2,4,6]
  *     
  * @function map
- * @param {function} mapfunc The function to apply to each item in *list*
+ * @param {function} mapfunc The function to apply to each item the *sourcelist*
  * @param {iterable} sourcelist An iterable object
  * @returns {iterable}
  */
-function map(mapfunc, sourcelist) {
+const map = curry1(function map(mapfunc, sourcelist) {
     
-    isfunction(mapfunc) || (mapfunc = compose(...mapfunc));
+    isfunction(mapfunc) || raisebadmapfuncerror(mapfunc);
     
-    return isfunction(sourcelist.map) ? sourcelist.map(mapfunc)
-         : isiterable(sourcelist) ? maplist(mapfunc, sourcelist)
+    return isfunction(sourcelist?.map) ? sourcelist.map(mapfunc)
+         : isiterablenotstring(sourcelist) ? maplist(mapfunc, sourcelist)
          : raisebadlisterror(sourcelist);
-}
+});
 
 function maplist(mapfunc, sourcelist) {
 
     return list(
-        function* mappedlist() {
+        function* mapiterator() {
             for(const item of sourcelist) yield mapfunc(item);
         }
     )
 }
 
-module.exports = curry(1, map);
+module.exports = map;
