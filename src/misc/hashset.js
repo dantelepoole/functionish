cdbfee61a3acfe64568863204ee673ed8dc99e1d
Dante le Poole <dantelepoole@gmail.com>
2023-12-16 10:44:51 -0400

    Add input type check and JSDoc comment

--- src/misc/hashset.js
+++ src/misc/hashset.js
@@ -4,16 +4,23 @@
 
 'use strict';
 
+const ERR_BAD_HASHFUNC = `functionish/misc/hashset(): The hashing function has type %s. Expected a function or null/undefined.`;
+
 const HashSet = require('../../lib/HashSet');
 
-const curry = require('../curry');
+const compose = require('../compose');
+const curry1 = require('../curry1');
+const format = require('./format');
 const isfunction = require('../types/isfunction');
+const typeorclassname = require('../types/typeorclassname');
 
-function hashset(hashfunc, initialitems=[]) {
+const throwbadhashfuncerror = compose(format(ERR_BAD_HASHFUNC), typeorclassname);
+
+const hashset = curry1(function hashset(hashfunc, initialitems=[]) {
     
-    return isfunction(hashfunc)
-         ? new HashSet(hashfunc, initialitems)
-         : new Set(initialitems);
-}
+    return isfunction(hashfunc) ? new HashSet(hashfunc, initialitems)
+         : isvoid(hashfunc) ? new Set(initialitems)
+         : throwbadhashfuncerror(hashfunc);
+});
 
-module.exports = curry(1, hashset);
+module.exports = hashset;
