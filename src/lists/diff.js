0c0e4cd8503408abab48c01f8d17885f90adb4c4
Dante le Poole <dantelepoole@gmail.com>
2023-12-16 10:44:16 -0400

    Edit JSDoc comment and restructure code

--- src/lists/diff.js
+++ src/lists/diff.js
@@ -4,28 +4,58 @@
 
 'use strict';
 
-const curry = require('../curry');
+const curry2 = require('../curry2');
 const filter = require('./filter');
 const hashset = require('../misc/hashset');
 const uniqfilter = require('../misc/uniqfilter');
 
 
 /**
- * to do
+ * Return a lazy iterable object that produces only those items from *list1* that are not present in *list2*, with
+ * any duplicates removed, using the optional *hashfunc* to compare list items. If *hashfunc* is
+ * <abbr title="null or undefined">void</abbr>, list items are compared using string equality.
+ * 
+ * `diff()` is curried by default with binary arity.
+ * 
+ * @example <caption>Example usage of `diff()`</caption>
+ * 
+ * const { diff } = require('functionish/lists');
+ * 
+ * const list1 = [1,2,3,4,5];
+ * const list2 = [3,4,5,6,7];
+ * 
+ * [ ...diff(null, list1, list2) ]; // returns [1,2]
+ * 
+ * list1.shift();
+ * list1.shift();
  *  
+ * [ ...diff(null, list1, list2) ]; // returns []
+ * 
+ * @example <caption>Example usage of `diff()` with a hashing function</caption>
+ * 
+ * const users1 = [ { id:1, name:'Fu' }, { id:2, name:'Fubar' }, { id:3, name:'Bar' } ];
+ * const users2 = [ { id:2, name:'Fubar' }, { id:3, name:'Bar' }, { id:42, name:'Douglas Adams' } ];
+ * const getuserid = user => user.id;
+ * 
+ * [ ...diff(getuserid, users1, users2) ]; // returns [ { id:1, name:'Fu' } ]
+ * 
+ * users1.shift();
+ * 
+ * [ ...diff(getuserid, users1, users2) ]; // returns []
+ * 
  * @function diff
- * @param {function} [hashfunc] The hashing function
+ * @param {function} [hashfunc=null] The hashing function
  * @param {iterable} list1 The first iterable
  * @param {iterable} list2 The second iterable
  * @returns {iterable}
  */
-function diff(hashfunc, list1, list2) {
+const diff = curry2(function diff(hashfunc=null, list1, list2) {
 
     const list2set = hashset(hashfunc, list2);
     const isuniq = uniqfilter(hashfunc, list2set);
 
     return filter(isuniq, list1);
-}
+});
 
 
-module.exports = curry(2, diff);
+module.exports = diff;
--- src/lists/uniq.js
+++ src/lists/uniq.js
@@ -4,27 +4,46 @@
 
 'use strict';
 
-const curry = require('../curry');
+const curry1 = require('../curry1');
 const filter = require('./filter');
 const uniqfilter = require('../misc/uniqfilter');
 
 /**
- * to do
+ * Return a lazy iterable object that discards any duplicate items in the *sourcelist*, using the optional *hashfunc*
+ * to compare items. If the *hashfunc* is <abbr title="null or undefined">void</abbr>, the items are compared using
+ * strict equality.
+ * 
+ * `uniq()` is curried by default with binary arity.
  * 
  * @example <caption>Example usage of `uniq()`</caption>
  * 
- * to do
+ * const { uniq } = require('functionish/lists');
+ * 
+ * const numbers = [1,2,2,3,3,3,4,4,4,4,5,5,5,5];
+ * [ ...uniq(null, numbers) ]; // returns [1,2,3,4,5] 
+ * 
+ * numbers.slice(3);
+ * 
+ * [ ...uniq(null,numbers) ]; // returns [3,4,5]
+ * 
+ * @example <caption>Example usage of `uniq()` with a hashing function</caption>
+ * 
+ * const users = [ { id:1, name:'Fu' }, { id:1, name:'Fu Again' }, { id:2, name:'Fubar' } ];
+ * const getuserid = user => user.kd;
+ * 
+ * [ ...uniq(getuserid, users) ]; // returns [ { id:1, name:'Fu' }, { id:2, name:'Fubar' } ];
+ * 
+ * users.shift();
+ * 
+ * [ ...uniq(getuserid, users) ]; // returns [ { id:1, name:'Fu Again' }, { id:2, name:'Fubar' } ];
  * 
  * @function uniq
- * @param {function} [hashfunc] An optional hashing function
+ * @param {function} [hashfunc=null] An optional hashing function or `null` to compare with strict equality
  * @param {iterable} sourcelist An iterable object producing the items to deduplicate
  * @returns {iterable}
  */
-function uniq(hashfunc, sourcelist) {
+const uniq = curry1(function uniq(hashfunc=null, sourcelist) {
+    return filter( uniqfilter(hashfunc), sourcelist );
+});
 
-    const isuniq = uniqfilter(hashfunc);
-    
-    return filter(isuniq, sourcelist);
-}
-
-module.exports = curry(1, uniq);
+module.exports = uniq;
