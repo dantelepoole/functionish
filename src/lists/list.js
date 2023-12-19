5013fc528c9a09717bd2dd522c9117921b4106a9
Dante le Poole <dantelepoole@gmail.com>
2023-12-18 22:03:25 -0400

    Import List class from other module

--- src/lists/list.js
+++ src/lists/list.js
@@ -6,57 +6,63 @@
 
 const ERR_BAD_SOURCE = `functionish/lists/list(): The source argument has type %s. Expected an @@iterator-function or iterable object.`;
 
+const List = require('../../lib/List');
+
 const compose = require('../compose');
 const error = require('../errors/error');
 const isfunction = require('../types/isfunction');
 const isiterable = require('../types/isiterable');
+const islist = require('../types/islist');
 const raise = require('../errors/raise');
 const typeorclassname = require('../types/typeorclassname');
 
-class List {
-
-    constructor(iteratormethod) {
-        this[Symbol.iterator] = iteratormethod;
-        this[Symbol.isConcatSpreadable] = true;
-    }
-
-    [Symbol.iterator]
-
-    [Symbol.isConcatSpreadable]
-}
-
-const islist = list => (list instanceof List);
 const raisebadsource = compose(raise, error.Type(ERR_BAD_SOURCE), typeorclassname);
 
 /**
- * Return an object whose sole purpose is to be lazily iterable.
+ * Return a List-instance whose sole purpose is to be lazily iterable.
  * 
- * If the *source* is a function, it is assumed to be the \@\@iterator method to invoke when the returned object is
- * iterated over. This may a generator function or a function that returns an iterator object.
- * 
- * If the *source* is already a list object, it is returned unaltered.
+ * If the *source* is a function, it is assumed to be the `@@iterator` function to invoke when the returned object is
+ * iterated over. This may be any function that returns an iterator object, e.g. a generator function.
+ *  
+ * If the *source* is already a List instance, it is returned unaltered.
  * 
  * Otherwise, the *source* is itself assumed to be an iterable object, and the returned object bind's to *source*'s
  * @@iterator method, effectively masking the *source* object itself. The return value will perform lazy iteration,
  * so it will reflects any changes to *source*'s contents in between iterations.
  * 
- * The returned object is also {@link external:Symbol.isConcatSpreadable concat-spreadable}.
+ * The returned object has a {@link external:Symbol.isConcatSpreadable Symbol.isConcatSpreadable} property set to
+ * `true`.
  * 
  * @example <caption>Example usage of `list()`</caption>
  * 
  * const { list } = require('functionish/lists');
  * 
- * [to do]
+ * const array = [1,2,3];
+ * const x1 = list(array);
+ * 
+ * typeof x1; // 'object'
+ * x1 === array; // false
+ * 
+ * [...x1]; // [1,2,3];
+ * array..clear;
+ * [...x1]; // []
+ * 
+ * const yieldnumbers = function* () { yield 1; yield 2; yield 3 }
+ * const x2 = list(yieldnumbers);
+ * 
+ * typeof x2; // 'object';
+ * x2 === yieldnumbers; // false
+ * [...x2]; // [1,2,3]
  * 
  * @function list
  * @see {@link external:Symbol.isConcatSpreadable Symbol.isConcatSpreadable}
- * @param {*} source 
+ * @param {(List|function|iterable)} source 
  * @returns 
  */
 function list(source) {
     
-    return isfunction(source) ? new List(source)
-         : islist(source) ? source
+    return islist(source) ? source
+         : isfunction(source) ? new List(source)
          : isiterable(source) ? new List( source[Symbol.iterator].bind(source) )
          : raisebadsource(source);
 }
