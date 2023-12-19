4b4682f7e42926f386c170889d40c2fada77b886
Dante le Poole <dantelepoole@gmail.com>
2023-12-18 22:03:01 -0400

    Initial import

---
+++ src/types/islist.js
@@ -0,0 +1,33 @@
+/**
+ * @module types/iserror
+ */
+
+'use strict';
+
+const List = require('../../lib/List');
+
+/**
+ * Return `true` if the argument is a List instance, in which case it will be lazily iterable. Otherwise, return
+ * `false`.
+ * 
+ * @example <caption>Example usage of `islist()`</caption>
+ * 
+ * const { list } = require('functionish/lists');
+ * const { islist } = require('functionish/types');
+ * 
+ * islist( list([]) ); // returns true
+ * islist( list( function* () { yield 42; } ) ); // returns true
+ * 
+ * islist(); // returns false
+ * islist( [] ); // returns false
+ * islist( 'list' ); // returns false
+ * 
+ * @function islist
+ * @param {any} value The value to check
+ * @returns {boolean}
+ */
+function islist(list) {
+    return (list instanceof List);
+}
+
+module.exports = islist;
