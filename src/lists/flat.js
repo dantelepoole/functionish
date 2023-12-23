351652528476a878ff215f9c46dcffd37b1de9a8
Dante le Poole <dantelepoole@gmail.com>
2023-12-23 11:27:39 -0400

    Flatten recursively per the depth parameter

--- src/lists/flat.js
+++ src/lists/flat.js
@@ -4,51 +4,81 @@
 
 'use strict';
 
+const ERR_BAD_DEPTH = `functionish/lists/flat(): The depth %s. Expected a positive integer of 0 or more.`;
 const ERR_BAD_LIST = `functionish/lists/flat(): The source list has type %s. Expected an iterable object.`;
 
 const compose = require('../compose');
+const curry1 = require('../curry1');
 const error = require('../errors/error');
 const isfunction = require('../types/isfunction');
+const isinteger = require('../types/isinteger');
+const isnumber = require('../types/isnumber');
+const ispositive = require('../math/ispositive');
+const ispositiveorzero = require('../math/ispositiveorzero');
 const list = require('./list');
 const isiterablenotstring = require('../types/isiterablenotstring');
+const not = require('../logic/not');
 const raise = require('../errors/raise');
 const typeorclassname = require('../types/typeorclassname');
+
+const notiterable = not(isiterablenotstring);
+const builddeptherrormessage = depth => isnumber(depth) ? `is ${depth}` : `has type ${depth}`;
+const raisebaddeptherror = compose(raise, error.Type(ERR_BAD_DEPTH), builddeptherrormessage);
+const validatedepth = depth => isinteger(depth) && ispositiveorzero(depth) || raisebaddeptherror(depth);
 
 const raisebadlisterror = compose(raise, error.Type(ERR_BAD_LIST), typeorclassname);
 
 /**
- * Return an iterable object that flattens the values in *sourcelist* by one level, meaning that if any
- * value in *sourcelist* is iterable, that value itself is expanded.
+ * If *source* has a `flat()` method, invoke it with the *depth* argument and return the result. Otherwise, return a
+ * lazy iterable object that flattens the values in *values* by *depth* levels (default: 1).
  * 
- * If *sourcelist* has a `flat()` method, this function calls it and returns the result. Otherwise, return a lazy iterable
- * object that flattens the values in *sourcelist* by one level (meaning that all items in *sourcelist* that are themselves
- * iterable, will be expanded).
+ * Althought Javascript strings are iterable, `flat()` does not flatten them.
+ * 
+ * `flat()` is curried by default by with unary arity.
  * 
  * @example <caption>Example usage of `flat()`</caption>
  * 
  * const { flat } = require('functionish/lists');
  * 
  * const batches = [ [1,2,3], [4,5,6], [7,8], 9, [[10]] ];
- * const flattened = flat(batches);
  * 
- * Array.from(flattened); // returns [1,2,3,4,5,6,7,8,9,[10]];
+ * Array.from( flat(1, batches) ); // returns [1,2,3,4,5,6,7,8,9,[10]];
+ * Array.from( flat(2, batches) ); // returns [1,2,3,4,5,6,7,8,9,10];
  * 
  * @function flat
- * @param {iterable} sourcelist An iterable object
+ * @param {number} [depth=1] The number of levels to recurse
+ * @param {iterable} source An iterable object or an object with a `.flat()` method
  * @returns {iterable}
  */
-function flat(sourcelist) {
+const flat = curry1(function flat(depth=1, source) {
 
-    return isfunction(sourcelist.flat) ? sourcelist.flat()
-         : isiterablenotstring(sourcelist) ? flatlist(sourcelist)
-         : raisebadlisterror(sourcelist);
+    validatedepth(depth);
+
+    return isfunction(source?.flat) ? source.flat(depth)
+         : notiterable(source) ? raisebadlisterror(source)
+         : ispositive(depth) ? flatlist(depth, source)
+         : source;
+});
+
+function* flatsingle(source) {
+
+    for(const item of source) {
+        isiterablenotstring(item) ? yield* item : yield item;
+    }
 }
 
-function flatlist(targetlist) {
+function *flatdeep(depth, source) {
+
+    for(const item of source) {
+        isiterablenotstring(item) ? yield* flatlist(depth-1, item) : yield item;
+    }
+}
+
+function flatlist(depth, source) {
 
     return list(
-        function* () {
-            for(const value of targetlist) isiterablenotstring(value) ? yield* value : yield value;
+        function* _flatlist() {
+            yield* (depth === 1) ? flatsingle(source) : flatdeep(depth, source);
         }
     )
 }
--- test/lists/flat.js
+++ test/lists/flat.js
@@ -9,12 +9,25 @@ describe( 'flat()', function() {
         beforeEach(function () {
         })
 
+        it(`should be curried with unary arity`, function() {
+            
+            should.return.a.function(flat, 1);
+            should.return.an.iterable( flat(1), multidimensionalarray );
+        })
+
+        it(`should throw if the depth is negative or not an integer number`, function() {
+            
+            should.throw(flat, {}, multidimensionalarray);
+            should.throw(flat, flat, multidimensionalarray);
+            should.throw(flat, { flat:'notamethod' }, multidimensionalarray);
+        })
+
         it(`should throw if the source list has no flat()-method and is not iterable`, function() {
             
-            should.throw(flat, {});
-            should.throw(flat, null);
-            should.throw(flat, flat);
-            should.throw(flat, { flat:'notamethod' });
+            should.throw(flat, {}, multidimensionalarray);
+            should.throw(flat, null, multidimensionalarray);
+            should.throw(flat, flat, multidimensionalarray);
+            should.throw(flat, { flat:'notamethod' }, multidimensionalarray);
         })
 
         it(`should call the source list's flat() method if it has one and return the result`, function() {
@@ -22,7 +35,7 @@ describe( 'flat()', function() {
             const fakeflat = fake(x=>x);
             const sourcelist = { flat:fakeflat }
 
-            flat(sourcelist);
+            flat(1, sourcelist);
 
             should.be.called(fakeflat);
         })
@@ -30,26 +43,38 @@ describe( 'flat()', function() {
         it('should return an iterable, non-Array object if the source list does not have a flat() method', function() {
 
             const multidimensionallist = { [Symbol.iterator]:Array.prototype.values.bind(multidimensionalarray) };
-            const flatlist = flat(multidimensionallist);
+            const flatlist = flat(1, multidimensionallist);
 
             should.be.iterable(flatlist);
             should.not.be.an.Array(flatlist);
         })
 
-        describe( 'The iterable returned by flat()', function() {
+        describe( 'The depth argument', function() {
 
-            it('should flatten the sourcelist by one level', function() {
+            it('should default to 1', function() {
                 
                 const multidimensionallist = { [Symbol.iterator]:Array.prototype.values.bind(multidimensionalarray) };
-                const flatlist = flat(multidimensionallist);
+                const flatlist = flat(undefined, multidimensionallist);
 
-                should.be.like( multidimensionalarray.flat(), [...flatlist] );
+                should.be.like( multidimensionalarray.flat(1), [...flatlist] );
+            })
+        });
+
+        describe( 'The iterable returned by flat()', function() {
+
+            it('should flatten the sourcelist recursively as per the depth argument', function() {
+                
+                const multidimensionallist = { [Symbol.iterator]:Array.prototype.values.bind(multidimensionalarray) };
+
+                should.be.like( multidimensionalarray.flat(0), [...flat(0, multidimensionallist)] );
+                should.be.like( multidimensionalarray.flat(1), [...flat(1, multidimensionallist)] );
+                should.be.like( multidimensionalarray.flat(2), [...flat(2, multidimensionallist)] );
             })
 
             it('should be empty if the sourcelist is empty', function() {
 
                 const emptylist = { [Symbol.iterator]:Array.prototype.values.bind([]) };
-                const flatlist = flat(emptylist);
+                const flatlist = flat(1, emptylist);
 
                 should.be.like( [], [...flatlist] );
             })
@@ -59,13 +84,13 @@ describe( 'flat()', function() {
                 const multidimensionalarraycopy = multidimensionalarray.slice();
                 const multidimensionallist = { [Symbol.iterator]:Array.prototype.values.bind(multidimensionalarraycopy) };
 
-                const flatlist = flat(multidimensionallist);
+                const flatlist = flat(1, multidimensionallist);
                 
-                should.be.like( multidimensionalarraycopy.flat(), [...flatlist]);
+                should.be.like( multidimensionalarraycopy.flat(1), [...flatlist]);
                 
                 multidimensionalarraycopy.pop();
                 
-                should.be.like( multidimensionalarraycopy.flat(), [...flatlist]);
+                should.be.like( multidimensionalarraycopy.flat(1), [...flatlist]);
             })
         });
 
