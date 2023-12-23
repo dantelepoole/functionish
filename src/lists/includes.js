fff736f18fac702741a2e3e1a6e48708f0ac1169
Dante le Poole <dantelepoole@gmail.com>
2023-12-23 17:29:24 -0400

    Edit JSDoc comment

--- src/lists/includes.js
+++ src/lists/includes.js
@@ -4,28 +4,35 @@
 
 'use strict';
 
-const curry = require('../curry');
+const curry1 = require('../curry1');
 const isfunction = require('../types/isfunction');
 
 /**
- * [to do]
+ * If *sourcelist* has an `includes()` method, it is passed the *targetvalue* and the result is returned. Otherwise,
+ * return `true` if any item in the *sourcelist* is strictly equal to the *targetvalue*, or `false` if no such items
+ * exists in the *sourcelist*.
+ * 
+ * `includes()` is curried by default with unary arity.
  * 
  * @example <caption>Example usage of `includes()`</caption>
  * 
- * to do
+ * const { includes } = require('functionish/lists');
+ * 
+ * includes(42, [1,2,3,4]); // returns false
+ * includes(42, [42]); // returns true
  * 
  * @function includes
  * @param {any} targetvalue The value to look for
  * @param {iterable} list An iterable object
  * @returns {boolean}
  */
-function includes(targetvalue, list) {
+const includes = curry1(function includes(targetvalue, sourcelist) {
 
-    if( isfunction(list.includes) ) return list.includes(targetvalue);
+    if( isfunction(sourcelist.includes) ) return sourcelist.includes(targetvalue);
     
-    for(const value of list) if(value === targetvalue) return true;
+    for(const item of sourcelist) if(item === targetvalue) return true;
 
     return false;
-}
+})
 
-module.exports = curry(1, includes);
+module.exports = includes;
