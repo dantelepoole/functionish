220bd9a5be7768fb158ec73a131361e6cefbf92c
Dante le Poole <32337322+dantelepoole@users.noreply.github.com>
2023-12-29 18:39:58 +0000

    Resolve string predicate argument

--- src/lists/all.js
+++ src/lists/all.js
@@ -4,22 +4,34 @@
 
 'use strict';
 
-const curry1 = require('../curry1');
+const ERR_BAD_LIST = `functionish/lists/all(): The source list has type %s. Expected an iterable object.`;
+
+const exception = require('../errors/exception');
+const isiterable = require('../types/isiterable');
+const issingleton = require('../arrays/issingleton');
+const isstring = require('../types/isstring');
 const isvoid = require('../types/isvoid');
+const not = require('../logic/not');
+const resolve = require('../misc/resolve');
+const typeorclassname = require('../types/typeorclassname');
+
+const notiterable = not(isiterable);
+const raisebadlisterror = exception('TypeError', ERR_BAD_LIST, typeorclassname);
 
 /**
  * Functional variant of {@link external:Array.prototype.every() Array.prototype.every()}. Apply the *predicate*
- * function to each item in *list* and return `true` if and only if *predicate* returns `true` each time. Otherwise,
+ * function to each item in *sourcelist* and return `true` if and only if *predicate* returns `true` each time. Otherwise,
  * return `false`.
  * 
  * If the *predicate* is <abbr title="null or undefined">void</abbr>, `all()` evaluates the boolish values of the
- * individual *list* items instead. If *predicate* is neither <abbr title="null or undefined">void</abbr> nor a
- * function, an error is thrown.
+ * individual *sourcelist* items instead. * If the *predicate* is a string, it is assumed to be the path to a function in a package or file module 
+ * to be resolved using {@link module:misc/resolve resolve()}. If *predicate* is neither <abbr title="null or undefined">void</abbr> nor a
+ * function or a string, an error is thrown.
  * 
- * If the *list* is empty, `all()` returns `true`.
+ * If the *sourcelist* is empty, `all()` returns `true`.
  * 
  * The function is short-circuited, so it returns `false` as soon as the *predicate* returns a falsy value, without
- * evaluating any remaining items in *list*.
+ * evaluating any remaining items in *sourcelist*.
  * 
  * `all()` is curried by default with unary arity.
  * 
@@ -38,19 +50,26 @@ const isvoid = require('../types/isvoid');
  * @function all
  * @see {@link module:any any()}
  * @see {@link module:none none()}
- * @param {function} [predicate=undefined] The predicate function
- * @param {iterable} list An iterable object producing the items to test
+ * {@link module:misc/resolve resolve()}
+ * @param {(function|string)} [predicate=undefined] The predicate function
+ * @param {iterable} sourcelist An iterable object producing the items to test
  * @returns {boolean}
  */
-const all = curry1(function all(predicate, list) {
+function all(predicate, sourcelist) {
 
-    if( isvoid(predicate) ) {
-        for(const value of list) if( !value ) return false;
+    isstring(predicate) && (predicate = resolve(predicate));
+    
+    if( issingleton(arguments) ) {
+        return all.bind(null, predicate);
+    } else if( notiterable(sourcelist) ) {
+        raisebadlisterror(sourcelist); 
+    } else if( isvoid(predicate) ) {
+        for(const value of sourcelist) if( !value ) return false;
     } else {
-        for(const value of list) if( !predicate(value) ) return false;
+        for(const value of sourcelist) if( !predicate(value) ) return false;
     }
 
     return true;
-});
+};
 
 module.exports = all;
--- test/lists/all.js
+++ test/lists/all.js
@@ -3,6 +3,7 @@ const dispatch = require('../../lib/test/dispatch');
 const fake = require('sinon').fake;
 const should = require('../../lib/test/should');
 
+const paths = ['/', '/home', '../'];
 const emptylist = { [Symbol.iterator]:Array.prototype.values.bind([]) }
 const list1to10 = { [Symbol.iterator]:Array.prototype.values.bind([1,2,3,4,5,6,7,8,9,10]) }
 
@@ -25,6 +26,25 @@ describe('lists/all()', function() {
         function() {
             should.return.true(all, isnumber, emptylist)
             should.not.be.called(isnumber);
+        }
+    )
+
+    
+    it('should throw if the predicate is not a function, a string or null/undefined', function() {
+        should.throw(all, 42, paths);
+        should.throw(all, {}, paths);
+        should.throw(all, paths, paths);
+    })
+    
+    it('should throw if the predicate is a string that does not resolve to a function in a package or file module', function() {
+        
+        should.throw(all, 'path#FuBar', paths);
+        should.throw(all, 'path#delimiter', paths);
+    })
+    
+    it('should throw if the list is not iterable',
+        function () {
+            should.throw(all, isnumber, {});
         }
     )
 
@@ -71,18 +91,6 @@ describe('lists/all()', function() {
             should.return.true(all, undefined, list1to10);
             should.return.false(all, null, [0, ...list1to10]);
             should.return.false(all, undefined, [0, ...list1to10]);
-        }
-    )
-
-    it('should throw if the predicate is not a function nor null or undefined',
-        function () {
-            should.throw(all, 41, list1to10);
-        }
-    )
-
-    it('should throw if the list is not iterable',
-        function () {
-            should.throw(all, isnumber, {});
         }
     )
 })
