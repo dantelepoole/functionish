5af81dd6a6a5aee344bec2d09af0fc2ed4739f07
Dante le Poole <dantelepoole@gmail.com>
2024-01-03 20:00:22 -0400

    Check argument type and edit JSDoc comment

--- src/lists/shuffle.js
+++ src/lists/shuffle.js
@@ -4,15 +4,32 @@
 
 'use strict';
 
-const coinflip = require('../misc/coinflip');
+const ERR_BAD_LIST = `functionish/lists/shuffle(): The source list has type %s. Expected an iterable object.`;
+
+const exception = require('../errors/exception');
+const isiterable = require('../types/isiterable');
+const typeorclassname = require('../types/typeorclassname');
+const validator = require('../errors/validator');
+
+const randomitemshuffle = () => (Math.random() - 0.5);
+const validatelist = validator(
+    exception('TypeError', ERR_BAD_LIST, typeorclassname),
+    isiterable
+)
+
 
 /**
- * [to do]
+ * Return an array containing the items from *sourcelist* shuffled randomly.
  * 
  * @function shuffle
+ * @param {iterable} sourcelist An iterable object with the items to sort
+ * @returns {any[]}
  */
-function shuffle(list) {
-    return Array.from(list).sort(coinflip);
+function shuffle(sourcelist) {
+
+    validatelist(sourcelist);
+
+    return Array.from(sourcelist).sort(randomitemshuffle);
 }
 
 module.exports = shuffle;
