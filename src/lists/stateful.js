65114cc0ec709ee3d12407b8a03ab36124fe719f
Dante le Poole <dantelepoole@gmail.com>
2024-01-06 13:11:19 -0400

    Edit code and JSDoc comment

--- src/lists/stateful.js
+++ src/lists/stateful.js
@@ -9,21 +9,46 @@ const compose = require('../compose');
 const iterator = require('./iterator');
 const list = require('./list');
 
-const initstatefuliterator = compose(list, always, iterator);
+const initstatefullist = compose(list, always, iterator);
 
 /**
- * to do
+ * Return an iterable object that always returns the same iterator object, preserving the iterator's state between
+ * subsequent iterations.
  * 
  * @example <caption>Example usage of `stateful()`</caption>
  * 
- * to do
+ * const { stateful } = require('functionish/lists');
  * 
- * @function filter
+ * const statefullist = stateful([1,2,3,4,5,6]);
+ * 
+ * for(const item of statefullist) {
+ *   console.log(item);
+ *   if(item === 3) break;
+ * }
+ * // prints:
+ * //   1
+ * //   2
+ * //   3
+ * 
+ * for(const item of statefullist) {
+ *   console.log(item);
+ * }
+ * // prints:
+ * //   4
+ * //   5
+ * //   6
+ * 
+ * for(const item of statefullist) {
+ *   console.log(item);
+ * }
+ * // prints nothing
+ * 
+ * @function stateful
  * @param {iterable} sourcelist An iterable object
  * @returns {iterable} 
  */
 function stateful(sourcelist) {
-    return initstatefuliterator(sourcelist);
+    return initstatefullist(sourcelist);
 }
 
 module.exports = stateful;
